4ab0a0358b034882ca0a8493776d9c4010ad1c7d
yanndanielou <yanndanielou@users.noreply.github.com>
2024-12-27 23:12:27 +0100

    lists

--- Python/Common/list_utils.py
+++ Python/Common/list_utils.py
@@ -2,7 +2,21 @@
 
 #https://www.digitalocean.com/community/tutorials/how-to-compare-two-lists-in-python
 
-def are_list_equals(list1, list2):
+def are_list_equals(list1:list, list2:list):
     l1_sorted = sorted(list1)
     l2_sorted = sorted(list2)
     return l1_sorted == l2_sorted
+
+
+#https://www.geeksforgeeks.org/python-check-if-a-list-is-contained-in-another-list/
+
+#Approach #1: Naive Approach 
+def is_list_included_in_second_list(list1:list, list2:list):
+
+    pass
+
+
+
+#Approach #1: Naive Approach 
+def are_all_elements_of_list_included_in_list(list1:list, list2:list):
+    return set(list1) <= set(list2)
--- Python/Common/test_list_utils.py
+++ Python/Common/test_list_utils.py
@@ -16,6 +16,18 @@ class TestListEquals(unit_tests_helpers.TestCaseBase):
         self.assertTrue(list_utils.are_list_equals([0,1],[0,1]))
         self.assertTrue(list_utils.are_list_equals([0,1],[1,0]))
         
-    
+class TestAreAllElementsOfListIncludedInList(unit_tests_helpers.TestCaseBase):
+
+    def __init__(self, methodName='runTest'):  
+        super().__init__(methodName)  
+
+    def test_list_with_order(self):
+        self.assertTrue(list_utils.are_all_elements_of_list_included_in_list([0],[0]))
+        self.assertTrue(list_utils.are_all_elements_of_list_included_in_list([1, 2],[1, 2, 3, 1, 1, 2, 2]))
+        self.assertTrue(list_utils.are_all_elements_of_list_included_in_list(['x', 'y', 'z'],['x', 'a', 'y', 'x', 'b', 'z']))
+        self.assertTrue(list_utils.are_all_elements_of_list_included_in_list([0,1],[0,1]))
+        self.assertTrue(list_utils.are_all_elements_of_list_included_in_list([0,1,1],[0,1,1,2]))
+        self.assertFalse(list_utils.are_all_elements_of_list_included_in_list([0,1,2],[0,1,3]))
+
 if __name__ == "__main__":
     unittest.main()
