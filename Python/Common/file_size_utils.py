1058b112bee2480f181a9b7589e60f3d5e39e4a1
yanndanielou <yanndanielou@users.noreply.github.com>
2025-01-05 20:49:53 +0100

    Update file_size_utils.py

--- Python/Common/file_size_utils.py
+++ Python/Common/file_size_utils.py
@@ -16,7 +16,8 @@ import logger_config
 import urllib.request
 from urllib.error import URLError, HTTPError
 
-def get_url_file_size(link:str)-> int:
+def get_url_file_size(link:str)-> tuple[bool,int|str]:
+    """ get_url_file_size """
     try:
         with urllib.request.urlopen(link) as url_open:
             return True, url_open.length
@@ -26,9 +27,8 @@ def get_url_file_size(link:str)-> int:
         return False, f'Error! {e}'
     except URLError as e:
         # do something
-        logger_config.print_and_log_error(f'Error code: {e.code}')
+        logger_config.print_and_log_error(f'Error code: {e}')
         return False, f'Error! {e}'
-    
 
 def convert_bits_to_human_readable_size(size_as_bits:int)-> str:
     """ https://stackoverflow.com/questions/1094841/get-a-human-readable-version-of-a-file-size """
