e9de47ed9d5d204417541ce00a18a4828713e7a4
yanndanielou <yanndanielou@users.noreply.github.com>
2025-01-05 21:21:44 +0100

    Update destinations.py

--- Python/M3uToFreebox/destinations.py
+++ Python/M3uToFreebox/destinations.py
@@ -11,36 +11,35 @@ from tkinter import (
 
 class DestinationsFolders(metaclass=Singleton):
     """ Manager of m3u filters"""
-    def __init__(self):
-        self._destinations_folders:list[tuple[str, str]] = [DestinationFolder("Freebox", "\\\\Freebox_Server\\NO NAME\\M3U_Playlist"),DestinationFolder("Local", "D:\\"), DestinationFolder("..", None)]
+    def __init__(self)->None:
+        self._destinations_folders:list[DestinationFolder] = [DestinationFolder("Freebox", "\\\\Freebox_Server\\NO NAME\\M3U_Playlist"),
+                                                              DestinationFolder("Local", "D:\\"),
+                                                              DestinationFolder("..", None)]
 
-
-    def add_destination(self, name:str, path:str):
-        """ Add destinations """
-        self._destinations_folders.append((name, path))
-        
     @property
-    def destinations_folders(self):
+    def destinations_folders(self)->list['DestinationFolder']:
         """ Getter filters """
         #return self._destinations_folders
         return self._destinations_folders
 
 class DestinationFolder():
-    def __init__(self, label, path):
+    """ DestinationFolder """
+    def __init__(self, label:str, path:str|None)->None:
         self._label = label
         self._path = path
-    
-    def get_path(self):
+
+    def get_path(self)->str|None:
+        """ get_path """
         if self._path is not None:
             return self._path
-        
+
         else:
-            directory_path:filedialog.Directory = filedialog.askdirectory()
+            directory_path = filedialog.askdirectory()
             directory_path_name = str(directory_path)
             logger_config.print_and_log_info("Directory chosen:" + str(directory_path_name))
 
             if directory_path_name != "":
-                DestinationsFolders().destinations_folders.append(directory_path_name)
+                DestinationsFolders().destinations_folders.append(DestinationFolder(directory_path_name, directory_path_name))
                 return directory_path_name
             else:
                 logger_config.print_and_log_info("No directory chosen")
