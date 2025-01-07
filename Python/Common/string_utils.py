daf4dc7605650b4b802e609164d4db2406d04939
yanndanielou <yanndanielou@users.noreply.github.com>
2025-01-07 17:07:54 +0100

    Update string_utils.py

--- Python/Common/string_utils.py
+++ Python/Common/string_utils.py
@@ -18,17 +18,17 @@ Taken from https://gist.github.com/seanh/93666#file-formatfilename-py
 """
 
     windows_reserved_filemanes = ["CON, PRN, AUX, NUL, COM1, COM2, COM3, COM4, COM5, COM6, COM7, COM8, COM9, LPT1, LPT2, LPT3, LPT4, LPT5, LPT6, LPT7, LPT8, LPT9"]# pylint: disable=line-too-long
-    forbidden_windows_caracters = [("<","(less than)"), (">" ,"(greater than)"), (":", "(colon - sometimes works, but is actually NTFS Alternate Data Streams)"),('"',"(double quote)"),("/","(forward slash)"),("\\","(backslash)"),("|","(vertical bar or pipe)"),("?","(question mark)"),("*","(asterisk)")]
+    forbidden_windows_caracters = [("<","(less than)"), (">" ,"(greater than)"), (":", "(colon - sometimes works, but is actually NTFS Alternate Data Streams)"),('"',"(double quote)"),("/","(forward slash)"),("\\","(backslash)"),("|","(vertical bar or pipe)"),("?","(question mark)"),("*","(asterisk)")]# pylint: disable=line-too-long
 
     not_convenient_caracters = [chr(9600), chr(9604)]
 
     #0-31 (ASCII control characters)
-    windows_non_printable_characters = list(map(chr, range(0, 31))) # previously list(map(lambda x: chr(x), range(0, 31)))# pylint: disable=line-too-long
+    windows_non_printable_characters = list(map(chr, range(0, 31))) # previously list(map(lambda x: chr(x), range(0, 31)))
 
     if input_original_string in windows_reserved_filemanes:
         return "_" + input_original_string
 
-    for forbidden_windows_caracter in list(map(lambda x: x[0],forbidden_windows_caracters)) + windows_non_printable_characters + not_convenient_caracters:
+    for forbidden_windows_caracter in list(map(lambda x: x[0],forbidden_windows_caracters)) + windows_non_printable_characters + not_convenient_caracters:# pylint: disable=line-too-long
         input_original_string = input_original_string.replace(forbidden_windows_caracter, "")
 
     if not allow_spaces:
