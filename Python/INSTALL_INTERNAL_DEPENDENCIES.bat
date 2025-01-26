a3c253000c5364a51a38baa8e4e876f7424f247e
yanndanielou <yanndanielou@users.noreply.github.com>
2025-01-26 09:14:13 +0100

    Update INSTALL_INTERNAL_DEPENDENCIES.bat

--- Python/INSTALL_INTERNAL_DEPENDENCIES.bat
+++ Python/INSTALL_INTERNAL_DEPENDENCIES.bat
@@ -1,12 +1,22 @@
 @CALL SET_PYTHON_HOME.bat
 
-CALL :INSTALL_INTERNAL_PYTHON_LIB Common
+CALL :INSTALL_INTERNAL_PYTHON_LIB ShutTheBox
+pause
+
+rem CALL :INSTALL_INTERNAL_PYTHON_LIB Common
 CALL :INSTALL_INTERNAL_PYTHON_LIB Logger
+pause
 
 @GOTO :END_OF_FILE
 
 
 :INSTALL_INTERNAL_PYTHON_LIB
+@Title install internal python library %1
+@Echo install internal python library %1
+call %PYTHON_HOME%\python.exe -m pip install -e %1
+@EXIT /B 0
+
+:INSTALL_WITH_MANUAL_FILE_COPYINTERNAL_PYTHON_LIB
 @Title install internal python library %1
 @Echo install internal python library %1
 call %PYTHON_HOME%\python.exe -m pip install -e %1
