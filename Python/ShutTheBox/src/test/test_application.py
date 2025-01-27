1f04f8bdda96d16a00522f149e944159985cd026
yanndanielou <yanndanielou@users.noreply.github.com>
2025-01-27 12:18:58 +0100

    Update test_application.py

--- Python/ShutTheBox/src/test/test_application.py
+++ Python/ShutTheBox/src/test/test_application.py
@@ -17,7 +17,7 @@ class TestOneStep:
     checker:application.Application = application.Application()
 
     
-    def test_last_step(self)->None:
+    def test_last_step(self) -> None:
         all_possibilities_with_hatches:list[application.OneGame] = self.checker.compute_all_possibilities_with_hatches([1,2,3])
         
         assert all_possibilities_with_hatches is not None
@@ -53,10 +53,3 @@ class TestApplicationMisc:
     def get_all_unique_combinaisons_to_attains_exaclty_sum_using_element_no_more_than_once(self)->None:
         assert self.checker.get_all_unique_combinaisons_to_reach_exaclty_sum_using_element_no_more_than_once([1,2,3],2) == set(set(2))
     
-
-    def test_get_two_dices_all_combinaisons_with_occurences(self)->None:
-        """ Number of results """
-        two_dices_all_combinaisons_with_occurences:dict[int, int]= self.checker.get_two_dices_all_combinaisons_with_occurences()
-        assert two_dices_all_combinaisons_with_occurences[2] == 1
-        assert two_dices_all_combinaisons_with_occurences[3] == 2
-        assert len(two_dices_all_combinaisons_with_occurences) == 11
