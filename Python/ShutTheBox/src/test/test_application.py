a61748329e37ca1df99f5741109d226a43936970
yanndanielou <yanndanielou@users.noreply.github.com>
2025-01-27 12:28:07 +0100

    Shut the box

--- Python/ShutTheBox/src/test/test_application.py
+++ Python/ShutTheBox/src/test/test_application.py
@@ -1,27 +1,17 @@
 # -*-coding:Utf-8 -*
-import pytest
-
-import os
-import sys
-
-
-
-from typing import List, Tuple
+#import pytest
 
 from shutthebox import application
-    
+
 class TestOneStep:
-    
+
     checker:application.Application = application.Application()
 
-    
     def test_last_step(self) -> None:
         all_possibilities_with_hatches:list[application.OneGame] = self.checker.compute_all_possibilities_with_hatches([1,2,3])
-        
         assert all_possibilities_with_hatches is not None
-        
 
-        
+
 class TestUniqueccombinaisonsToReachExacltySumUsingElementNoMoreThanOnce:
     """ TestApplicationWithoutHmi """
     checker:application.Application = application.Application()
@@ -30,7 +20,7 @@ class TestUniqueccombinaisonsToReachExacltySumUsingElementNoMoreThanOnce:
         assert not self.checker.get_all_unique_combinaisons_to_reach_exaclty_sum_using_element_no_more_than_once([1],12)
         assert not self.checker.get_all_unique_combinaisons_to_reach_exaclty_sum_using_element_no_more_than_once([1,2,3],7)
         assert not self.checker.get_all_unique_combinaisons_to_reach_exaclty_sum_using_element_no_more_than_once([1,5,7],4)
-        
+
     def test_chatgpt(self)->None:
         liste = [2, 3, 5, 7]
         somme_a_atteindre = 7
@@ -41,13 +31,11 @@ class TestUniqueccombinaisonsToReachExacltySumUsingElementNoMoreThanOnce:
         assert self.checker.get_all_unique_combinaisons_to_reach_exaclty_sum_using_element_no_more_than_once([1],1) == [[1]]
         assert self.checker.get_all_unique_combinaisons_to_reach_exaclty_sum_using_element_no_more_than_once([1,2,3],2) == [[2]]
 
-    
 
 class TestApplicationMisc:
     """ TestApplicationWithoutHmi """
     
     checker:application.Application = application.Application()
 
-    def get_all_unique_combinaisons_to_attains_exaclty_sum_using_element_no_more_than_once(self)->None:
-        assert self.checker.get_all_unique_combinaisons_to_reach_exaclty_sum_using_element_no_more_than_once([1,2,3],2) == set(set(2))
-    
+    def test_get_all_unique_combinaisons_to_attains_exaclty_sum_using_element_no_more_than_once(self)->None:
+        assert self.checker.get_all_unique_combinaisons_to_reach_exaclty_sum_using_element_no_more_than_once([1,2,3],2) == [[2]]
