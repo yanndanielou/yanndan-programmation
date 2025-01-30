9413e7cea1f37622cfcd0aa74b0c1c3fa3b1378f
yanndanielou <yanndanielou@users.noreply.github.com>
2025-01-30 11:55:03 +0100

    Update test_multiplication.py

--- Python/ExampleLibrary/tests/test_multiplication.py
+++ Python/ExampleLibrary/tests/test_multiplication.py
@@ -4,5 +4,5 @@ from ExampleLibrary.multiplication import multiply_custom
 def test_multiply_custom() -> None:
     assert multiply_custom(2, 3) == 6
     assert multiply_custom(-1, 3) == -3
-    assert multiply_custom(0, 5) == 0
+    assert not multiply_custom(0, 5)
     assert multiply_custom(4, -2) == -8
