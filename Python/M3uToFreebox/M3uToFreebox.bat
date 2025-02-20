b89612d0fdd00a20a8c3e3c1a883765d464f845a
yanndanielou <yanndanielou@users.noreply.github.com>
2025-02-20 19:20:14 +0100

    Update M3uToFreebox.bat

--- Python/M3uToFreebox/M3uToFreebox.bat
+++ Python/M3uToFreebox/M3uToFreebox.bat
@@ -1,4 +1,4 @@
 cd D:\github\yanndanielou-programmation\Python\M3uToFreebox
 CALL D:\github\yanndanielou-programmation\Python\SET_PYTHON_HOME.bat
 
-CALL %PYTHON_HOME%\python.exe main.py
+CALL %PYTHON_HOME%\python.exe src\m3utofreebox\main.py
