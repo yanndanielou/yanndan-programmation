0ea5acf801ca67a2a66310855e313de3b4561e94
yanndanielou <yanndanielou@users.noreply.github.com>
2025-02-21 16:49:18 +0100

    Update main.py

--- Python/M3uToFreebox/src/m3utofreebox/main.py
+++ Python/M3uToFreebox/src/m3utofreebox/main.py
@@ -1,6 +1,6 @@
 # -*-coding:Utf-8 -*
 
-#import sys
+# import sys
 
 import time
 
@@ -11,22 +11,25 @@ from common import date_time_formats
 from application import M3uToFreeboxApplication
 from main_view import M3uToFreeboxMainView
 
-def main()->None:
-    """ Main function """
+
+def main() -> None:
+    """Main function"""
 
     with logger_config.stopwatch_with_label("Application duration"):
         application_start_time = time.time()
         logger_config.configure_logger_with_random_log_file_suffix("M3uToFreebox")
 
-        logger_config.print_and_log_info('Start application')
+        logger_config.print_and_log_info("Start application")
 
-        mainview = M3uToFreeboxMainView()
-        app: M3uToFreeboxApplication = M3uToFreeboxApplication(mainview)
-        mainview.m3u_to_freebox_application = app
-        mainview.mainloop()
+        main_view = M3uToFreeboxMainView()
+        app: M3uToFreeboxApplication = M3uToFreeboxApplication(main_view)
+        main_view.m3u_to_freebox_application = app
+        main_view.mainloop()
 
-        logger_config.print_and_log_info("End. Nominal end of application in " +
-                                         date_time_formats.format_duration_to_string(time.time() - application_start_time))
+        logger_config.print_and_log_info(
+            "End. Nominal end of application in "
+            + date_time_formats.format_duration_to_string(time.time() - application_start_time)
+        )
 
 
 if __name__ == "__main__":
