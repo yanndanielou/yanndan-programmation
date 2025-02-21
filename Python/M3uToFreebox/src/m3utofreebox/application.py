9e3b558f2906496b1cf8b101b91be8f79086078d
yanndanielou <yanndanielou@users.noreply.github.com>
2025-02-21 16:51:22 +0100

    Update application.py

--- Python/M3uToFreebox/src/m3utofreebox/application.py
+++ Python/M3uToFreebox/src/m3utofreebox/application.py
@@ -1,9 +1,8 @@
 # -*-coding:Utf-8 -*
 
 from warnings import deprecated
+import importlib
 
-import requests
-import tqdm
 
 from logger import logger_config
 from common import file_size_utils
@@ -17,108 +16,16 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from main_view import M3uToFreeboxMainView
-import importlib
-
-import io
-
-import urllib.request
-import urllib.error
-
-from urllib.error import URLError, HTTPError, ContentTooShortError
 
 
 class M3uToFreeboxApplication:
     """Application"""
 
-    def __init__(self, mainview: "M3uToFreeboxMainView") -> None:
+    def __init__(self, main_view: "M3uToFreeboxMainView") -> None:
 
         self._m3u_library: m3u.M3uEntriesLibrary = m3u.M3uEntriesLibrary()
 
-        self._main_view: "M3uToFreeboxMainView" = mainview
-
-    def download_file_blocking_without_progress(
-        self, file_destination_full_path: str, m3u_entry: m3u.M3uEntry, filename: str
-    ) -> None:
-        """download_file_blocking_without_progress"""
-        logger_config.print_and_log_info("Start download of " + file_destination_full_path)
-        url_open = urllib.request.urlopen(m3u_entry.link)
-        meta = url_open.info()
-        logger_config.print_and_log_info(f"File to download size {url_open.length}")
-        logger_config.print_and_log_info(f"File to download meta content length {meta.get('Content-Length')}")
-
-        try:
-            urlretrieve_result = urllib.request.urlretrieve(m3u_entry.link, file_destination_full_path)
-        except HTTPError as e:
-            print(e)
-            # logger_config.print_and_log_info(e)
-        except ContentTooShortError as e:
-            print(e)
-            # logger_config.print_and_log_info(e)
-        except URLError as e:
-            print(e)
-            # logger_config.print_and_log_info(e)
-        except OSError as e:
-            print(e)
-            # logger_config.print_and_log_info(e)
-        except Exception as e:
-            print(e)
-            # logger_config.print_and_log_info(e)
-
-        logger_config.print_and_log_info(f"Download ended. urlretrieve_result {urlretrieve_result}")
-
-        # self.download_tqdm(m3u_entry.link, file_destination_full_path)
-
-    def download_urllib_request_with_progress(self, url: str, filename: str) -> None:
-        """download_urllib_request_with_progress"""
-        with urllib.request.urlopen(url) as response:
-            length = response.getheader("content-length")
-            block_size = 1000000  # default value
-
-            if length:
-                length = int(length)
-                block_size = max(4096, length // 20)
-
-            print("UrlLib len, blocksize: ", length, block_size)
-
-            buffer_all = io.BytesIO()
-            size = 0
-            while True:
-                buffer_now = response.read(block_size)
-                if not buffer_now:
-                    break
-                buffer_all.write(buffer_now)
-                size += len(buffer_now)
-                if length:
-                    Percent = int((size / length) * 100)
-                    print(f"download: {Percent}% {url}")
-
-            print("Buffer All len:", len(buffer_all.getvalue()))
-
-        with open(filename) as f:  ## Excel File
-            print(type(f))  ## Open file is TextIOWrapper
-            bw = io.TextIOWrapper(buffer_all)  ## Conversion to TextIOWrapper
-            print(type(bw))  ## Just to confirm
-
-    def download_tqdm(self, url: str, filename: str) -> None:
-        """download_tqdm"""
-        with open(filename, "wb") as f:
-            with requests.get(url, stream=True) as r:
-                r.raise_for_status()
-                total = int(r.headers.get("content-length", 0))
-
-                # tqdm has many interesting parameters. Feel free to experiment!
-                tqdm_params = {
-                    "desc": url,
-                    "total": total,
-                    "miniters": 1,
-                    "unit": "B",
-                    "unit_scale": True,
-                    "unit_divisor": 1024,
-                }
-                with tqdm.tqdm(**tqdm_params) as pb:
-                    for chunk in r.iter_content(chunk_size=8192):
-                        pb.update(len(chunk))
-                        f.write(chunk)
+        self._main_view: "M3uToFreeboxMainView" = main_view
 
     def get_to_be_downloaded_movie_infos_by_id_str(self, destination_directory: str, m3u_entry_id: str):
         m3u_entry_id_int = int(m3u_entry_id)
@@ -130,38 +37,6 @@ class M3uToFreeboxApplication:
             return m3u_entry.link, file_destination_full_path
         else:
             return None, None
-
-    def download_movie_file_by_id_str(self, destination_directory: str, m3u_entry_id: str) -> None:
-        """download_movie_file_by_id_str"""
-        m3u_entry_id_int = int(m3u_entry_id)
-        self.download_movie_file_by_id(destination_directory, m3u_entry_id_int)
-
-    def download_movie_file_by_id(self, destination_directory: str, m3u_entry_id: int) -> None:
-        """download_movie_file_by_id"""
-        m3u_entry: m3u.M3uEntry = self.m3u_library.get_m3u_entry_by_id(m3u_entry_id)
-        if m3u_entry.can_be_downloaded():
-            file_destination_full_path = (
-                destination_directory + "\\" + m3u_entry.title_as_valid_file_name + m3u_entry.file_extension
-            )
-
-            with open(file_destination_full_path, "wb") as f:
-                # Get Response From URL
-                response = requests.get(m3u_entry.link, stream=True, timeout=5)
-                # Find Total Download Size
-                total_length = response.headers.get("content-length")
-                logger_config.print_and_log_info(f"total_length: {total_length}")
-                # Number Of Iterations To Write To The File
-                # self.download_urllib_request_with_progress(m3u_entry.link, file_destination_full_path)
-                # chunk_size = 4096
-
-                self.download_file_blocking_without_progress(
-                    file_destination_full_path, m3u_entry, file_destination_full_path
-                )
-
-            logger_config.print_and_log_info("Download ended")
-
-        else:
-            logger_config.print_and_log_error(str(m3u_entry) + " cannot be downloaded")
 
     @deprecated("Just for tests")
     def load_fake(self, m3u_entry_id_str: str) -> bool:
