14ed7c10521c0b1196aed9d1e24443cb8d4f720b
yanndanielou <yanndanielou@users.noreply.github.com>
2025-02-25 23:51:50 +0100

    Update TransformExcelToCsvFiles.py

--- Python/TransformExcelToCsvFiles/TransformExcelToCsvFiles.py
+++ Python/TransformExcelToCsvFiles/TransformExcelToCsvFiles.py
@@ -16,7 +16,7 @@ import logging
 import os
 import sys
 
-import param;
+import param
 
 # To get line number for logs
 from inspect import currentframe, getframeinfo
@@ -34,138 +34,69 @@ import getopt
 # Regex
 import re
 
-# param
-end_line_character_in_text_file = "\n"
-logger_level = logging.INFO
+from logger import logger_config
 
 # wordContainingUnderscoreToTranformRegexCompiledPattern = re.compile(wordContainingUnderscoreToTranformRegexPatternAsString)
 
 
-def printAndLogCriticalAndKill(toPrintAndLog):
-    log_timestamp = time.asctime(time.localtime(time.time()))
-
-    previous_stack = inspect.stack(1)[1]
-    line_number = previous_stack.lineno
-
-    print(log_timestamp + '\t' + "line#" +
-          str(line_number) + '\t' + toPrintAndLog)
-    logging.critical("line#" + str(line_number) + '\t' + toPrintAndLog)
-    sys.exit()
+def transform_text_to_valid_excel_date(initial_text: str) -> str:
+    transformed_text: str = initial_text.replace("UTC+1", "")
+    transformed_text = transformed_text.replace("UTC+2", "")
+    transformed_text = transformed_text.replace("à ", "")
+    return transformed_text
 
 
-def print_and_log_info(toPrintAndLog):
-
-    log_timestamp = time.asctime(time.localtime(time.time()))
-
-    previous_stack = inspect.stack(1)[1]
-    line_number = previous_stack.lineno
-
-    print(log_timestamp + '\t' + "line#" +
-          str(line_number) + '\t' + str() + toPrintAndLog)
-    logging.info("line#" + str(line_number) + '\t' + toPrintAndLog)
-
-
-def printAndLogWarning(toPrintAndLog):
-    log_timestamp = time.asctime(time.localtime(time.time()))
-
-    previous_stack = inspect.stack(1)[1]
-    line_number = previous_stack.lineno
-
-    print(log_timestamp + '\t' + "line#" +
-          str(line_number) + '\t' + toPrintAndLog)
-    logging.warning("line#" + str(line_number) + '\t' + toPrintAndLog)
-
-
-def printAndLogError(toPrintAndLog):
-    log_timestamp = time.asctime(time.localtime(time.time()))
-
-    previous_stack = inspect.stack(1)[1]
-    line_number = previous_stack.lineno
-
-    print(log_timestamp + '\t' + "!!ERROR!!")
-    print(log_timestamp + '\t' "line#" + str(line_number))
-    print(log_timestamp + '\t' + toPrintAndLog)
-    logging.error("line#" + str(line_number) + '\t' + toPrintAndLog)
-
-
-def configureLogger(log_file_name):
-    logger_directory = "logs"
-
-    if not os.path.exists(logger_directory):
-        os.makedirs(logger_directory)
-
-    print(time.asctime(time.localtime(time.time())) +
-          '\t' + "Logger level:" + str(logger_level))
-
-    logging.basicConfig(level=logger_level,
-                        format='%(asctime)s %(levelname)-8s %(message)s',
-                        datefmt='%a, %d %b %Y %H:%M:%S',
-                        filename=logger_directory + '\\' + log_file_name,
-                        filemode='w')
-    # logging.debug
-    # logging.info
-    # logging.warning
-    # logging.error
-    # logging.critical
-
-
-
-def transformTextToValidExcelDate(initialText):
-    transformedText = initialText.replace("UTC+1","")
-    transformedText = transformedText.replace("UTC+2","")
-    transformedText = transformedText.replace("à ","")
-    return transformedText
-
-
-def getListOfExcelFilesNames():
-    dir_path = r'*.xls*'
+def get_list_of_excel_files_names() -> list[str]:
+    dir_path = r"*.xls*"
     res = glob.glob(dir_path)
     return res
 
-def transformExcelFileToCsvFiles(excelFileNameWithExtension):
 
-    print_and_log_info("Load workbook:" + excelFileNameWithExtension)
-    workbook = load_workbook(filename=excelFileNameWithExtension)
+def transform_excel_file_to_csv_files(excel_file_name_with_extension: str) -> None:
 
-    excelFileNameWithoutExtension = excelFileNameWithExtension.split(".")[0]
+    logger_config.print_and_log_info("Load workbook:" + excel_file_name_with_extension)
+    workbook = load_workbook(filename=excel_file_name_with_extension)
 
-    workbook_sheetnames = workbook.sheetnames 
-    print_and_log_info("Workbook:" + excelFileNameWithoutExtension + " has following sheets:" + str(workbook_sheetnames))
-    
-    if not os.path.exists(excelFileNameWithoutExtension):
-        print_and_log_info("Create folder " + excelFileNameWithoutExtension) 
-        os.makedirs(excelFileNameWithoutExtension)
+    excel_file_name_without_extension = excel_file_name_with_extension.split(".")[0]
+
+    workbook_sheetnames = workbook.sheetnames
+    logger_config.print_and_log_info(
+        "Workbook:" + excel_file_name_without_extension + " has following sheets:" + str(workbook_sheetnames)
+    )
+
+    if not os.path.exists(excel_file_name_without_extension):
+        logger_config.print_and_log_info("Create folder " + excel_file_name_without_extension)
+        os.makedirs(excel_file_name_without_extension)
 
     for workbookSheetName in workbook_sheetnames:
-        read_file = pandas.read_excel (excelFileNameWithExtension, sheet_name=workbookSheetName)
-        output_csv_file = excelFileNameWithoutExtension + '/' + workbookSheetName + '.csv'
-        print_and_log_info("Extract sheet " + workbookSheetName + " to " + output_csv_file)
-        read_file.to_csv (output_csv_file, sep=param.csv_separator, index = None, header=True)
+        read_file = pandas.read_excel(excel_file_name_with_extension, sheet_name=workbookSheetName)
+        output_csv_file = excel_file_name_without_extension + "/" + workbookSheetName + ".csv"
+        logger_config.print_and_log_info("Extract sheet " + workbookSheetName + " to " + output_csv_file)
+        read_file.to_csv(output_csv_file, sep=param.csv_separator, index=None, header=True)
 
 
-def transformAllExcelFilesToCsvFiles(argv):
+def transform_all_excel_files_to_csv_files() -> None:
 
-    listOfExcelFileNames = getListOfExcelFilesNames()
+    list_of_excel_files_names = get_list_of_excel_files_names()
 
-    print_and_log_info("Number of excel files found:" + str(len(listOfExcelFileNames)) + " : " + str(listOfExcelFileNames))
- 
-    for excelFileName in listOfExcelFileNames:
-        transformExcelFileToCsvFiles(excelFileName)
-    
-  
+    logger_config.print_and_log_info(
+        "Number of excel files found:" + str(len(list_of_excel_files_names)) + " : " + str(list_of_excel_files_names)
+    )
+
+    for excel_file_name in list_of_excel_files_names:
+        transform_excel_file_to_csv_files(excel_file_name)
 
 
-def main(argv):
-    log_file_name = 'TransformExcelToCsvFiles' + '_' + \
-        str(random.randrange(100000)) + ".log"
+def main() -> None:
+    log_file_name = "TransformExcelToCsvFiles" + "_" + str(random.randrange(100000)) + ".log"
 
     # log_file_name = 'TransformCodeToCamelCase' + "." +  str(random.randrange(10000)) + ".log"
-    configureLogger(log_file_name)
+    logger_config.configure_logger_with_random_log_file_suffix(log_file_name)
 
-    print_and_log_info('Start application. Log file name: ' + log_file_name)
-    transformAllExcelFilesToCsvFiles(argv)
-    print_and_log_info('End application')
+    logger_config.print_and_log_info("Start application. Log file name: " + log_file_name)
+    transform_all_excel_files_to_csv_files()
+    logger_config.print_and_log_info("End application")
 
 
-if __name__ == '__main__':
-    main(sys.argv[1:])
+if __name__ == "__main__":
+    main()
