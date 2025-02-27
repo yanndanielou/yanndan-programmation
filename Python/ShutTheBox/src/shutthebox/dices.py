eb4b21471cb9575db3c5872bfd2e096e35670a6f
yanndanielou <yanndanielou@users.noreply.github.com>
2025-02-27 22:36:21 +0100

    dices

--- Python/ShutTheBox/src/shutthebox/dices.py
+++ Python/ShutTheBox/src/shutthebox/dices.py
@@ -1,27 +1,92 @@
 # -*-coding:Utf-8 -*
 """ Class that handles dices """
 
+from dataclasses import dataclass, fields, field
 from typing import List, Tuple, cast, Dict
 import itertools
 
-#from common import singleton
 
-#class Dices(metaclass=singleton.Singleton):
+# from common import singleton
+
+
+@dataclass
+class DicesThrownCombinationsOfOneSum:
+    _sum: int
+    _total_number_of_combinations_with_all_sums: int
+    _combinations: list[int] = field(default_factory=list)
+
+    def add_combination(self, combination):
+        self._combinations.append(combination)
+
+    def get_chances(self) -> float:
+        return (
+            len(self._combinations) / self._total_number_of_combinations_with_all_sums
+        )
+
+
+class DicesThrownCombinationsResults:
+
+    def __init__(self, number_of_dices: int):
+        self._number_of_dices: int = number_of_dices
+        self._all_combinations: List[Tuple[int, ...]] = (
+            Dices.get_several_standard_dices_all_combinations(
+                number_of_dices=number_of_dices
+            )
+        )
+
+        self._all_combinations_by_sum: dict[int, DicesThrownCombinationsOfOneSum] = (
+            dict()
+        )
+
+        for combination in self._all_combinations:
+            sum_dices: int = sum(combination)
+
+            if sum_dices not in self._all_combinations_by_sum:
+                self._all_combinations_by_sum[sum_dices] = (
+                    DicesThrownCombinationsOfOneSum(
+                        _sum=sum_dices,
+                        _total_number_of_combinations_with_all_sums=len(
+                            self._all_combinations
+                        ),
+                    )
+                )
+
+            self._all_combinations_by_sum[sum_dices].add_combination(combination)
+
+
+@dataclass
+class Dice:
+    _sides_values: list[int] = field(default_factory=lambda: list(range(1, 7)))
+
+    @property
+    def sides_values(self):
+        return self._sides_values
+
+
+# class Dices(metaclass=singleton.Singleton):
 class Dices:
-
-    """ Dices """
-
-    @staticmethod
-    def get_two_dices_all_combinations_by_sum() -> dict[int,list[Tuple[int, int]]]:
-        return cast(Dict[int, List[Tuple[int, int]]], Dices.get_several_dices_all_combinations_by_sum(2))
+    """Dices"""
 
     @staticmethod
-    def get_several_dices_all_combinations_by_sum(number_of_dices:int) -> dict[int,list[Tuple[int, ...]]]:
-        several_dices_all_combinations = Dices.get_several_dices_all_combinations(number_of_dices=number_of_dices)
-        all_combinations_by_sum:dict[int,list[Tuple[int, ...]]] = dict()
+    def get_two_dices_all_combinations_by_sum() -> dict[int, list[Tuple[int, int]]]:
+        return cast(
+            Dict[int, List[Tuple[int, int]]],
+            Dices.get_several_dices_all_combinations_by_sum(2),
+        )
+
+    @staticmethod
+    def get_several_dices_all_combinations_by_sum(
+        number_of_dices: int,
+    ) -> dict[int, list[Tuple[int, ...]]]:
+        several_dices_all_combinations = (
+            Dices.get_several_standard_dices_all_combinations(
+                number_of_dices=number_of_dices
+            )
+        )
+        all_combinations_by_sum: dict[int, list[Tuple[int, ...]]] = dict()
 
         for combination in several_dices_all_combinations:
-            sum_dices:int =  sum(combination)
+            sum_dices: int = sum(combination)
 
             if sum_dices in all_combinations_by_sum:
                 all_combinations_by_sum[sum_dices] += [combination]
@@ -31,12 +96,28 @@ class Dices:
 
         return all_combinations_by_sum
 
-
     @staticmethod
-    def get_several_dices_all_combinations(number_of_dices:int) -> List[Tuple[int, ...]]:
-        one_dice_combinations = list(range(1,7))
+    def get_several_standard_dices_all_combinations(
+        number_of_dices: int,
+    ) -> List[Tuple[int, ...]]:
+        one_dice_combinations = list(range(1, 7))
 
-        #Le cast est une façon explicite de dire à MyPy : "je sais mieux que toi dans ce cas précis". Il informe MyPy que nous avons bien une List[Tuple[int, int]] # pylint: disable=line-too-long
-        all_combinations: List[Tuple[int, ...]] = list(itertools.product(one_dice_combinations, repeat=number_of_dices))
+        all_combinations: List[Tuple[int, ...]] = list(
+            itertools.product(one_dice_combinations, repeat=number_of_dices)
+        )
 
         return all_combinations
+
+    @staticmethod
+    def get_dices_all_combinations(dices: list[Dice]) -> List[Tuple[int, ...]]:
+        """
+        Calculates all possible combinations of the given list of Dice.
+
+        Args:
+            dices (list[Dice]): A list of Dice objects.
+
+        Returns:
+            list[tuple[int, ...]]: A list of tuples, where each tuple represents a combination of the dice rolls.
+        """
+        side_values = [dice.sides_values for dice in dices]
+        return list(map(tuple, itertools.product(*side_values)))
--- Python/ShutTheBox/src/test/test_dices.py
+++ Python/ShutTheBox/src/test/test_dices.py
@@ -6,38 +6,67 @@ import os
 import sys
 
 
-
 from typing import List, Tuple
 
 from shutthebox import dices
 
-class TestCombinaisons:
-    """ TestApplicationWithoutHmi """
+
+class TestCombinationsUsingDiceClass:
+
+    class TestThreeSidesDices:
+        class TestOneThreeSideDice:
+            def test_all_combinations_with_occurrences(self) -> None:
+                one_dice = [dices.Dice(_sides_values=[1, 2, 3])]
+                all_combinations = dices.Dices.get_dices_all_combinations(one_dice)
+                assert len(all_combinations) == 3
+
+    class TestStandardDices:
+
+        class TestOneStandardDices:
+
+            def test_all_combinations_with_occurrences(self) -> None:
+                one_dice = [dices.Dice()]
+                all_combinations = dices.Dices.get_dices_all_combinations(one_dice)
+                assert len(all_combinations) == 6
+
+        class TestTwoStandardDices:
+
+            def test_all_combinations_with_occurrences(self) -> None:
+                all_dices = [dices.Dice(), dices.Dice()]
+                all_combinations = dices.Dices.get_dices_all_combinations(all_dices)
+                assert len(all_combinations) == 36
+
+
+class TestCombinations:
+    """TestApplicationWithoutHmi"""
 
     class TestTwoDices:
 
-        def test_get_two_dices_all_combinaisons_with_occurences(self)->None:
-            """ Number of results """
-            two_dices_all_combinaisons_with_occurences:dict[int,list[Tuple[int, int]]] = dices.Dices.get_two_dices_all_combinations_by_sum()
-            
-            #assert two_dices_all_combinaisons_with_occurences[1] is None
-            assert two_dices_all_combinaisons_with_occurences[2] == [(1,1)]
+        def test_get_two_dices_all_combinaisons_with_occurences(self) -> None:
+            """Number of results"""
+            two_dices_all_combinaisons_with_occurences: dict[
+                int, list[Tuple[int, int]]
+            ] = dices.Dices.get_two_dices_all_combinations_by_sum()
+
+            # assert two_dices_all_combinaisons_with_occurences[1] is None
+            assert two_dices_all_combinaisons_with_occurences[2] == [(1, 1)]
             assert len(two_dices_all_combinaisons_with_occurences[2]) == 1
             assert len(two_dices_all_combinaisons_with_occurences[3]) == 2
-            assert two_dices_all_combinaisons_with_occurences[12] == [(6,6)]
+            assert two_dices_all_combinaisons_with_occurences[12] == [(6, 6)]
             assert len(two_dices_all_combinaisons_with_occurences) == 11
-
-
 
     class TestThreeDices:
         number_of_dices = 3
 
-        def test_get_several_dices_all_combinaisons_by_sum(self)->None:
-            """ Number of results """
-            dices_all_combinaisons_with_occurences:dict[int,list[Tuple[int, ...]]] = dices.Dices.get_several_dices_all_combinations_by_sum(self.number_of_dices)
+        def test_get_several_dices_all_combinaisons_by_sum(self) -> None:
+            """Number of results"""
+            dices_all_combinaisons_with_occurences: dict[int, list[Tuple[int, ...]]] = (
+                dices.Dices.get_several_dices_all_combinations_by_sum(
+                    self.number_of_dices
+                )
+            )
 
-            #assert two_dices_all_combinaisons_with_occurences[1] is None
-            #assert two_dices_all_combinaisons_with_occurences[2] is None
+            # assert two_dices_all_combinaisons_with_occurences[1] is None
+            # assert two_dices_all_combinaisons_with_occurences[2] is None
             assert len(dices_all_combinaisons_with_occurences[3]) == 1
-            assert dices_all_combinaisons_with_occurences[3] == [(1,1,1)]
-
+            assert dices_all_combinaisons_with_occurences[3] == [(1, 1, 1)]
