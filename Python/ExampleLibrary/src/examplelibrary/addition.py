8bd9a57d3ca4aa718f07b78ab750e435e4688d5c
yanndanielou <yanndanielou@users.noreply.github.com>
2025-02-28 14:55:45 +0100

    fix mypy

--- Python/ExampleLibrary/src/examplelibrary/addition.py
+++ Python/ExampleLibrary/src/examplelibrary/addition.py
@@ -5,10 +5,16 @@ from logger import logger_config
 
 def add_custom(a: int, b: int) -> int:
     """Returns the sum of two integers."""
-    res = a + b
+    res = add_custom_without_log(a, b)
     logger_config.print_and_log_info(f"add_custom for {a} and {b} returns  {res} ")
     return res
 
 
-def no_return_type(a: int):
+def add_custom_without_log(a, b):
+    """Returns the sum of two integers."""
+    res = a + b
+    return res
+
+
+def no_return_type(a):
     return a + 1
