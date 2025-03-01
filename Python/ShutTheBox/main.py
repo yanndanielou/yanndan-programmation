3593f6337dc2bcc5bb6cc455cf261dc15c304318
yanndanielou <yanndanielou@users.noreply.github.com>
2025-03-01 20:37:09 +0100

    Update main.py

--- Python/ShutTheBox/main.py
+++ Python/ShutTheBox/main.py
@@ -1,7 +1,13 @@
 # -*-coding:Utf-8 -*
 """ Main """
+import tkinter as tk
+from tkinter import ttk
+
 from logger import logger_config
+
 from shutthebox.application import Application, SimulationRequest
+from shutthebox.dices import Dice
+from shutthebox.gui import TreeViewApp
 
 
 def main() -> None:
@@ -10,7 +16,12 @@ def main() -> None:
     with logger_config.stopwatch_with_label("Application duration"):
         logger_config.configure_logger_with_random_log_file_suffix("CloseTheBox")
 
-        simulation = Application().run()
+        simulation_request = SimulationRequest([Dice()], list(range(1, 7)))
+        simulation = Application().run(simulation_request)
+
+        root = tk.Tk()
+        app = TreeViewApp(root, simulation.complete_simulation_result.initial_situation)
+        root.mainloop()
 
         logger_config.print_and_log_info("End. Nominal end of application")
 
