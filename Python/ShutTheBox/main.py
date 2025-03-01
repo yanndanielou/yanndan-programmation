b0a5755d626189cffeed72bb759b27700dce11cb
yanndanielou <yanndanielou@users.noreply.github.com>
2025-03-01 09:12:27 +0100

    Update main.py

--- Python/ShutTheBox/main.py
+++ Python/ShutTheBox/main.py
@@ -1,23 +1,19 @@
 # -*-coding:Utf-8 -*
 """ Main """
 from logger import logger_config
-from shutthebox.application import Application
+from shutthebox.application import Application, SimulationRequest
 
 
-def main()->None:
-    """ Main function """
+def main() -> None:
+    """Main function"""
 
     with logger_config.stopwatch_with_label("Application duration"):
         logger_config.configure_logger_with_random_log_file_suffix("CloseTheBox")
 
-        logger_config.print_and_log_info('Start application')
-        application:Application = Application()
-        application.run()
-
-
-
+        simulation = Application().run()
 
         logger_config.print_and_log_info("End. Nominal end of application")
+
 
 if __name__ == "__main__":
     # sys.argv[1:]
