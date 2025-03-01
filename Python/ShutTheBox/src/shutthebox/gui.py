efe796ca713fc6e42dc23f2b8f18b8004bd40845
yanndanielou <yanndanielou@users.noreply.github.com>
2025-03-01 21:08:22 +0100

    Update gui.py

--- Python/ShutTheBox/src/shutthebox/gui.py
+++ Python/ShutTheBox/src/shutthebox/gui.py
@@ -21,16 +21,14 @@ class TreeViewApp:
         self.tree.heading("#0", text="Game Turns")
 
         # Populate tree
-        self.populate_tree("Initial Situation", initial_situation.next_turns)
-
-        logger_config.print_and_log_info("Gui completed")
+        self.populate_tree(f"Initial Situation: Opened Hatches={initial_situation._initial_opened_hatches}", initial_situation.next_turns)
 
     def populate_tree(self, parent_name: str, turns: List["OneTurn"], parent_id: str = "") -> None:
         """Recursively add turns to the tree"""
         parent_node = self.tree.insert(parent_id, "end", text=parent_name)
 
         for i, turn in enumerate(turns):
-            dices_step_text = f"Dices: Sum={turn._dices_result_action.dices_sum}, Odds={turn._dices_result_action._dices_sum_odds:.2f}"
+            dices_step_text = f"Dices: Sum={turn._dices_result_action.dices_sum}, Odds={turn._dices_result_action._dices_sum_odds * 100:.2f}%"
             dices_step_node = self.tree.insert(parent_node, "end", text=dices_step_text)
 
             close_hatches_text = f"Close Hatches: Opened={turn._close_hatches_action._opened_hatches_before_turn}, Closed={turn._close_hatches_action._closed_hatches_during_turn}"
