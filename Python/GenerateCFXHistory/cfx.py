6129b0ef174142bbe47dd90d7d8a1f874388ce53
yanndanielou <yanndanielou@users.noreply.github.com>
2025-03-07 07:30:47 +0100

    Update cfx.py

--- Python/GenerateCFXHistory/cfx.py
+++ Python/GenerateCFXHistory/cfx.py
@@ -5,11 +5,25 @@ from logger import logger_config
 
 import utils
 
+from enum import Enum, auto
+
+
+class State(Enum):
+    Submitted = auto()
+    Analysed = auto()
+    Assigned = auto()
+    Resolved = auto()
+    Rejected = auto()
+    Postponed = auto()
+    Verified = auto()
+    Validated = auto()
+    Closed = auto()
+
 
 class ChampFXEntry:
     def __init__(self, row):
         self.cfx_id = row["CFXID"]
-        self.state = row["State"]
+        self.state: State = State[(row["State"])]
         self.submit_date = utils.convert_champfx_extract_date(row["SubmitDate"])
         self.analysis_date = utils.convert_champfx_extract_date(row["HistoryOfLastAction.AnalysisDate"])
         self.solution_date = utils.convert_champfx_extract_date(row["HistoryOfLastAction.SolutionDate"])
