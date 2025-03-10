a3fea37ba72cf60390503d5ccd923ba477f5a003
yanndanielou <yanndanielou@users.noreply.github.com>
2025-03-10 18:42:36 +0100

    Handle datetime for json dump

--- Python/Common/src/common/json_encoders.py
+++ Python/Common/src/common/json_encoders.py
@@ -1,23 +1,30 @@
 # -*-coding:Utf-8 -*
-""" json encoders """
+"""json encoders"""
 
 import json
 from json import JSONEncoder
 from typing import Any
+from datetime import datetime
 
 from common import singleton
 
+
 class ListOfObjectsEncoder(JSONEncoder):
-    def default(self, o:Any)-> (list | Any):
+    def default(self, o: Any) -> list | Any:
         if isinstance(o, set):
             return list(o)
-        #return json.JSONEncoder.default(self, obj)
+        if isinstance(o, datetime):
+            return str(o)
+        # return json.JSONEncoder.default(self, obj)
         return o.__dict__
+
 
 class JsonEncodersUtils(metaclass=singleton.Singleton):
 
     @staticmethod
-    def serialize_list_objects_in_json(list_objects:list[Any], json_file_full_path:str) -> None:
+    def serialize_list_objects_in_json(
+        list_objects: list[Any], json_file_full_path: str
+    ) -> None:
         json_file = open(json_file_full_path, "w", encoding="utf-8")
 
         result_json_dump = json.dumps(list_objects, indent=4, cls=ListOfObjectsEncoder)
