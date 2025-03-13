b1cf69d151447eab99f233a23acb8f00b08b8738
yanndanielou <yanndanielou@users.noreply.github.com>
2025-03-13 21:51:27 +0100

    Update sandbox_cfx_excel.py

--- Python/GenerateCFXHistory/sandbox_cfx_excel.py
+++ Python/GenerateCFXHistory/sandbox_cfx_excel.py
@@ -13,9 +13,36 @@ chrome_options = selenium.webdriver.chrome.options.Options()
 # chrome_options.headless = True
 driver_service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=driver_service, options=chrome_options)
-# login_url = f"https://champweb.siemens.net/cqweb/restapi/01_CHAMP/CFX?format=HTML&loginId=AD001%5Cfr232487&password\=Zzeerrttyy9."
-# driver.get(login_url)
-ccb_nexteo_query_url = f"https://champweb.siemens.net/cqweb/restapi/01_CHAMP/CFX/QUERY/Personal%20Queries/NEXTEO/ATS%2B%20%26%20NExTEO%20pour%20CMC?format=HTML&loginId=AD001%5Cfr232487&password\=Zzeerrttyy9.&noframes=true"
+connexion_param_champfx_login = "AD001%5Cfr232487"
+connexion_param_champfx_password = "Zzeerrttyy9."
+login_url = f"https://champweb.siemens.net/cqweb/restapi/01_CHAMP/CFX?format=HTML&loginId={connexion_param_champfx_login}&password={connexion_param_champfx_password}"
+#https://champweb.siemens.net/cqweb/restapi/01_CHAMP/CFX?format=HTML&loginId=AD001%5Cfr232487&password=Zzeerrttyy9."
+driver.get(login_url)
+ccb_nexteo_query_url = f"https://champweb.siemens.net/cqweb/restapi/01_CHAMP/CFX/QUERY/Personal%20Queries/NEXTEO/ATS%2B%20%26%20NExTEO%20pour%20CMC?format=HTML&loginId={connexion_param_champfx_login}&password={connexion_param_champfx_password}&noframes=true"
 driver.get(ccb_nexteo_query_url)
-export_button = driver.find_element(By.ID, "dijit_form_ComboButton_1_label")
-export_button.click()
+
+
+with open("D:\\temp\\before_arrow_to_acces_export_click.txt", "w", encoding='utf-8') as text_dump_file:
+    text_dump_file.write(driver.page_source)
+    
+arrow_to_acces_export = driver.find_element(By.ID, "dijit_form_ComboButton_1_arrow")
+arrow_to_acces_export.click()
+
+with open("D:\\temp\\after_arrow_to_acces_export_click.txt", "w", encoding='utf-8') as text_dump_file:
+    text_dump_file.write(driver.page_source)
+
+excel_export_button = driver.find_element(By.XPATH, "//td[contains(text(),'Exporter vers un tableur Excel')]")
+excel_export_button.click()
+
+
+export_button_by_id = driver.find_element(By.ID, "dijit_form_ComboButton_1_label")
+export_button_by_id.click()
+
+excel_export_button_by_id = driver.find_element(By.ID, "dijit_MenuItem_42_text")
+excel_export_button_by_id.click()
+
+export_button_by_text = driver.find_element(By.XPATH, "//td[contains(text(),'Exporter')]")
+
+page_html = driver.page_source
+with open("D:\\temp\\text_dump_file.txt", "w", encoding='utf-8') as text_dump_file:
+    text_dump_file.write(page_html)
