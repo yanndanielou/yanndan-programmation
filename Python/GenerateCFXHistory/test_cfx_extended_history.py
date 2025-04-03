25de1742ef56e1df2026accf813aaacf9cf9a503
yanndanielou <yanndanielou@users.noreply.github.com>
2025-04-03 20:11:36 +0200

    Update test_cfx_extended_history.py

---
+++ Python/GenerateCFXHistory/test_cfx_extended_history.py
@@ -0,0 +1,66 @@
+import pytest
+
+import cfx_extended_history
+
+
+class TestDecodeTime:
+
+    def tests_weird_dates_found_in_cfx(self):
+        assert cfx_extended_history.decode_time("2023-10-01 00:05:13 -718:00") is not None
+
+
+class TestAllCFXCompleteHistoryExport:
+
+    def test_full_file(self):
+        pass
+        # cfx_extended_history.AllCFXCompleteHistoryExport.parse_full_complete_extended_histories_text_file
+
+
+class TestParseExtendedHistory:
+
+    def test_simple_case1(self):
+        extended_history_raw_text = """====START====
+Time           :    2021-04-14 11:13:19 +02:00
+Schema Rev     :    105
+User Name      :    SERDJANIAN, GREGOR
+User Login     :    z004686c
+User Groups    :    Siemens    FR_NEXTEO    Everyone
+Action         :    Close
+State          :    Closed
+==Fields==
+State            (9:6)
+    Old :    Validated
+    New :    Closed
+
+====END===="""
+        assert cfx_extended_history.parse_history(cfx_id="CFX00123", extended_history_text=extended_history_raw_text)
+
+    def test_simple_case2(self):
+        extended_history_raw_text = """====START====
+Time           :    2021-04-14 11:13:19 +02:00
+Schema Rev     :    105
+User Name      :    SERDJANIAN, GREGOR
+User Login     :    z004686c
+User Groups    :    Siemens    FR_NEXTEO    Everyone
+Action         :    Close
+State          :    Closed
+==Fields==
+State            (9:6)
+    Old :    Validated
+    New :    Closed
+
+====END===="""
+
+        cfx_complete_history = cfx_extended_history.parse_history(cfx_id="CFX00123", extended_history_text=extended_history_raw_text)
+
+        parsed_history_parsed = cfx_complete_history.history_elements
+        assert len(parsed_history_parsed) == 1
+        cfx_history_element = parsed_history_parsed[0]
+        assert cfx_history_element.action == "Close"
+        assert cfx_history_element.state == "Closed"
+
+        assert len(cfx_history_element.fields) == 1
+        first_field = cfx_history_element.fields[0]
+        assert first_field.field_id == "State"
+        assert first_field.old_state == "Validated"
+        assert first_field.new_state == "Closed"
