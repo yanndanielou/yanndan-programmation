54599a71d786b2d5bccaaa789ee81f3d341c7e47
yanndanielou <yanndanielou@users.noreply.github.com>
2025-04-07 13:46:03 +0200

    Create test_decode_messages.py

---
+++ Python/ProfibusLogAnalyzis/test_decode_messages.py
@@ -0,0 +1,29 @@
+import pytest
+
+import datetime
+
+import decode_message
+
+# fmt: on
+
+# time_field_value: int, time_offset_value: int, decade_field_value: int, day_on_decade_field_value: int, expected_datetime: datetime.datetime
+decode_hlf_test_data = [
+    (0, 0, 0, 0, datetime.datetime(year=2000, month=1, day=1, hour=0, minute=0, second=0)),
+    (0, 0, 1, 0, datetime.datetime(year=2010, month=1, day=1, hour=0, minute=0, second=0)),
+    (0, 0, 1, 1, datetime.datetime(year=2010, month=1, day=2, hour=0, minute=0, second=0)),
+    (0, 0, 1, 40, datetime.datetime(year=2010, month=2, day=10, hour=0, minute=0, second=0)),
+]
+# fmt: off
+
+
+class TestDecodeHlfFieldsToDatetime:
+    def test_0(self):
+        decoded_datetime = decode_message.decode_hlf_fields_to_datetime(time_field_value=0, time_offset_value=0, decade_field_value=0, day_on_decade_field_value=0)
+        assert decode_message.decode_hlf_fields_to_datetime(time_field_value=0, time_offset_value=0, decade_field_value=0, day_on_decade_field_value=0) == datetime.datetime(
+            year=2000, month=1, day=1, hour=0, minute=0, second=0
+        )
+
+    @pytest.mark.parametrize("time_field_value, time_offset_value,decade_field_value, day_on_decade_field_value, expected_datetime", decode_hlf_test_data)
+    def test_several_data(self, time_field_value: int, time_offset_value: int, decade_field_value: int, day_on_decade_field_value: int, expected_datetime: datetime.datetime):
+        decoded_hlf = decode_message.decode_hlf_fields_to_datetime(time_field_value=time_field_value, time_offset_value=time_offset_value, decade_field_value=decade_field_value, day_on_decade_field_value=day_on_decade_field_value)
+        assert decoded_hlf == expected_datetime, str(decoded_hlf)
