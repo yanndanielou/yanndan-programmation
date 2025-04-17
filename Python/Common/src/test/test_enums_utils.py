830e5711133704b282bf736ad4619620c7c7df5d
yanndanielou <yanndanielou@users.noreply.github.com>
2025-04-17 14:44:40 +0200

    Create test_enums_utils.py

---
+++ Python/Common/src/test/test_enums_utils.py
@@ -0,0 +1,27 @@
+import pytest
+
+from common import enums_utils
+
+from enum import auto
+
+
+class NameBasedEnumExample(enums_utils.NameBasedEnum):
+    A = auto()
+    B = auto()
+
+
+class NameBasedIntEnumExample(enums_utils.NameBasedIntEnum):
+    ONE = auto()
+    TWO = auto()
+
+
+class TestNameBasedReprEnum:
+    def test_repr(self) -> None:
+        assert f"{NameBasedEnumExample.A}" == "A"
+        assert str(NameBasedEnumExample.A) == "A"
+
+
+class TestNameBasedReprIntEnum:
+    def test_repr(self) -> None:
+        assert f"{NameBasedIntEnumExample.ONE}" == "ONE"
+        assert str(NameBasedIntEnumExample.ONE) == "ONE"
