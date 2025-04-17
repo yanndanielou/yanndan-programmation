48f55a9e5872394477c336ea759f3f261965cf72
yanndanielou <yanndanielou@users.noreply.github.com>
2025-04-17 15:57:06 +0200

    Update test_file_name_utils.py

--- Python/Common/src/test/test_file_name_utils.py
+++ Python/Common/src/test/test_file_name_utils.py
@@ -5,9 +5,24 @@ import pytest
 from common import file_name_utils
 
 
-class TestFileExtension():
+class TestFileExtension:
 
     def test_full_path(self) -> None:
-        assert file_name_utils.file_extension_from_full_path("http://vevoxtv.top:2103/movie/412910643GRB/dn5QFp3/24950.mp4") == ".mp4"
-        assert file_name_utils.file_extension_from_full_path("http://vevoxtv.top:2103/series/412910643GRB/dn5QFp3/57996.mkv") == ".mkv"
-        assert file_name_utils.file_extension_from_full_path("http://vevoxtv.top:2103/412910643GRB/dn5QFp3/37744" is not None
+        assert (
+            file_name_utils.file_extension_from_full_path(
+                "http://vevoxtv.top:2103/movie/412910643GRB/dn5QFp3/24950.mp4"
+            )
+            == ".mp4"
+        )
+        assert (
+            file_name_utils.file_extension_from_full_path(
+                "http://vevoxtv.top:2103/series/412910643GRB/dn5QFp3/57996.mkv"
+            )
+            == ".mkv"
+        )
+        assert (
+            file_name_utils.file_extension_from_full_path(
+                "http://vevoxtv.top:2103/412910643GRB/dn5QFp3/37744"
+            )
+            is not None
+        )
