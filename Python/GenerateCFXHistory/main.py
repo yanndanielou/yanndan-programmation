4178afc553396fd2ae2106323681564432ee6889
yanndanielou <yanndanielou@users.noreply.github.com>
2025-05-02 21:58:44 +0200

    Update main.py

--- Python/GenerateCFXHistory/main.py
+++ Python/GenerateCFXHistory/main.py
@@ -14,26 +14,26 @@ def print_gathering_time(champfx_library: cfx.ChampFXLibrary, number_iterations:
 
     for _ in range(1, number_iterations):
         with logger_config.stopwatch_with_label("gather_state_counts_for_each_date no filter"):
-            champfx_library.gather_state_counts_for_each_date(relativedelta.relativedelta(days=10))
+            champfx_library.gather_state_counts_for_each_date(dates_generator=cfx.ConstantIntervalDatesGenerator(time_delta=relativedelta.relativedelta(days=10)))
 
     for _ in range(1, number_iterations):
         with logger_config.stopwatch_with_label("gather_state_counts_for_each_date filter nexteo"):
             champfx_library.gather_state_counts_for_each_date(
-                relativedelta.relativedelta(days=10),
+                dates_generator=cfx.ConstantIntervalDatesGenerator(time_delta=relativedelta.relativedelta(days=10)),
                 cfx_filters=[cfx.ChampFxFilter(field_filters=[cfx.ChampFXFieldFilter(field_name="_cfx_project", field_accepted_values=[cfx.CfxProject.FR_NEXTEO])])],
             )
 
     for _ in range(1, number_iterations):
         with logger_config.stopwatch_with_label("gather_state_counts_for_each_date filter ats+"):
             champfx_library.gather_state_counts_for_each_date(
-                relativedelta.relativedelta(days=10),
+                dates_generator=cfx.ConstantIntervalDatesGenerator(time_delta=relativedelta.relativedelta(days=10)),
                 cfx_filters=[cfx.ChampFxFilter(field_filters=[cfx.ChampFXFieldFilter(field_name="_cfx_project", field_accepted_values=[cfx.CfxProject.ATSP])])],
             )
 
     for _ in range(1, number_iterations):
         with logger_config.stopwatch_with_label("gather_state_counts_for_each_date security relevant"):
             champfx_library.gather_state_counts_for_each_date(
-                relativedelta.relativedelta(days=10),
+                dates_generator=cfx.ConstantIntervalDatesGenerator(time_delta=relativedelta.relativedelta(days=10)),
                 cfx_filters=[
                     cfx.ChampFxFilter(field_filters=[cfx.ChampFXFieldFilter(field_name="_security_relevant", field_accepted_values=[cfx.SecurityRelevant.YES, cfx.SecurityRelevant.MITIGATED])])
                 ],
@@ -42,21 +42,21 @@ def print_gathering_time(champfx_library: cfx.ChampFXLibrary, number_iterations:
     for _ in range(1, number_iterations):
         with logger_config.stopwatch_with_label("gather_state_counts_for_each_date ChampFXRoleDependingOnDateFilter([role.SubSystem.ATS]"):
             champfx_library.gather_state_counts_for_each_date(
-                relativedelta.relativedelta(days=10),
+                dates_generator=cfx.ConstantIntervalDatesGenerator(time_delta=relativedelta.relativedelta(days=10)),
                 cfx_filters=[cfx.ChampFxFilter(role_depending_on_date_filter=cfx.ChampFXRoleDependingOnDateFilter(roles_at_date_allowed=[role.SubSystem.ATS]))],
             )
 
     for _ in range(1, number_iterations):
         with logger_config.stopwatch_with_label("gather_state_counts_for_each_date ChampFXRoleDependingOnDateFilter([role.SubSystem.SW]"):
             champfx_library.gather_state_counts_for_each_date(
-                relativedelta.relativedelta(days=10),
+                dates_generator=cfx.ConstantIntervalDatesGenerator(time_delta=relativedelta.relativedelta(days=10)),
                 cfx_filters=[cfx.ChampFxFilter(role_depending_on_date_filter=cfx.ChampFXRoleDependingOnDateFilter(roles_at_date_allowed=[role.SubSystem.SW]))],
             )
 
     for _ in range(1, number_iterations):
         with logger_config.stopwatch_with_label("gather_state_counts_for_each_date ChampFXRoleDependingOnDateFilter([role.SubSystem.QUALITE]"):
             champfx_library.gather_state_counts_for_each_date(
-                relativedelta.relativedelta(days=10),
+                dates_generator=cfx.ConstantIntervalDatesGenerator(time_delta=relativedelta.relativedelta(days=10)),
                 cfx_filters=[cfx.ChampFxFilter(role_depending_on_date_filter=cfx.ChampFXRoleDependingOnDateFilter(roles_at_date_allowed=[role.SubSystem.QUALITE]))],
             )
 
@@ -89,8 +89,8 @@ def main() -> None:
         # produce_results_and_displays_for_adonem_known_cstmr()
         # ui_and_results_generation.block_execution_and_keep_all_windows_open()
 
-        all_champfx_library = cfx.ChampFXLibrary()
-        # print_gathering_time(champfx_library=all_champfx_library, number_iterations=3)
+        all_champfx_library = cfx.ChampFXLibrary(champfx_filters=[cfx.ChampFXWhitelistFilter(cfx_to_treat_whitelist_text_file_full_path="Input_for_Tests/sample_cfx_ids.txt")])
+        print_gathering_time(champfx_library=all_champfx_library, number_iterations=2)
 
         ui_and_results_generation.produce_results_and_displays_for_libary(
             cfx_library=all_champfx_library,
