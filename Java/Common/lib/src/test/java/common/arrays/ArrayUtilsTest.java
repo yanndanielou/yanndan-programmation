32009ebbe16bef2688182c9154ac47191b44ab70
yanndanielou <yanndanielou@users.noreply.github.com>
2025-05-15 10:53:05 +0200

    Update ArrayUtilsTest.java

--- Java/Common/lib/src/test/java/common/arrays/ArrayUtilsTest.java
+++ Java/Common/lib/src/test/java/common/arrays/ArrayUtilsTest.java
@@ -1,22 +1,23 @@
 package common.arrays;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class ArrayUtilsTest {
 
-	@Test
-	void nullArgument() {
-		assertNull(ArrayUtils.byteArrayToIntArray(null));
-	}
+    @Test
+    void nullArgument() {
+        assertNull(ArrayUtils.byteArrayToIntArray(null));
+    }
 
-	@Test
-	void validTable() {
-		int[] intArray = { 1, 2, 3 };
-		byte[] byteArray = { 1, 2, 3 };
-		assertArrayEquals(intArray, ArrayUtils.byteArrayToIntArray(byteArray));
-	}
+    @Test
+    void validTable() {
+        int[] intArray = {1, 2, 3};
+        byte[] byteArray = {1, 2, 3};
+        assertArrayEquals(intArray, ArrayUtils.byteArrayToIntArray(byteArray));
+    }
 
 }
