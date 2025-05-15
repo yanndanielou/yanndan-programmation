da808846de259af866b230ca50d788dd7fc4a0cb
yanndanielou <yanndanielou@users.noreply.github.com>
2025-05-15 10:53:20 +0200

    Update StringToColorConversionTest.java

--- Java/Common/lib/src/test/java/common/color/StringToColorConversionTest.java
+++ Java/Common/lib/src/test/java/common/color/StringToColorConversionTest.java
@@ -1,39 +1,39 @@
 package common.color;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.awt.Color;
-import java.util.stream.Stream;
-
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.awt.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class StringToColorConversionTest {
 
-	@Nested
-	public class ConvertStringToColor {
-		@Nested
-		public class BadUsages {
-			@Test
-			void nullArgument() {
-				assertNull(StringToColorConversion.convertStringToColor(null));
-			}
+    @Nested
+    public class ConvertStringToColor {
+        @Nested
+        public class BadUsages {
+            @Test
+            void nullArgument() {
+                assertNull(StringToColorConversion.convertStringToColor(null));
+            }
 
-			@Test
-			void invalidColorName() {
-				assertNull(StringToColorConversion.convertStringToColor("not existing color"));
-			}
-		}
+            @Test
+            void invalidColorName() {
+                assertNull(StringToColorConversion.convertStringToColor("not existing color"));
+            }
+        }
 
-		@Nested
-		public class CorrectUsages {
-			private static Stream<Arguments> provideColorsByName() {
+        @Nested
+        public class CorrectUsages {
+            private static Stream<Arguments> provideColorsByName() {
 
-				// @formatter:off
+                // @formatter:off
 				return Stream.of(
 						Arguments.of(Color.blue, "blue"),
 						Arguments.of(Color.black, "black"),
@@ -42,19 +42,19 @@ public class StringToColorConversionTest {
 						Arguments.of(Color.yellow, "yellow")
 						);
 				// @formatter:on	
-			}
+            }
 
-			@ParameterizedTest
-			@MethodSource("provideColorsByName")
-			void colorName(Color colorAsColor, String colorAsString) {
-				assertEquals(colorAsColor, StringToColorConversion.convertStringToColor(colorAsString));
-			}
+            @ParameterizedTest
+            @MethodSource("provideColorsByName")
+            void colorName(Color colorAsColor, String colorAsString) {
+                assertEquals(colorAsColor, StringToColorConversion.convertStringToColor(colorAsString));
+            }
 
-			@Test
-			void byRBG() {
-				assertEquals(Color.gray, StringToColorConversion.convertStringToColor("" + Color.gray.getRGB()));
-			}
-		}
-	}
+            @Test
+            void byRBG() {
+                assertEquals(Color.gray, StringToColorConversion.convertStringToColor("" + Color.gray.getRGB()));
+            }
+        }
+    }
 
 }
