545848378e4f4f9fc12b57bf5112bf7c6355422b
yanndanielou <yanndanielou@users.noreply.github.com>
2025-05-23 22:20:59 +0200

    Update main_visual.py

--- Python/GenerateCFXHistory/src/main_visual.py
+++ Python/GenerateCFXHistory/src/main_visual.py
@@ -30,6 +30,32 @@ def main() -> None:
             cfx_library=all_champfx_library,
             output_directory_name=OUTPUT_DIRECTORY_NAME,
             for_global=True,
+            for_each_subsystem=False,
+            for_each_current_owner_per_date=False,
+            cfx_filters=[
+                cfx.ChampFxFilter(cfx_to_treat_whitelist_text_file_full_path="Input/CFX_list_cyber_aio.txt"),
+            ],
+            create_excel_file=True,
+            create_html_file=True,
+        )
+
+        ui_and_results_generation.produce_results_and_displays_for_libary(
+            cfx_library=all_champfx_library,
+            output_directory_name=OUTPUT_DIRECTORY_NAME,
+            for_global=True,
+            for_each_subsystem=True,
+            for_each_current_owner_per_date=True,
+            cfx_filters=[
+                cfx.ChampFxFilter(cfx_to_treat_whitelist_text_file_full_path="Input/CFX_usine_site.txt"),
+            ],
+            create_excel_file=True,
+            create_html_file=True,
+        )
+
+        ui_and_results_generation.produce_results_and_displays_for_libary(
+            cfx_library=all_champfx_library,
+            output_directory_name=OUTPUT_DIRECTORY_NAME,
+            for_global=True,
             for_each_subsystem=True,
             for_each_current_owner_per_date=True,
             create_excel_file=True,
