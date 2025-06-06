6aeba5656f7b2933d0b54f0df2250e9ca19cc8b1
yanndanielou <yanndanielou@users.noreply.github.com>
2025-06-06 21:39:05 +0200

    Update copy_outputs_to_sharepoint.bat

--- Python/GenerateCFXHistory/copy_outputs_to_sharepoint.bat
+++ Python/GenerateCFXHistory/copy_outputs_to_sharepoint.bat
@@ -4,11 +4,21 @@ del /S /Q  "C:\Users\fr232487\OneDrive - Siemens AG\General - Projet Nexteo_ATS+
 timeout /t 30
 
 move "CFX_list_cyber_aio.txt*.html" "C:\Users\fr232487\OneDrive - Siemens AG\General - Projet Nexteo_ATS+\10 - KPI des lots\KPI CFX\CFX Cyber"
+move "Security Relevant among*YES*.html" "C:\Users\fr232487\OneDrive - Siemens AG\General - Projet Nexteo_ATS+\10 - KPI des lots\KPI CFX\CFX Cyber"
 
 move "All cumulative *.html" "C:\Users\fr232487\OneDrive - Siemens AG\General - Projet Nexteo_ATS+\10 - KPI des lots\KPI CFX\Globaux"
+
+move "*Project FR_NEXTEO*.html" "C:\Users\fr232487\OneDrive - Siemens AG\General - Projet Nexteo_ATS+\10 - KPI des lots\KPI CFX\Projet NExTEO"
 move "*Project FR_NEXTEO.html" "C:\Users\fr232487\OneDrive - Siemens AG\General - Projet Nexteo_ATS+\10 - KPI des lots\KPI CFX\Projet NExTEO"
+move "Project FR_NEXTEO*.html" "C:\Users\fr232487\OneDrive - Siemens AG\General - Projet Nexteo_ATS+\10 - KPI des lots\KPI CFX\Projet NEXTEO"
+
 move "CFX_usine_site*.html" "C:\Users\fr232487\OneDrive - Siemens AG\General - Projet Nexteo_ATS+\10 - KPI des lots\KPI CFX\Projet NExTEO"
+
+move "*Project ATSP*.html" "C:\Users\fr232487\OneDrive - Siemens AG\General - Projet Nexteo_ATS+\10 - KPI des lots\KPI CFX\Projet ATSP"
 move "*Project ATSP.html" "C:\Users\fr232487\OneDrive - Siemens AG\General - Projet Nexteo_ATS+\10 - KPI des lots\KPI CFX\Projet ATSP"
+move "Project ATSP*.html" "C:\Users\fr232487\OneDrive - Siemens AG\General - Projet Nexteo_ATS+\10 - KPI des lots\KPI CFX\Projet ATSP"
+
+move "*.html" "C:\Users\fr232487\OneDrive - Siemens AG\General - Projet Nexteo_ATS+\10 - KPI des lots\KPI CFX\"
 
 timeout /t 30
 
