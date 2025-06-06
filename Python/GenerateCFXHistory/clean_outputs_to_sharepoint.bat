0a8635c1ea3048c9d31633de4044b780d0caa981
yanndanielou <yanndanielou@users.noreply.github.com>
2025-06-06 21:42:34 +0200

    clean

---
+++ Python/GenerateCFXHistory/clean_outputs_to_sharepoint.bat
@@ -0,0 +1,5 @@
+del /S /Q  "C:\Users\fr232487\OneDrive - Siemens AG\General - Projet Nexteo_ATS+\10 - KPI des lots\KPI CFX\*"s
+
+timeout /t 30
+
+
--- Python/GenerateCFXHistory/copy_outputs_to_sharepoint.bat
+++ Python/GenerateCFXHistory/copy_outputs_to_sharepoint.bat
@@ -1,8 +1,5 @@
 PUSHD Output
 
-del /S /Q  "C:\Users\fr232487\OneDrive - Siemens AG\General - Projet Nexteo_ATS+\10 - KPI des lots\KPI CFX\*"
-timeout /t 30
-
 move "CFX_list_cyber_aio.txt*.html" "C:\Users\fr232487\OneDrive - Siemens AG\General - Projet Nexteo_ATS+\10 - KPI des lots\KPI CFX\CFX Cyber"
 move "Security Relevant among*YES*.html" "C:\Users\fr232487\OneDrive - Siemens AG\General - Projet Nexteo_ATS+\10 - KPI des lots\KPI CFX\CFX Cyber"
 
