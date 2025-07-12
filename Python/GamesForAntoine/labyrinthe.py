7909c8cd03066494e7704cbb00a8fa402d117332
yanndanielou <yanndanielou@users.noreply.github.com>
2025-07-12 17:23:24 +0200

    Update labyrinthe.py

--- Python/GamesForAntoine/labyrinthe.py
+++ Python/GamesForAntoine/labyrinthe.py
@@ -2,9 +2,12 @@ import tkinter as tk
 import random
 from typing import List, Tuple
 
+EXIT_CASE_CONTENT = "E"
+
 
 class MazeGame:
-    def __init__(self, root: tk.Tk, size: int = 14) -> None:
+    def __init__(self, root: tk.Tk, embedded_in_other_application: bool, size: int = 14) -> None:
+        self.embedded_in_other_application = embedded_in_other_application
         self.size = size
         self.root = root
         self.root.title("Maze Game")
@@ -24,10 +27,10 @@ class MazeGame:
         solution_path: List[Tuple[int, int]] = []
         self._generate_path(maze, solution_path, 1, 1)
         maze[1][1] = "S"
-        maze[self.size - 2][self.size - 2] = "E"
+        maze[self.size - 2][self.size - 2] = EXIT_CASE_CONTENT
         return maze, solution_path
 
-    def _generate_path(self, maze: List[Tuple[int, int]], solution_path: List[Tuple[int, int]], x: int, y: int) -> List[Tuple[int, int]]:
+    def _generate_path(self, maze: List[List[str]], solution_path: List[Tuple[int, int]], x: int, y: int) -> List[List[str]]:
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         random.shuffle(directions)
         solution_path.append((x, y))
@@ -38,7 +41,7 @@ class MazeGame:
                 if sum(1 for dx, dy in directions if maze[nx + dx][ny + dy] == " ") < 2:
                     maze[nx][ny] = " "
                     self._generate_path(maze, solution_path, nx, ny)
-                    if maze[self.size - 2][self.size - 2] == "E":
+                    if maze[self.size - 2][self.size - 2] == EXIT_CASE_CONTENT:
                         break
         return maze
 
@@ -69,6 +72,7 @@ class MazeGame:
             self.player_pos = new_position
 
     def key_press(self, event: tk.Event) -> None:
+        show_solution = False
         if event.keysym == "Up":
             self.move_player(-1, 0)
         elif event.keysym == "Down":
@@ -78,16 +82,19 @@ class MazeGame:
         elif event.keysym == "Right":
             self.move_player(0, 1)
         elif event.keysym == "s":
-            self.draw_maze(show_solution=True)
+            show_solution = True
 
-        self.draw_maze()
+        self.draw_maze(show_solution)
 
         if self.maze[self.player_pos[0]][self.player_pos[1]] == "E":
             print("You've reached the end!")
-            self.root.quit()
+            if self.embedded_in_other_application:
+                self.canvas.quit()
+            else:
+                self.root.quit()
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    game = MazeGame(root)
+    game = MazeGame(root, False)
     root.mainloop()
