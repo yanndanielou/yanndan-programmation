a3b11526741e36ef626f63b9afefca2c276d11f8
yanndanielou <yanndanielou@users.noreply.github.com>
2025-07-16 21:33:36 +0200

    Create lancement_visual_studio_server - next.bat

---
+++ Python/ExtractCodeToDebugInTCM/lancement_visual_studio_server - next.bat
@@ -0,0 +1,120 @@
+set SIEMENS_SOURCES=M:\NEXT_PCC_V0_40_BLD
+
+
+
+SET BIN_PATH=bin\deliverables\vc140\Debug\x86
+SET BIN_STUDIO_PATH=bin\deliverables\vc140\DebugStudio\x86
+SET BOOST_LIB_VERSION=V1_66_0
+
+SET CL=/DACQ_WITH_DCS_SNMP
+
+
+SET COMMON_CSV_PATH=M:\NEXT_PCC_BD_V0_39_BLD\NEXT_PCC\BD\Csv\Common
+SET COMMON_MAIN_PATH=M:\NEXT_PCC_BD_V0_39_BLD\NEXT_PCC\BD
+
+
+SET COMPILE_CONFIG=Release
+
+
+SET CPP_LIB_PATH_SELECTOR=CppLibPath
+SET CPP_LIB_PATH=%SIEMENS_SOURCES%\XT_PCC_TOOLS\CppLibraries
+SET CPPUNIT_VER=1.12.1
+SET CYGWIN_HOME=C:\produits\cygwin
+SET CYGWIN=nodosfilewarning
+SET DB_API_PREPROCESSOR=DBff_API_SQLITE
+
+
+SET DELIVERY_DRIVE=D
+SET DELIVERY_PATH=M:\NEXT_PCC_BD_V0_39_BLD\NEXT_PCC\BD\Common\Install\Delivery
+SET DOT_HOME=C:\Program Files (x86)\Graphviz2.34\bin
+
+SET EVENT_LOGGER_PREPROCESSOR=
+SET FINDANDREPLACE_BIN_PATH=%SIEMENS_SOURCES%\SPOCC\bin\deliverables\vc140\Debug\x86
+SET FINDANDREPLACE_CONFIG_PATH=%SIEMENS_SOURCES%\RIYL_PCC\BD_L2\FindAndReplace\Data\Config
+
+SET GCL_ACS=%SIEMENS_SOURCES%\XT_PCC\ACS
+SET GCL_AF=%SIEMENS_SOURCES%\XT_PCC\AF
+SET GCL_ATSP_AF=%SIEMENS_SOURCES%\ATSP\AF
+SET GCL_ATSP_CCK=%SIEMENS_SOURCES%\ATSP\CCK
+SET GCL_ATSP_GPT=%SIEMENS_SOURCES%\ATSP\GPT
+SET GCL_ATSP_RDV3=%SIEMENS_SOURCES%\ATSP\RDV3
+SET GCL_ATSP_SECU_IHM=%SIEMENS_SOURCES%\ATSP\SECU_IHM
+SET GCL_ATSP_SMT=%SIEMENS_SOURCES%\ATSP\SMT
+SET GCL_ATSP_SPOCC=%SIEMENS_SOURCES%\ATSP\SPOCC
+SET GCL_CBTC_ITF=%SIEMENS_SOURCES%\CBTC\ITF
+SET GCL_CBTC_ITF_PCC=%SIEMENS_SOURCES%\CBTC\ITF
+SET GCL_MSG=%SIEMENS_SOURCES%\XT_PCC\RDV3
+SET GCL_MSG_PCC=%SIEMENS_SOURCES%\XT_PCC\RDV3
+SET GCL_PCC=%SIEMENS_SOURCES%\NEXT_PCC\PCC
+SET GCL_SPOCC=%SIEMENS_SOURCES%\SPOCC
+SET GCL_XT_CPP=%SIEMENS_SOURCES%\XT_CPP
+SET GCL_XT_PCC_TOOLS=%SIEMENS_SOURCES%\XT_PCC_TOOLS
+
+SET HOMEDRIVE=D:
+
+
+SET ILM_LICENSE_FILE=C:\produits\Ilog\Ilm27\access.ilm
+SET ILOG_SERVER_TRACE=-ilstrace
+SET ILOG_SRV_VER=65
+SET ILOG_VIEW_VER=65
+SET ILS_TCP_DEBUG=1
+SET ILS_TCP_TRACE=1
+SET ILSHOME=%SIEMENS_SOURCES%\XT_PCC_TOOLS\CppLibraries\Ilog\Server65
+
+SET ILV_OBJECTS_EXTRACTOR_BIN_PATH=%SIEMENS_SOURCES%\XT_PCC\AF\bin\deliverables\vc140\DebugStudio\x86
+SET ILV_OBJECTS_EXTRACTOR_CONFIG_PATH=%SIEMENS_SOURCES%\RIYL_PCC\BD_L2\IlvObjectsExtractor\Data\Config
+SET ILVHOME=%SIEMENS_SOURCES%\XT_PCC_TOOLS\CppLibraries\Ilog\Views65
+
+SET ILVLANG=fr_FR
+SET MSG_CBTC_VERSION=804
+
+
+SET LINE_SPECIFIC_PROJECT_PREFIX=NEXT
+
+SET LOCALE_FOLDER=%SIEMENS_SOURCES%\NEXT_PCC\BD\Hmi\Data\locale
+
+SET MSVC_VER=140
+
+SET NATS_SERVER_VER=2.1.9
+
+SET NSQ_VER=1.2.0
+
+SET PATH=C:\IBM\RationalSDLC\common;C:\Produits\Python35\Scripts\;C:\Produits\Python35\;C:\Program Files\Microsoft MPI\Bin\;C:\ProgramData\Oracle\Java\javapath;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Program Files (x86)\Microsoft SQL Server\130\Tools\Binn\;C:\Program Files\Microsoft SQL Server\130\Tools\Binn\;C:\Program Files (x86)\Microsoft SQL Server\130\DTS\Binn\;C:\Program Files\Microsoft SQL Server\130\DTS\Binn\;C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn\;C:\Program Files\doxygen\bin;\\frctn02nas\users\users\drl\RL_REFCC\GCL\bat;C:\Produits\Cygwin\bin;C:\Program Files (x86)\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn\;C:\Program Files (x86)\Microsoft SQL Server\140\Tools\Binn\;C:\Program Files (x86)\Microsoft SQL Server\140\DTS\Binn\;C:\Program Files (x86)\Microsoft SQL Server\140\Tools\Binn\ManagementStudio\;C:\Program Files\Common Files\ThinPrint\;D:\Users\fr232487\.dnx\bin;C:\Program Files\Microsoft DNX\Dnvm\;C:\Program Files\Microsoft SQL Server\120\Tools\Binn\;C:\Produits\PERL\c\bin;C:\Produits\PERL\perl\site\bin;C:\Produits\PERL\perl\bin;C:\IBM\RationalSDLC\ClearCase\bin;C:\Program Files (x86)\IBM\gsk8\lib;C:\Program Files (x86)\IBM\gsk8\bin;C:\IBM\RationalSDLC\clearcase\RemoteClient\cteapis;C:\Program Files\dotnet\;C:\Program Files (x86)\dotnet\;D:\Users\fr232487\AppData\Local\Programs\Python\Launcher\;D:\Users\fr232487\AppData\Local\Microsoft\WindowsApps;;C:\Program Files (x86)\IBM\gsk8\lib;C:\Program Files (x86)\IBM\gsk8\bin
+SET PATHEXT=.COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC;.tcl;.PY;.PYW
+
+SET PRODUCT_CONFIG_PATH=%SIEMENS_SOURCES%\SPOCC\tools\Utilities
+SET PRODUCTS_PATH=C:\produits
+
+SET PROJECT_NAME=NEXT
+SET PROJECT_PREFIX=NEXT
+SET PROMPT=$P$G
+SET PSMODULEPATH=C:\Program Files\WindowsPowerShell\Modules;C:\Windows\system32\WindowsPowerShell\v1.0\Modules;C:\Program Files (x86)\Microsoft SQL Server\130\Tools\PowerShell\Modules\
+
+SET SCENE_BUILDER_VER=8.5.0
+
+
+SET SHELLOPTS=igncr
+
+
+SET TARGET_PLATFORM=x86
+
+
+SET USE_JNI=2
+
+
+SET VS100COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\
+SET VS140COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\
+
+
+SET XT_TARGET_PLATFORM_WINVER=0x0601
+SET XT_USE_MEMORYLEAK_TOOL=0
+
+
+SET DEVENV_HOME_MSVC_100="%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\Common7\IDE"
+SET DEVENV_HOME_MSVC_140="%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE"
+
+SET DEVENV_HOME="%DEVENV_HOME_MSVC_140%"
+
+"%DEVENV_HOME%\devenv.exe" %SIEMENS_SOURCES%\ATSP\AF\AFP\Server\ServerApplication.sln
+
+timeout /t 100
