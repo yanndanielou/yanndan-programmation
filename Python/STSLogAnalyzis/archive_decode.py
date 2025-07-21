3343a41a1bc6e375cade29d2859eaff5a78e8c75
yanndanielou <yanndanielou@users.noreply.github.com>
2025-07-21 16:12:01 +0200

    Update archive_decode.py

--- Python/STSLogAnalyzis/archive_decode.py
+++ Python/STSLogAnalyzis/archive_decode.py
@@ -1,46 +1,56 @@
 import json
 
+
+class ArchiveLine:
+    def __init__(self, full_raw_archive_line: str) -> None:
+
+        # Parsing JSON string
+        self.full_archive_line_as_json = json.loads(full_raw_archive_line)
+
+        # Directly copy all items from SQLARCH section into a new dictionary
+        self.sqlarch_fields_dict = self.full_archive_line_as_json.get("SQLARCH", {})
+
+        # Access global fields
+        self.date = self.full_archive_line_as_json["date"]
+        self.tags = self.full_archive_line_as_json["tags"]
+
+        # Accessing specific fields
+        self.sqlarch_json_section = self.full_archive_line_as_json["SQLARCH"]
+
+        # Directly copy all items from SQLARCH section into a new dictionary
+        self.sqlarch_fields_dict = self.full_archive_line_as_json.get("SQLARCH", {})
+
+        # Accessing specific fields
+        self.caller = self.sqlarch_json_section.get("caller")
+        self.catAla = self.sqlarch_json_section.get("catAla")
+        self.eqp = self.sqlarch_json_section.get("eqp")
+        self.eqpId = self.sqlarch_json_section.get("eqpId")
+        self.exeSt = self.sqlarch_json_section.get("exeSt")
+        self.id_field = self.sqlarch_json_section.get("id")
+        self.jdb = self.sqlarch_json_section.get("jdb")
+        self.label = self.sqlarch_json_section.get("label")
+        self.loc = self.sqlarch_json_section.get("loc")
+
+    def print_all(self) -> None:
+        # Print extracted fields
+        print("Caller:", self.caller)
+        print("CatAla:", self.catAla)
+        print("Eqp:", self.eqp)
+        print("EqpId:", self.eqpId)
+        print("ExeSt:", self.exeSt)
+        print("ID:", self.id_field)
+        print("Jdb:", self.jdb)
+        print("Label:", self.label)
+        print("Loc:", self.loc)
+
+        # Print the dictionary for SQLARCH fields
+        for key, value in self.sqlarch_fields_dict.items():
+            print(f"{key}: {value}")
+
+        print("Date:", self.date)
+        print("Tags:", self.tags)
+
+
 # JSON string
 json_data = '{"SQLARCH":{"caller":"","catAla":0,"eqp":"PAS 05","eqpId":"EQ_PAS_05_PAS_05","exeSt":"","id":"M_PAS_05_ZC_ATS_TRACKING_STATUS_TRAINS_ID","jdb":false,"label":"PAS_05 : ZC ATS TRACKING STATUS TRAINS ID [161]","loc":"PAS 05","locale":"2025-07-21T10:54:18.439+02:00","newSt":"14 09 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 04 E4 44 00 01 19 40 27 EC ","oldSt":"","orders":"","sigT":"TSA","tstamp":"2025-07-21T10:54:18.000+02:00","utc_locale":"2025-07-21T08:54:18.439+01:00"},"date":"2025-07-21T10:54:18.440+02:00","tags":["SQLARCH"]}'
-
-# Parsing JSON string
-data = json.loads(json_data)
-
-# Directly copy all items from SQLARCH section into a new dictionary
-sqlarch_fields = data.get("SQLARCH", {})
-
-# Access global fields
-date = data["date"]
-tags = data["tags"]
-
-
-# Accessing specific fields
-sqlarch = data["SQLARCH"]
-caller = sqlarch.get("caller")
-catAla = sqlarch.get("catAla")
-eqp = sqlarch.get("eqp")
-eqpId = sqlarch.get("eqpId")
-exeSt = sqlarch.get("exeSt")
-id_field = sqlarch.get("id")
-jdb = sqlarch.get("jdb")
-label = sqlarch.get("label")
-loc = sqlarch.get("loc")
-
-# Print extracted fields
-print("Caller:", caller)
-print("CatAla:", catAla)
-print("Eqp:", eqp)
-print("EqpId:", eqpId)
-print("ExeSt:", exeSt)
-print("ID:", id_field)
-print("Jdb:", jdb)
-print("Label:", label)
-print("Loc:", loc)
-
-
-# Print the dictionary for SQLARCH fields
-for key, value in sqlarch_fields.items():
-    print(f"{key}: {value}")
-
-print("Date:", date)
-print("Tags:", tags)
+ArchiveLine(json_data).print_all()
