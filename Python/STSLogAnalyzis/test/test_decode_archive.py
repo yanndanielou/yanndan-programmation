1c9c6aa65e8f22cd40f0d5361b79852f88500b99
yanndanielou <yanndanielou@users.noreply.github.com>
2025-07-23 15:59:19 +0200

    Update test_decode_archive.py

--- Python/STSLogAnalyzis/test/test_decode_archive.py
+++ Python/STSLogAnalyzis/test/test_decode_archive.py
@@ -12,14 +12,14 @@ archive_line_str_message_pae_ats_soft_versions = '{"SQLARCH":{"caller":"","catAl
 archive_line_str_message_ats_pae_action_set = '{"SQLARCH":{"caller":"","catAla":0,"eqp":"TRAIN 9","eqpId":"EQ_CET_9","exeSt":"","id":"M_TRAIN_CC_9_ATS_CC_ACTION_SET","jdb":false,"label":"TRAIN : ATS_CC_ACTION_SET [192]","loc":"TB_CDV_z8321_03","locale":"2025-07-21T13:06:13.489+02:00","newSt":"08 08 30 F8 30 00 00 00 70 00 00 00 00 00 00 00 01 44 A2 C9 24 94 49 24 92 44 44 4B 60 91 11 11 12 12 21 10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 29 5F EB FC 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ","oldSt":"","orders":"","sigT":"TCA","tstamp":"","utc_locale":"2025-07-21T11:06:13.489+01:00"},"date":"2025-07-21T13:06:13.490+02:00","tags":["SQLARCH"]}'
 archive_line_str_message_ats_pae_spe_remote_ctrl = '{"SQLARCH":{"caller":"","catAla":0,"eqp":"TRAIN 9","eqpId":"EQ_CET_9","exeSt":"","id":"M_TRAIN_CC_9_ATS_CC_REMOTE_CONTROL_SPE","jdb":false,"label":"TRAIN : ATS_CC_REMOTE_CONTROL_SPE [101]","loc":"TB_CDV_z2466_01","locale":"2025-07-21T18:03:50.661+02:00","newSt":"04 A0 83 18 EA C7 00 71 82 0C 20 00 00 00 F8 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ","oldSt":"","orders":"","sigT":"TCA","tstamp":"","utc_locale":"2025-07-21T16:03:50.661+01:00"},"date":"2025-07-21T18:03:50.662+02:00","tags":["SQLARCH"]}'
 
-message_manager = decode_message.InvariantMessagesManager(messages_csv_file_full_path=r"D:\NEXT\Data\Csv\NEXT_message.csv")
+message_manager = decode_message.InvariantMessagesManager(messages_list_csv_file_full_path=r"D:\NEXT\Data\Csv\NEXT_message.csv")
 message_decoder = decode_message.MessageDecoder(xml_directory_path=r"D:\NEXT\Data\Xml")
 
 
 class TestDecodeOneArchiveFile:
     def test_process_one_file(self) -> None:
         archive_file = decode_archive.ArchiveFile(r"Input\archive_2025_07_22\NEXTFileArchiveServer_365.json")
-        archive_file.process()
+        archive_file.open_and_read_archive_file_lines()
         assert len(archive_file.all_version_lines) == 1
         assert len(archive_file.all_sqlarch_lines) > 100
         pass
@@ -27,7 +27,7 @@ class TestDecodeOneArchiveFile:
 
 class TestDecodeOneLine:
     def test_decode_basic_fields_zc_ats_tracking_status(self) -> None:
-        archive_line = decode_archive.ContentArchiveLine(full_raw_archive_line=archive_line_str_message_zc_ats_tracking_status)
+        archive_line = decode_archive.SqlArchArchiveLine(full_raw_archive_line=archive_line_str_message_zc_ats_tracking_status)
         assert archive_line.get_date_raw_str() == "2025-07-21T10:54:18.440+02:00"
         assert archive_line.get_id() == "M_PAS_05_ZC_ATS_TRACKING_STATUS_TRAINS_ID"
         assert (
@@ -36,7 +36,7 @@ class TestDecodeOneLine:
         )
 
     def test_decode_basic_fields_lc_ats_sso_versions(self) -> None:
-        archive_line = decode_archive.ContentArchiveLine(full_raw_archive_line=archive_line_str_message_lc_ats_sso_versions)
+        archive_line = decode_archive.SqlArchArchiveLine(full_raw_archive_line=archive_line_str_message_lc_ats_sso_versions)
         assert archive_line.get_id() == "M_PAL_01_LC_ATS_SSO_VERSIONS"
 
         invariant_message = message_manager.get_message_by_id(archive_line.get_id())
@@ -48,11 +48,11 @@ class TestDecodeOneLine:
         assert decoded_message.decoded_fields["Time"] == 8
 
     def test_get_archive_line_fields_message_ats_pae_action_set(self) -> None:
-        archive_line = decode_archive.ContentArchiveLine(full_raw_archive_line=archive_line_str_message_ats_pae_action_set)
+        archive_line = decode_archive.SqlArchArchiveLine(full_raw_archive_line=archive_line_str_message_ats_pae_action_set)
         assert archive_line.get_id() == "M_TRAIN_CC_9_ATS_CC_ACTION_SET"
 
     def test_decode_message_fields_with_xml_ats_pae_action_set(self) -> None:
-        archive_line = decode_archive.ContentArchiveLine(full_raw_archive_line=archive_line_str_message_ats_pae_action_set)
+        archive_line = decode_archive.SqlArchArchiveLine(full_raw_archive_line=archive_line_str_message_ats_pae_action_set)
 
         invariant_message = message_manager.get_message_by_id(archive_line.get_id())
         assert invariant_message
@@ -73,7 +73,7 @@ class TestDecodeOneLine:
         )
 
     def test_decode_string_version_field_in_message(self) -> None:
-        archive_line = decode_archive.ContentArchiveLine(full_raw_archive_line=archive_line_str_message_pae_ats_soft_versions)
+        archive_line = decode_archive.SqlArchArchiveLine(full_raw_archive_line=archive_line_str_message_pae_ats_soft_versions)
 
         invariant_message = message_manager.get_message_by_id(archive_line.get_id())
         assert invariant_message
@@ -86,7 +86,7 @@ class TestDecodeOneLine:
         assert cast(str, decoded_message.decoded_fields["SoftVersionPart1_2"]) == "E"
 
     def test_decode_when_selctor_rc_type(self) -> None:
-        archive_line = decode_archive.ContentArchiveLine(full_raw_archive_line=archive_line_str_message_ats_pae_spe_remote_ctrl)
+        archive_line = decode_archive.SqlArchArchiveLine(full_raw_archive_line=archive_line_str_message_ats_pae_spe_remote_ctrl)
         invariant_message = message_manager.get_message_by_id(archive_line.get_id())
         assert invariant_message
         decoded_message = message_decoder.decode_message(message_number=invariant_message.message_number, hexadecimal_content=archive_line.get_new_state_str())
