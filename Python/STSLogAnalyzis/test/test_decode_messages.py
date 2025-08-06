8c7ebba23a7782e2e23241d9bab7005ef07bd139
yanndanielou <yanndanielou@users.noreply.github.com>
2025-08-06 15:48:55 +0200

    message_with_record_with_dimension

--- Python/STSLogAnalyzis/test/test_decode_messages.py
+++ Python/STSLogAnalyzis/test/test_decode_messages.py
@@ -16,6 +16,17 @@ decode_hlf_test_data = [
 # fmt: on
 
 
+class TestDecodeCbtcMessages:
+    def test_message_with_record_with_dimension(self) -> None:
+        hexa_content_as_str = "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01 42 01 42 01 01 01 01 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 40 00 06 F0 E9 00 01 19 40 27 FC"
+
+        xml_message_decoder = decode_message.XmlMessageDecoder(xml_directory_path=r"D:\NEXT\Data\Xml")
+        decoded_message = xml_message_decoder.decode_xml_fields_in_message_hexadecimal(message_number=205, hexadecimal_content=hexa_content_as_str)
+        assert decoded_message
+        assert decoded_message.decoded_fields["UtoTrainReversingMode"] == 4
+        assert decoded_message.decoded_fields["TimeOffset"] == 72000
+
+
 class TestDecodeHlf:
     def test_0(self) -> None:
         assert decode_message.HLFDecoder.decode_hlf_fields_to_datetime(time_field_value=0, time_offset_value=0, decade_field_value=0, day_on_decade_field_value=0) == datetime.datetime(
