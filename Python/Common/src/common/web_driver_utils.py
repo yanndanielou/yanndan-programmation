cae13026fe96692456036d90b898c33922de152c
yanndanielou <yanndanielou@users.noreply.github.com>
2025-08-30 20:46:32 +0200

    Create web_driver_utils.py

---
+++ Python/Common/src/common/web_driver_utils.py
@@ -0,0 +1,78 @@
+from enum import Enum, auto
+from typing import cast
+
+from logger import logger_config
+
+# Third Party
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.remote.remote_connection import RemoteConnection
+
+
+class BrowserVisibilityType(Enum):
+    REGULAR = auto()
+    NOT_VISIBLE_AKA_HEADLESS = auto()
+    MINIMIZED = auto()
+
+
+def create_webdriver_chrome(browser_visibility_type: BrowserVisibilityType, download_directory_path: str, global_timeout_in_seconds: int = 1000) -> webdriver.Chrome:
+    logger_config.print_and_log_info("create_webdriver_chrome")
+    chrome_driver_path = "C:\\Users\\fr232487\\Downloads\\chromedriver-win64\\chromedriver.exe"
+
+    chrome_options = ChromeOptions()
+    prefs = {
+        "download.default_directory": download_directory_path,
+        "savefile.default_directory": download_directory_path,
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "safebrowsing.enabled": True,
+    }
+    chrome_options.add_experimental_option("prefs", prefs)
+    if browser_visibility_type == BrowserVisibilityType.NOT_VISIBLE_AKA_HEADLESS:
+        chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--disable-software-rasterizer")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+
+    elif browser_visibility_type == BrowserVisibilityType.MINIMIZED:
+        chrome_options.add_argument("--start-minimized")
+
+    driver_service = Service(chrome_driver_path)
+    driver = webdriver.Chrome(service=driver_service, options=chrome_options)
+
+    if browser_visibility_type == BrowserVisibilityType.MINIMIZED:
+        try:
+            driver.minimize_window()
+        except Exception as exc:
+            logger_config.print_and_log_exception(exc)
+
+    cast(RemoteConnection, driver.command_executor).set_timeout(global_timeout_in_seconds)
+    return driver
+
+
+def create_webdriver_firefox(browser_visibility_type: BrowserVisibilityType, download_directory_path: str, global_timeout_in_seconds: int = 1000) -> webdriver.Firefox:
+    logger_config.print_and_log_info("create_webdriver_firefox")
+    firefox_options = FirefoxOptions()
+    if browser_visibility_type == BrowserVisibilityType.NOT_VISIBLE_AKA_HEADLESS:
+        firefox_options.add_argument("--headless")
+    else:
+        firefox_options.add_argument("--width=800")
+        firefox_options.add_argument("--height=600")
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("browser.download.folderList", 2)
+    profile.set_preference("browser.download.dir", download_directory_path)
+    profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/vnd.ms-excel,text/plain,application/octet-stream")
+    profile.set_preference("pdfjs.disabled", True)
+    driver = webdriver.Firefox(options=firefox_options, firefox_profile=profile)
+
+    if browser_visibility_type == BrowserVisibilityType.MINIMIZED:
+        try:
+            driver.minimize_window()
+        except Exception:
+            pass
+    return driver
