871200f1b5c368d8a9e23b4cf865745838c7e7b8
yanndanielou <yanndanielou@users.noreply.github.com>
2025-09-03 14:31:32 +0200

    encapsulate generation instructions

--- Python/NetworkFlowMatrix/main.py
+++ Python/NetworkFlowMatrix/main.py
@@ -3,9 +3,11 @@ from typing import Callable, List, cast
 
 from logger import logger_config
 
-from networkflowmatrix import input_file_processing
+from networkflowmatrix import data_model
 
 if __name__ == "__main__":
     with logger_config.application_logger("networkflowmatrix"):
 
-        input_file_processing.load_network_flow_matrix_excel_file("Input/Matrice_next.xlsm")
+        network_flow_matrix = data_model.NetworkFlowMatrix.Builder.build_with_excel_file(excel_file_full_path="Input/Matrice_next.xlsm", sheet_name="Matrice_de_Flux_SITE")
+
+    logger_config.print_and_log_info(f"all_equipments_names:{data_model.all_equipments_names_with_subsystem}")
