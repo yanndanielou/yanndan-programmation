7f14876c514d3fe5b3d7ab3ca2ed63193ff32659
yanndanielou <yanndanielou@users.noreply.github.com>
2025-09-04 16:25:39 +0200

    Update main.py

--- Python/NetworkFlowMatrix/main.py
+++ Python/NetworkFlowMatrix/main.py
@@ -12,24 +12,24 @@ OUTPUT_PARENT_DIRECTORY_NAME = "Output"
 def subsystem_to_dict(subsystem: data_model.SubSystemInFlowMatrix, with_equipment: bool) -> Dict:
     if with_equipment:
         return {
-            "name": subsystem.name,
-            "number_equipments": len(subsystem.all_equipments_detected_in_flow_matrix),
-            "all_equipments": [equipment_to_dict(eq, False) for eq in subsystem.all_equipments_detected_in_flow_matrix],
+            "subsystem_name": subsystem.name,
+            "subsystem_number_equipments": len(subsystem.all_equipments_detected_in_flow_matrix),
+            "subsystem_all_equipments": [equipment_to_dict(eq, False) for eq in subsystem.all_equipments_detected_in_flow_matrix],
         }
     else:
-        return {"name": subsystem.name}
+        return {"subsystem_name": subsystem.name}
 
 
 def equipment_to_dict(equipment: data_model.EquipmentInFLowMatrix, with_subsystem: bool) -> Dict:
     if with_subsystem:
         new_var = {
-            "name": equipment.name,
-            "ip_addresses": [str(ip) for ip in equipment.ip_addresses],
-            "number_of_subsystems_detected_in_flow_matrix": len(equipment.all_subsystems_detected_in_flow_matrix),
-            "all_subsystems_detected_in_flow_matrix": [subsystem_to_dict(subsys, False) for subsys in equipment.all_subsystems_detected_in_flow_matrix],
+            "equipment_name": equipment.name,
+            "equipment_ip_addresses": [str(ip) for ip in equipment.ip_addresses],
+            "equipment_number_of_subsystems_detected_in_flow_matrix": len(equipment.all_subsystems_detected_in_flow_matrix),
+            "equipment_all_subsystems_detected_in_flow_matrix": [subsystem_to_dict(subsys, False) for subsys in equipment.all_subsystems_detected_in_flow_matrix],
         }
     else:
-        new_var = {"name": equipment.name, "ip_addresses": [str(ip) for ip in equipment.ip_addresses]}
+        new_var = {"equipment_name": equipment.name, "equipment_ip_addresses": [str(ip) for ip in equipment.ip_addresses]}
 
     return new_var
 
@@ -61,3 +61,6 @@ if __name__ == "__main__":
         json_encoders.JsonEncodersUtils.serialize_list_objects_in_json(
             data_model.all_equipments_names_with_subsystem, f"{OUTPUT_PARENT_DIRECTORY_NAME}/data_model.all_equipments_names_with_subsystem.json"
         )
+
+        network_line = network_flow_matrix.get_line_by_identifier(1970)
+        assert network_line
