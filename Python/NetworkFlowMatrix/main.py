111d65ec92d02985bd67ebdb7def7151bd2ad740
yanndanielou <yanndanielou@users.noreply.github.com>
2025-09-05 10:36:08 +0200

    Update main.py

--- Python/NetworkFlowMatrix/main.py
+++ Python/NetworkFlowMatrix/main.py
@@ -34,20 +34,14 @@ def equipment_to_dict(equipment: data_model.EquipmentInFLowMatrix, with_subsyste
     return new_var
 
 
-def dump_subsystems_to_json(filepath: str) -> None:
-    data = [
-        subsystem_to_dict(subsystem, with_equipment=True)
-        for subsystem in sorted(data_model.SubSystemInFlowMatrix.all_matrix_flow_subsystems_definitions_instances, key=lambda subsystem: subsystem.name)
-    ]
+def dump_subsystems_to_json(network_flow_matrix: data_model.NetworkFlowMatrix, filepath: str) -> None:
+    data = [subsystem_to_dict(subsystem, with_equipment=True) for subsystem in sorted(network_flow_matrix.all_matrix_flow_subsystems_definitions_instances, key=lambda subsystem: subsystem.name)]
     with open(filepath, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
 
 
-def dump_equipments_to_json(filepath: str) -> None:
-    data = [
-        equipment_to_dict(equipment, with_subsystem=True)
-        for equipment in sorted(data_model.EquipmentInFLowMatrix.all_matrix_flow_equipments_definitions_instances, key=lambda subsystem: subsystem.name)
-    ]
+def dump_equipments_to_json(network_flow_matrix: data_model.NetworkFlowMatrix, filepath: str) -> None:
+    data = [equipment_to_dict(equipment, with_subsystem=True) for equipment in sorted(network_flow_matrix.all_matrix_flow_equipments_definitions_instances, key=lambda subsystem: subsystem.name)]
     with open(filepath, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
 
@@ -59,6 +53,11 @@ if __name__ == "__main__":
         radio_std_conf_file = network_conf_files.RadioStdNetworkConfFile.build_with_excel_file(
             equipments_library=equipments_library, excel_file_full_path="Input_Downloaded/NExTEO-S-273000-02-0125-01 Dossier de Configuration Réseau STD Radio - V02-00 Annexe A_diffa.xlsx"
         )
+
+        sol_std_conf_file = network_conf_files.SolStdNetworkConfFile.build_with_excel_file(
+            equipments_library=equipments_library, excel_file_full_path="Input_Downloaded/NExTEO-S-271000-02-0125-02  Dossier de Configuration Réseau Sol - V10-00 Annexe A.xlsb"
+        )
+
         logger_config.print_and_log_info(f"After radio_std_conf_file, {len(equipments_library.network_conf_files_defined_equipments)} equipments")
 
         with logger_config.stopwatch_with_label("Build matrix", inform_beginning=True, monitor_ram_usage=True):
@@ -67,11 +66,11 @@ if __name__ == "__main__":
         for directory_path in [OUTPUT_PARENT_DIRECTORY_NAME]:
             file_utils.create_folder_if_not_exist(directory_path)
 
-        dump_subsystems_to_json(f"{OUTPUT_PARENT_DIRECTORY_NAME}/all_subsystems_in_flow_matrix.json")
-        dump_equipments_to_json(f"{OUTPUT_PARENT_DIRECTORY_NAME}/all_equipments_in_flow_matrix.json")
+        dump_subsystems_to_json(network_flow_matrix, f"{OUTPUT_PARENT_DIRECTORY_NAME}/all_subsystems_in_flow_matrix.json")
+        dump_equipments_to_json(network_flow_matrix, f"{OUTPUT_PARENT_DIRECTORY_NAME}/all_equipments_in_flow_matrix.json")
 
         json_encoders.JsonEncodersUtils.serialize_list_objects_in_json(
-            data_model.all_equipments_names_with_subsystem, f"{OUTPUT_PARENT_DIRECTORY_NAME}/data_model.all_equipments_names_with_subsystem.json"
+            network_flow_matrix.all_equipments_names_with_subsystem, f"{OUTPUT_PARENT_DIRECTORY_NAME}/data_model.all_equipments_names_with_subsystem.json"
         )
 
         network_line = network_flow_matrix.get_line_by_identifier(1970)
