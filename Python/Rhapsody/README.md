de0f6d9b1510c0cc83ee6ade20e037ec5b02a501
yanndanielou <yanndanielou@users.noreply.github.com>
2025-09-07 21:16:30 +0200

    Rhapsody utils

---
+++ Python/Rhapsody/README.md
@@ -0,0 +1,20 @@
+# DownloadAndCleanDML
+
+
+## Running Tests
+
+
+## Installation
+
+```sh
+pip install .
+pip install -e ..\Logger
+pip install -e ..\Common
+```
+
+## Running Tests
+
+```sh
+pytest
+```
+
---
+++ Python/Rhapsody/src/rhapsody/rhapsody_utils.py
@@ -0,0 +1,49 @@
+# Standard
+
+
+import time
+from typing import Optional
+
+from common import download_utils, web_driver_utils
+
+# Other libraries
+from logger import logger_config
+
+# Third Party
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def download_file_from_rhapsody(
+    file_to_download_pattern: str, file_to_download_url: str, file_move_after_download_action: Optional[download_utils.DownloadFileDetector.FileMoveAfterDownloadAction] = None
+) -> Optional[str]:
+
+    driver = web_driver_utils.create_webdriver_firefox(web_driver_utils.BrowserVisibilityType.NOT_VISIBLE_AKA_HEADLESS)
+
+    driver.get(file_to_download_url)
+
+    time.sleep(0.5)
+
+    wait = WebDriverWait(driver, 10)
+    more_providers_button = wait.until(expected_conditions.element_to_be_clickable((By.ID, "moreProviders")))
+    more_providers_button.click()
+
+    # Wait until the Azure option is visible and then click it
+    azure_option = wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "//li[@class='secondary-menu-item authprovider-choice' and @data-authhandler='Azure']")))
+
+    download_file_detector = download_utils.DownloadFileDetector(
+        directory_path=web_driver_utils.DEFAULT_DOWNLOAD_DIRECTORY, filename_pattern=file_to_download_pattern, timeout_in_seconds=30, file_move_after_download_action=file_move_after_download_action
+    )
+
+    azure_option.click()
+
+    file_downloaded_path: Optional[str] = download_file_detector.monitor_download()
+    if not file_downloaded_path:
+        logger_config.print_and_log_error("No downloaded file found")
+        return None
+
+    time.sleep(5)
+
+    driver.quit()
+    return file_downloaded_path
