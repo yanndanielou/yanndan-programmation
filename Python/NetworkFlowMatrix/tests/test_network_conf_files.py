a1d8437d9b35c2b0297cb107e7632908cda38b88
yanndanielou <yanndanielou@users.noreply.github.com>
2025-09-11 15:30:23 +0200

    Update test_network_conf_files.py

--- Python/NetworkFlowMatrix/tests/test_network_conf_files.py
+++ Python/NetworkFlowMatrix/tests/test_network_conf_files.py
@@ -1,21 +1,19 @@
-from typing import List, Set
+from typing import List, Set, cast
 
 import pytest
 
 from tests import secret_tests_data
 
-from networkflowmatrix.network_conf_files import (
-    EquipmentsLibrary,
-    RadioStdNetworkConfFile,
-    SolStdNetworkConfFile,
-)
+from networkflowmatrix.network_conf_files import EquipmentsLibrary, RadioStdNetworkConfFile, SolStdNetworkConfFile, NetworkConfFilesDefinedUnicastIpAddress, SolStdNetworkConfV10Description
 
 
 @pytest.fixture(scope="session", name="parse_std_sol_dossier_conf_v10_file_and_build_objects_fixture")
 def parse_std_sol_dossier_conf_v10_file_and_build_objects() -> SolStdNetworkConfFile:
     equipments_library = EquipmentsLibrary()
     std_sol_dossier_conf = SolStdNetworkConfFile.Builder.build_with_excel_file(
-        equipments_library=equipments_library, excel_file_full_path="Input_Downloaded/NExTEO-S-271000-02-0125-02  Dossier de Configuration Réseau Sol - V10-00 Annexe A.xlsb"
+        equipments_library=equipments_library,
+        excel_file_full_path="Input_Downloaded/NExTEO-S-271000-02-0125-02  Dossier de Configuration Réseau Sol - V10-00 Annexe A.xlsb",
+        equipment_definition_tabs=SolStdNetworkConfV10Description().all_tabs_definition,
     )
     return std_sol_dossier_conf
 
@@ -26,7 +24,16 @@ class TestSolStdNetworkV10ConfFileTabIpCbtcOnly:
         std_sol_dossier_conf = SolStdNetworkConfFile.Builder.build_with_excel_file(
             equipments_library=equipments_library,
             excel_file_full_path="Input_Downloaded/NExTEO-S-271000-02-0125-02  Dossier de Configuration Réseau Sol - V10-00 Annexe A.xlsb",
-            equipment_definition_tabs=[SolStdNetworkConfFile.IP_CBTC_TAB],
+            equipment_definition_tabs=[SolStdNetworkConfV10Description().ip_cbtc_tab],
+        )
+        assert std_sol_dossier_conf
+
+    def all_ip_definitions_have_decoded_ip_addresses(self) -> None:
+        equipments_library = EquipmentsLibrary()
+        std_sol_dossier_conf = SolStdNetworkConfFile.Builder.build_with_excel_file(
+            equipments_library=equipments_library,
+            excel_file_full_path="Input_Downloaded/NExTEO-S-271000-02-0125-02  Dossier de Configuration Réseau Sol - V10-00 Annexe A.xlsb",
+            equipment_definition_tabs=[SolStdNetworkConfV10Description().ip_cbtc_tab],
         )
         assert std_sol_dossier_conf
 
@@ -35,6 +42,7 @@ class TestSolStdNetworkV10FullConfFile:
     def test_no_empty_ip_address(self, parse_std_sol_dossier_conf_v10_file_and_build_objects_fixture: SolStdNetworkConfFile) -> None:
         for network_conf_files_defined_equipment in parse_std_sol_dossier_conf_v10_file_and_build_objects_fixture.equipments_library.network_conf_files_defined_equipments:
             for ip_address in network_conf_files_defined_equipment.ip_addresses:
+                ip_address = cast(NetworkConfFilesDefinedUnicastIpAddress, ip_address)
                 assert ip_address.vlan_name
                 assert ip_address.mask
                 assert ip_address.gateway
@@ -46,7 +54,21 @@ class TestSolStdNetworkV10FullConfFile:
         assert network_conf_files_defined_equipment
         assert len(network_conf_files_defined_equipment.ip_addresses) == 1
         for ip_address in network_conf_files_defined_equipment.ip_addresses:
+            ip_address = cast(NetworkConfFilesDefinedUnicastIpAddress, ip_address)
             assert ip_address.vlan_name
             assert ip_address.mask
             assert ip_address.gateway
             assert ip_address.ip_raw
+
+    def all_ip_definitions_have_decoded_ip_addresses(self, parse_std_sol_dossier_conf_v10_file_and_build_objects_fixture: SolStdNetworkConfFile) -> None:
+        equipments_library = EquipmentsLibrary()
+        std_sol_dossier_conf = SolStdNetworkConfFile.Builder.build_with_excel_file(
+            equipments_library=equipments_library,
+            excel_file_full_path="Input_Downloaded/NExTEO-S-271000-02-0125-02  Dossier de Configuration Réseau Sol - V10-00 Annexe A.xlsb",
+            equipment_definition_tabs=[SolStdNetworkConfV10Description().ip_cbtc_tab],
+        )
+        assert std_sol_dossier_conf.equipment_definition_tabs
+        for equipment_definition_tab in std_sol_dossier_conf.equipment_definition_tabs:
+            assert equipment_definition_tab.equipment_ip_definitions
+            for equipment_ip_definition in equipment_definition_tab.equipment_ip_definitions:
+                assert equipment_ip_definition.all_ip_addresses_found
