91c62a0f36f082fd74aaaa569fc1cc62b3c62e21
yanndanielou <yanndanielou@users.noreply.github.com>
2025-09-11 20:50:45 +0200

    get_file_name_without_extension_from_full_path

--- Python/Common/src/common/file_name_utils.py
+++ Python/Common/src/common/file_name_utils.py
@@ -1,9 +1,11 @@
 # -*-coding:Utf-8 -*
 
 from common import string_utils
+import pathlib
 
-def file_extension_from_full_path(full_path:str)->str:
-    """ returns the file extension if any"""
+
+def file_extension_from_full_path(full_path: str) -> str:
+    """returns the file extension if any"""
     last_part_of_path = string_utils.right_part_after_last_occurence(full_path, "/")
 
     if not "." in last_part_of_path:
@@ -12,5 +14,7 @@ def file_extension_from_full_path(full_path:str)->str:
     after_point = string_utils.right_part_after_last_occurence(last_part_of_path, ".")
     return "." + after_point
 
-def totddo(aa):
-    return "pl" + aa
+
+def get_file_name_without_extension_from_full_path(full_path: str) -> str:
+    file_name_without_extension_from_full_path = pathlib.Path(full_path).stem
+    return file_name_without_extension_from_full_path
--- Python/Common/src/test/test_file_name_utils.py
+++ Python/Common/src/test/test_file_name_utils.py
@@ -6,9 +6,9 @@ from src.common import file_name_utils
 
 
 file_extension_test_data = [
-    ("http://vevoxtv.top:2103/movie/412910643GRB/dn5QFp3/24950.mp4", ".mp4"),
-    ("http://vevoxtv.top:2103/series/412910643GRB/dn5QFp3/57996.mkv", ".mkv"),
-    ("http://vevoxtv.top:2103/412910643GRB/dn5QFp3/37744", None),
+    ("http://vevtv.top:2103/movie/412910643GRB/dn5QFp3/24950.mp4", ".mp4"),
+    ("http://oxtv.top:2103/series/412910643GRB/dn5QFp3/57996.mkv", ".mkv"),
+    ("http://vefextv.top:2103/412910643GRB/dn5QFp3/37744", None),
 ]
 
 
@@ -19,7 +19,14 @@ class TestFileExtension:
         file_extension_test_data,
     )
     def test_full_path(self, full_path: str, expected_extension: str) -> None:
-        assert (
-            file_name_utils.file_extension_from_full_path(full_path)
-            == expected_extension
-        )
+        assert file_name_utils.file_extension_from_full_path(full_path) == expected_extension
+
+
+class TestFileName:
+
+    @pytest.mark.parametrize(
+        "full_path, expected_file_name",
+        [("Input_for_tests\\main_word.docx", "main_word"), ("Input_for_tests/main_word.docx", "main_word"), (r"Input_for_tests\main_word.docx", "main_word")],
+    )
+    def test_file_name(self, full_path: str, expected_file_name: str) -> None:
+        assert file_name_utils.get_file_name_without_extension_from_full_path(full_path) == expected_file_name
