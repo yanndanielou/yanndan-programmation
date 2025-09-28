a66d7e24f71fdec5577cf9e8bf39defc7f49251a
yanndanielou <yanndanielou@users.noreply.github.com>
2025-09-28 12:26:02 +0200

    Update install_external_common_libraries.bat

--- Python/install_external_common_libraries.bat
+++ Python/install_external_common_libraries.bat
@@ -2,6 +2,7 @@
 
 @call :UPDATE_PYTHON_LIB pip
 
+@call :INSTALL_AND_UPDATE_PYTHON_LIB pyshark
 @call :INSTALL_AND_UPDATE_PYTHON_LIB python-pcapng
 rem @call :INSTALL_AND_UPDATE_PYTHON_LIB dpkt
 @call :INSTALL_AND_UPDATE_PYTHON_LIB xlsxwriter
