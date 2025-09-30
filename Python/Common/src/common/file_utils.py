2b9c1bfe85ba1a6f3740200c4802d997091bb4fc
yanndanielou <yanndanielou@users.noreply.github.com>
2025-09-30 21:55:02 +0200

    Update file_utils.py

--- Python/Common/src/common/file_utils.py
+++ Python/Common/src/common/file_utils.py
@@ -1,10 +1,35 @@
 # -*-coding:Utf-8 -*
-from datetime import datetime
 import fnmatch
 import os
+import shutil
+import tempfile
+from collections.abc import Generator
+from contextlib import contextmanager
+from datetime import datetime
 from typing import List, Tuple
 
 from logger import logger_config
+
+from common import file_name_utils
+
+
+def get_temporary_copy_of_file(input_file_full_path: str) -> Tuple[str, str]:
+    temp_dir_path = tempfile.mkdtemp()
+    logger_config.print_and_log_info(f"temporary_copy_of_file, temp_dir:{temp_dir_path}")
+    input_file_name = file_name_utils.get_file_name_with_extension_from_full_path(input_file_full_path)
+    temp_file_copy_path = os.path.join(temp_dir_path, input_file_name)
+    logger_config.print_and_log_info(f"temporary_copy_of_file, temp_file_copy_path:{temp_file_copy_path}")
+    shutil.copyfile(input_file_full_path, temp_file_copy_path)
+    return temp_dir_path, temp_file_copy_path
+
+
+@contextmanager
+def temporary_copy_of_file(input_file_full_path: str) -> Generator[str, None, None]:
+    temp_dir_path, temp_file_copy_path = get_temporary_copy_of_file(input_file_full_path)
+    yield temp_file_copy_path
+
+    logger_config.print_and_log_info(f"temporary_copy_of_file, remove (rmtree):{temp_dir_path}")
+    shutil.rmtree(temp_dir_path)
 
 
 def create_folder_if_not_exist(directory_path: str) -> bool:
