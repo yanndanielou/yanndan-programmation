1a998cfe418296c993e3f28bdbecf36f679b560d
yanndanielou <yanndanielou@users.noreply.github.com>
2025-10-06 17:53:57 +0200

    Update test_decode_wireshark.py

--- Python/STSLogAnalyzis/test/test_decode_wireshark.py
+++ Python/STSLogAnalyzis/test/test_decode_wireshark.py
@@ -10,6 +10,8 @@ from stsloganalyzis import decode_wireshark
 class TestDecodeWiresharkWithOnlyCbtc:
     def test_process_one_file(self) -> None:
 
-        decode_wireshark.read_pcap_pcapng(r"Input_for_tests\pcap\2_trames_with_17_messages_each.pcapng")
+        dissector = decode_wireshark.PcapDissector(pcap_file=r"Input_for_tests\pcap\2_trames_with_17_messages_each.pcapng")
+        dissector.load_capture()
+        dissector.fill_dictionnary_for_each_packet()
 
         pass
