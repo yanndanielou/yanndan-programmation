0b7e9545e5f6fe313dd129db798263e7cd7bb569
yanndu91 <yanndanielou@users.noreply.github.com>
2025-10-07 08:28:32 +0200

    Update __install_all.bat

--- Java/ExternalLibraries/__install_all.bat
+++ Java/ExternalLibraries/__install_all.bat
@@ -5,11 +5,11 @@ set "exclude=__install_all.bat"
 
 
 for %%x in (*.bat)do (
-   rem echo %%x
+   echo %DATE% %TIME% Process %%x
    if %%x == __install_all.bat (
 	Echo Ingore current file %~nx0
    )   else (
-    Start %%x
+    CALL "%%x"
    )
 )
 
