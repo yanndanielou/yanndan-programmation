587d1360211a50b7e7550e71ea1501e4c69d1676
yanndu91 <yanndanielou@users.noreply.github.com>
2025-10-07 10:07:30 +0200

    Update install_external_common_libraries.bat

--- Python/install_external_common_libraries.bat
+++ Python/install_external_common_libraries.bat
@@ -66,7 +66,7 @@ rem does not work @call :INSTALL_AND_UPDATE_PYTHON_LIB lazyxml
 
 :INSTALL_AND_UPDATE_PYTHON_LIB
 @Title install and update python library %1
-@Echo install and update python library %1
+@Echo install and update python library %1 - %DATE% %TIME% 
 call %PYTHON_HOME%\python.exe -m pip install %1
 call %PYTHON_HOME%\python.exe -m pip install --upgrade %1
 @EXIT /B 0
@@ -74,14 +74,14 @@ call %PYTHON_HOME%\python.exe -m pip install --upgrade %1
 
 :INSTALL_PYTHON_LIB
 @Title install python library %1
-@Echo install python library %1
+@Echo install python library %1 - %DATE% %TIME% 
 call %PYTHON_HOME%\python.exe -m pip install %1
 @EXIT /B 0
 
 
 :UPDATE_PYTHON_LIB
 @Title Update python library %1
-@Echo Update python library %1
+@Echo Update python library %1 - %DATE% %TIME%
 call %PYTHON_HOME%\python.exe -m pip install --upgrade %1
 @EXIT /B 0
 
