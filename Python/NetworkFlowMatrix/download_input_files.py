716d8bfdea405bb2c36af4a81a76e891c948fcd9
yanndu91 <yanndanielou@users.noreply.github.com>
2025-10-10 21:31:57 +0200

    Update download_input_files.py

--- Python/NetworkFlowMatrix/download_input_files.py
+++ Python/NetworkFlowMatrix/download_input_files.py
@@ -21,8 +21,17 @@ if __name__ == "__main__":
                     file_to_download_url="https://rhapsody.siemens.net/livelink/livelink.exe?func=ll&objId=90870046&objAction=Download",
                     file_to_download_pattern="NExTEO-S-273000-02-0125-01 Dossier de Configuration Réseau STD Radio - V01-00 Annexe A*.xlsx",
                     file_move_after_download_action=download_utils.DownloadFileDetector.FileMoveAfterDownloadAction(
-                        final_path="Input_Downloaded/NExTEO-S-273000-02-0125-01 Dossier de Configuration Réseau STD Radio - V01-00 Annexe A.xlsx", retry_in_case_of_error=True
+                        final_path="Input_Downloaded/NExTEO-S-273000-02-0125-01 Dossier de Configuration Réseau STD Radio - V01-00 Annexe A.xlsx",
+                        retry_in_case_of_error=download_utils.DownloadFileDetector.RetryInCaseOfErrorAction(),
                     ),
-                )
+                ),
+                rhapsody_utils.DownloadFileInstruction(
+                    file_to_download_url="https://rhapsody.siemens.net/livelink/livelink.exe?func=ll&objId=91311347&objAction=Download",
+                    file_to_download_pattern="NExTEO-S-271000-02-0125-02 Dossier de Configuration Réseau Sol - V10-00 Annexe A*.xlsb",
+                    file_move_after_download_action=download_utils.DownloadFileDetector.FileMoveAfterDownloadAction(
+                        final_path="Input_Downloaded/NExTEO-S-271000-02-0125-02 Dossier de Configuration Réseau Sol - V10-00 Annexe A.xlsb",
+                        retry_in_case_of_error=download_utils.DownloadFileDetector.RetryInCaseOfErrorAction(),
+                    ),
+                ),
             ]
         )
