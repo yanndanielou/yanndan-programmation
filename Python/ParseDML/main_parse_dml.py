7a2d7e2ab2e0bbc965b97253ec46a4792f2c1136
yanndu91 <yanndanielou@users.noreply.github.com>
2025-10-19 19:43:59 +0200

    Update main_parse_dml.py

--- Python/ParseDML/main_parse_dml.py
+++ Python/ParseDML/main_parse_dml.py
@@ -1,8 +1,13 @@
 from logger import logger_config
 from parsedml import parse_dml
+from common import json_encoders
 import param
 
 if __name__ == "__main__":
 
     with logger_config.application_logger("ParseDML"):
         dml_file_content2 = parse_dml.DmlFileContent.Builder.build_with_excel_file(dml_excel_file_full_path=param.DML_FILE_WITH_USELESS_RANGES)
+        row_errors = dml_file_content2.could_not_be_parsed_because_error_rows
+        with open("logs/errors.txt", mode="w", encoding="utf-8") as error_file:
+            error_file.write("\n".join(str(row_errors)))
+        pass
