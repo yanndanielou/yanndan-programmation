6a5d2e3bf9b6aa1219e827ffd7299f44fb9661eb
yanndu91 <yanndanielou@users.noreply.github.com>
2025-10-19 19:47:48 +0200

    Update param.py

--- Python/ParseDML/param.py
+++ Python/ParseDML/param.py
@@ -3,5 +3,6 @@ import os
 DEFAULT_DOWNLOAD_DIRECTORY = os.path.expandvars(r"%userprofile%\downloads")
 
 DML_FILE_WITH_USELESS_RANGES = f"{DEFAULT_DOWNLOAD_DIRECTORY}\\DML_NEXTEO_ATS+_V14_with_useless_ranges_cleaned.xlsm"
+DML_FILE_CLEANED_FINAL = f"{DEFAULT_DOWNLOAD_DIRECTORY}\\DML_NEXTEO_ATS+_V14_cleaned_light.xlsm"
 
 USEFUL_DML_SHEET_NAME = "Database"
