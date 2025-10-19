f62bbdb28ddf312e1eff172348dee7c68b0e226d
yanndu91 <yanndanielou@users.noreply.github.com>
2025-10-19 20:52:30 +0200

    Update main_parse_dml.py

--- Python/ParseDML/main_parse_dml.py
+++ Python/ParseDML/main_parse_dml.py
@@ -3,11 +3,16 @@ from parsedml import parse_dml
 from common import json_encoders
 import param
 
+
+def dump_errors(dml_file_content_built: parse_dml.DmlFileContent) -> None:
+    row_errors = dml_file_content_built.could_not_be_parsed_because_error_rows
+    with open("logs/errors.txt", mode="w", encoding="utf-8") as error_file:
+        error_file.write("\n".join([str(row_error) for row_error in row_errors]))
+
+
 if __name__ == "__main__":
 
     with logger_config.application_logger("ParseDML"):
         dml_file_content_built = parse_dml.DmlFileContent.Builder.build_with_excel_file(dml_excel_file_full_path=param.DML_FILE_CLEANED_FINAL)
-        row_errors = dml_file_content_built.could_not_be_parsed_because_error_rows
-        with open("logs/errors.txt", mode="w", encoding="utf-8") as error_file:
-            error_file.write("\n".join([str(row_error) for row_error in row_errors]))
+        all_documents_that_have_several_titles = [document for document in dml_file_content_built.dml_documents if len(document.get_all_titles()) > 2]
         pass
