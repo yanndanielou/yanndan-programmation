83a77386a7c43384f054612b6d387915846d0ad3
yanndu91 <yanndanielou@users.noreply.github.com>
2025-10-21 22:30:16 +0200

    Update main_parse_dml.py

--- Python/ParseDML/main_parse_dml.py
+++ Python/ParseDML/main_parse_dml.py
@@ -4,8 +4,8 @@ from common import json_encoders
 import param
 
 
-def dump_errors(dml_file_content_built: parse_dml.DmlFileContent) -> None:
-    row_errors = dml_file_content_built.could_not_be_parsed_because_error_rows
+def dump_errors(dml_file_content_to_print: parse_dml.DmlFileContent) -> None:
+    row_errors = dml_file_content_to_print.could_not_be_parsed_because_error_rows
     with open("logs/errors.txt", mode="w", encoding="utf-8") as error_file:
         error_file.write("\n".join([str(row_error) for row_error in row_errors]))
 
@@ -14,5 +14,8 @@ if __name__ == "__main__":
 
     with logger_config.application_logger("ParseDML"):
         dml_file_content_built = parse_dml.DmlFileContent.Builder.build_with_excel_file(dml_excel_file_full_path=param.DML_FILE_CLEANED_FINAL)
-        all_documents_that_have_several_titles = [document for document in dml_file_content_built.dml_documents if len(document.get_all_titles()) > 2]
+        all_documents_that_have_several_titles = [document for document in dml_file_content_built.dml_documents if len(document.get_all_titles()) > 1]
+        pass
+
+        all_documents_that_have_several_references = [document for document in dml_file_content_built.dml_documents if len(document.get_all_code_ged_moes()) > 1]
         pass
