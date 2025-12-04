7d0b2ab5e1f6fa477c3f321031f2f4af1f7b7c85
yanndu91 <yanndanielou@users.noreply.github.com>
2025-12-04 23:14:07 +0100

    Update main.py

--- Python/NetworkFlowMatrix/main.py
+++ Python/NetworkFlowMatrix/main.py
@@ -56,13 +56,13 @@ if __name__ == "__main__":
 
         radio_std_conf_file = network_conf_files.NetworkConfFile.Builder.build_with_excel_file(
             equipments_library=equipments_library,
-            excel_file_full_path=network_conf_files_descriptions_data.StdRadioNetworkConfV2Description().excel_file_full_name,
+            excel_file_full_path=network_conf_files_descriptions_data.StdRadioNetworkConfV2Description().excel_file_full_path,
             equipment_definition_tabs=network_conf_files_descriptions_data.StdRadioNetworkConfV2Description().all_tabs_definition,
         )
 
         sol_std_conf_file = network_conf_files.NetworkConfFile.Builder.build_with_excel_file(
             equipments_library=equipments_library,
-            excel_file_full_path=network_conf_files_descriptions_data.SolStdNetworkConfV11Description().excel_file_full_name,
+            excel_file_full_path=network_conf_files_descriptions_data.SolStdNetworkConfV11Description().excel_file_full_path,
             equipment_definition_tabs=network_conf_files_descriptions_data.SolStdNetworkConfV11Description().all_tabs_definition,
         )
 
