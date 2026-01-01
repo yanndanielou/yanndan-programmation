7cb3bdb39f41f2d510a0facba9fb6c5b9992e536
yanndu91 <yanndanielou@users.noreply.github.com>
2026-01-01 20:09:07 +0100

    Update main.py

--- Python/NetworkFlowMatrix/main.py
+++ Python/NetworkFlowMatrix/main.py
@@ -34,7 +34,7 @@ def equipment_to_dict(equipment: network_matrix_data_model.EquipmentInFLowMatrix
     return new_var
 
 
-def dump_subsystems_to_json(network_flow_matrix_to_dump: network_matrix_data_model.NetworkFlowMatrix, filepath: str) -> None:
+def dump_matrix_subsystems_to_json(network_flow_matrix_to_dump: network_matrix_data_model.NetworkFlowMatrix, filepath: str) -> None:
     data = [
         subsystem_to_dict(subsystem, with_equipment=True) for subsystem in sorted(network_flow_matrix_to_dump.all_matrix_flow_subsystems_definitions_instances, key=lambda subsystem: subsystem.name)
     ]
@@ -42,7 +42,7 @@ def dump_subsystems_to_json(network_flow_matrix_to_dump: network_matrix_data_mod
         json.dump(data, f, indent=2, ensure_ascii=False)
 
 
-def dump_equipments_to_json(network_flow_matrix_to_dump: network_matrix_data_model.NetworkFlowMatrix, filepath: str) -> None:
+def dump_matrix_equipments_to_json(network_flow_matrix_to_dump: network_matrix_data_model.NetworkFlowMatrix, filepath: str) -> None:
     data = [
         equipment_to_dict(equipment, with_subsystem=True) for equipment in sorted(network_flow_matrix_to_dump.all_matrix_flow_equipments_definitions_instances, key=lambda subsystem: subsystem.name)
     ]
@@ -50,27 +50,21 @@ def dump_equipments_to_json(network_flow_matrix_to_dump: network_matrix_data_mod
         json.dump(data, f, indent=2, ensure_ascii=False)
 
 
-def build_
-
-
 if __name__ == "__main__":
     with logger_config.application_logger("networkflowmatrix"):
         equipments_library = equipments.NetworkConfFilesEquipmentsLibrary()
 
         radio_std_conf_file = network_conf_files.NetworkConfFile.Builder.build_with_excel_description(
-            equipments_library=equipments_library,
-            excel_file_full_path=network_conf_files_descriptions_data.StdRadioNetworkConfV2Description()
+            equipments_library=equipments_library, excel_description=network_conf_files_descriptions_data.StdRadioNetworkConfV2Description()
         )
 
         sol_std_conf_file = network_conf_files.NetworkConfFile.Builder.build_with_excel_description(
-            equipments_library=equipments_library,
-            excel_file_full_path=network_conf_files_descriptions_data.SolStdNetworkConfV11Description()
+            equipments_library=equipments_library, excel_description=network_conf_files_descriptions_data.SolStdNetworkConfV11Description()
         )
 
-        airlink_radio_layout_conf_file = network_conf_files.NetworkConfFile.Builder.build_with_excel_description(
-            equipments_library=equipments_library,
-            excel_file_full_path=network_conf_files_descriptions_data.RadioLayoutR841Description()
-        )
+        # airlink_radio_layout_conf_file = network_conf_files.NetworkConfFile.Builder.build_with_excel_description(
+        #    equipments_library=equipments_library, excel_description=network_conf_files_descriptions_data.RadioLayoutR841Description()
+        # )
 
         sith_conf_file = sith_equipments.SithConfFile.Builder.build(equipments_library=equipments_library)
 
@@ -86,11 +80,13 @@ if __name__ == "__main__":
         for directory_path in [OUTPUT_PARENT_DIRECTORY_NAME]:
             file_utils.create_folder_if_not_exist(directory_path)
 
-        dump_subsystems_to_json(network_flow_matrix, f"{OUTPUT_PARENT_DIRECTORY_NAME}/all_subsystems_in_flow_matrix.json")
-        dump_equipments_to_json(network_flow_matrix, f"{OUTPUT_PARENT_DIRECTORY_NAME}/all_equipments_in_flow_matrix.json")
+        dump_matrix_subsystems_to_json(network_flow_matrix, f"{OUTPUT_PARENT_DIRECTORY_NAME}/matrix_all_subsystems_in_flow_matrix.json")
+        dump_matrix_equipments_to_json(network_flow_matrix, f"{OUTPUT_PARENT_DIRECTORY_NAME}/matrix_all_equipments_in_flow_matrix.json")
+
+        equipments_library.dump_to_json_file(f"{OUTPUT_PARENT_DIRECTORY_NAME}/all_equipments_in_conf_files.json")
 
         json_encoders.JsonEncodersUtils.serialize_list_objects_in_json(
-            network_flow_matrix.all_equipments_names_with_subsystem, f"{OUTPUT_PARENT_DIRECTORY_NAME}/data_model.all_equipments_names_with_subsystem.json"
+            network_flow_matrix.all_equipments_names_with_subsystem, f"{OUTPUT_PARENT_DIRECTORY_NAME}/matrix_all_equipments_names_with_subsystem.json"
         )
 
         network_line = network_flow_matrix.get_line_by_identifier(1970)
