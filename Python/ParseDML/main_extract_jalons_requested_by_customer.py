dab21cd60986d2133c34dda9a282dfad345faa15
yanndu91 <yanndanielou@users.noreply.github.com>
2026-01-06 06:34:32 +0100

    Update main_extract_jalons_requested_by_customer.py

--- Python/ParseDML/main_extract_jalons_requested_by_customer.py
+++ Python/ParseDML/main_extract_jalons_requested_by_customer.py
@@ -17,7 +17,6 @@ def split_jalons_raw_to_jalon_names_list(jalons_raw: str) -> List[str]:
 
     jalon_with_unique_separator = (
         jalons_raw.strip()
-        .replace(" ", MAGIC_VALUE_SEPARATOR)
         .replace("\n", MAGIC_VALUE_SEPARATOR)
         .replace("\\", MAGIC_VALUE_SEPARATOR)
         .replace("/", MAGIC_VALUE_SEPARATOR)
@@ -27,14 +26,30 @@ def split_jalons_raw_to_jalon_names_list(jalons_raw: str) -> List[str]:
 
     assert MAGIC_VALUE_SEPARATOR + MAGIC_VALUE_SEPARATOR not in jalon_with_unique_separator, f"Could not handle raw jalon {jalons_raw}"
 
-    jalons_list = jalon_with_unique_separator.split(MAGIC_VALUE_SEPARATOR)
+    jalons_list = [jalon.strip() for jalon in jalon_with_unique_separator.split(MAGIC_VALUE_SEPARATOR)]
+
+    for jalon in jalons_list:
+        assert MAGIC_VALUE_SEPARATOR not in jalon
+
     return jalons_list
 
 
-class DocLine:
-    def __init__(self, code_moe_ged_raw: str, jalons_raw: Any) -> None:
+class Doc:
+    def __init__(self, code_moe_ged_raw: str) -> None:
         self.code_ged_moe = code_moe_ged_raw
+        self.unique_jalons: Set[Jalon] = set()
+
+
+class DocLine:
+    def __init__(self, code_moe_ged_raw: str, jalons_raw: Any, doc: Doc) -> None:
+        self.code_ged_moe = code_moe_ged_raw
+        self.doc = doc
         self.jalons_raw = jalons_raw
+        self._jalons: List[Jalon] = []
+
+    def add_jalon(self, jalon: "Jalon") -> None:
+        self._jalons.append(jalon)
+        self.doc.unique_jalons.add(jalon)
 
 
 class Jalon:
@@ -43,13 +58,14 @@ class Jalon:
         self.docs_codes_ged_moe: Set[str] = set()
 
         assert self.name
-        assert " " not in self.name
 
         logger_config.print_and_log_info(f"Create jalon {self.name}")
 
 
 def main() -> None:
     all_jalons: List[Jalon] = []
+    all_doc_lines: List[DocLine] = []
+    all_docs: List[Doc] = []
 
     with logger_config.application_logger("extract_jalons_requested_by_customer"):
 
@@ -63,7 +79,15 @@ def main() -> None:
 
                 assert isinstance(code_moe_ged_raw, str)
 
-                doc_line = DocLine(code_moe_ged_raw, jalons_raw)
+                doc_already_created_with_ref = [doc_it for doc_it in all_docs if doc_it.code_ged_moe == code_moe_ged_raw]
+                if doc_already_created_with_ref:
+                    doc = doc_already_created_with_ref[0]
+                else:
+                    doc = Doc(code_moe_ged_raw)
+                    all_docs.append(doc)
+
+                doc_line = DocLine(code_moe_ged_raw, jalons_raw, doc)
+                all_doc_lines.append(doc_line)
                 jalons_names_in_current_line = split_jalons_raw_to_jalon_names_list(jalons_raw)
 
                 for jalon_name in jalons_names_in_current_line:
@@ -74,21 +98,35 @@ def main() -> None:
                     else:
                         jalon = [jalon_found for jalon_found in all_jalons if jalon_found.name == jalon_name][0]
 
-                    logger_config.print_and_log_info(f"Add doc {code_moe_ged_raw} to jalon {jalon_name}")
+                    # logger_config.print_and_log_info(f"Add doc {code_moe_ged_raw} to jalon {jalon_name}")
                     jalon.docs_codes_ged_moe.add(doc_line.code_ged_moe)
+                    doc_line.add_jalon(jalon)
 
     logger_config.print_and_log_info(f"{len(all_jalons)} jalons found: \n{'\n'.join([jalon.name for jalon in all_jalons])}")
+    logger_config.print_and_log_info(f"{len(all_jalons)} jalons found")
 
     assert all_jalons
     for jalon in all_jalons:
         assert jalon.name
         assert jalon.docs_codes_ged_moe, f"Jalon {jalon.name} has no doc"
-        assert " " not in jalon.name
+
+    assert all_docs
+    assert all_doc_lines
 
     for directory_path in [OUTPUT_PARENT_DIRECTORY_NAME]:
         file_utils.create_folder_if_not_exist(directory_path)
 
-    json_encoders.JsonEncodersUtils.serialize_list_objects_in_json(all_jalons, f"{OUTPUT_PARENT_DIRECTORY_NAME}/all_jalons_{file_name_utils.get_file_suffix_with_current_datetime()}.json")
+    json_encoders.JsonEncodersUtils.serialize_list_objects_in_json(all_jalons, f"{OUTPUT_PARENT_DIRECTORY_NAME}/{file_name_utils.get_file_suffix_with_current_datetime()}_all_jalons.json")
+
+    json_encoders.JsonEncodersUtils.serialize_list_objects_in_json(
+        [(doc.code_ged_moe, [jalon.name for jalon in doc.unique_jalons]) for doc in all_docs if doc.unique_jalons],
+        f"{OUTPUT_PARENT_DIRECTORY_NAME}/{file_name_utils.get_file_suffix_with_current_datetime()}_all_docs_with_jalons.json",
+    )
+
+    json_encoders.JsonEncodersUtils.serialize_list_objects_in_json(
+        [doc.code_ged_moe for doc in all_docs if not doc.unique_jalons],
+        f"{OUTPUT_PARENT_DIRECTORY_NAME}/{file_name_utils.get_file_suffix_with_current_datetime()}_all_docs_without_jalons.json",
+    )
 
 
 if __name__ == "__main__":
