7e28b820a95877b518e6d03d9224be45f16cf31b
yanndu91 <yanndanielou@users.noreply.github.com>
2026-01-15 21:43:50 +0100

    Update network_matrix_data_model.py

--- Python/NetworkFlowMatrix/networkflowmatrix/network_matrix_data_model.py
+++ Python/NetworkFlowMatrix/networkflowmatrix/network_matrix_data_model.py
@@ -337,7 +337,7 @@ class FlowDestination(FlowEndPoint):
 
     def __post_init__(self) -> None:
         if self.cast_type == constants.CastType.MULTICAST:
-            assert isinstance(self.group_multicast_raw, str), self
+            assert isinstance(self.group_multicast_raw, str), f"{self.matrix_line_identifier} : empty group {self.group_multicast_raw} for multicast {self.cast_type}"
 
 
 @dataclass
