906b4d9d605532e92cea059112d8fe7c2ec7da00
yanndu91 <yanndanielou@users.noreply.github.com>
2026-01-28 17:07:59 +0100

    Update main_pai_analyzis.py

--- Python/STSLogAnalyzis/main_pai_analyzis.py
+++ Python/STSLogAnalyzis/main_pai_analyzis.py
@@ -22,7 +22,7 @@ with logger_config.application_logger("main_pai_analyzis"):
     cck_libary = decode_cck.CckMproTraceLibrary().load_folder(r"C:\Users\fr232487\Downloads\Traces_MPRO1_1_20260125_15_site_ccu")
     cck_libary.plot_problems_and_loss_link_by_period(output_folder_path=OUTPUT_FOLDER_NAME, interval_minutes=2, do_show=False)
 
-    enchainement_protocolaire_lines = [line for line in cck_libary.all_processed_lines if "le msg a un problème de 'enchainement numero protocolaire'" in line.full_raw_line]
+    enchainement_protocolaire_lines = [line for line in cck_libary.all_processed_lines if line.problem_enchainement_numero_protocolaire]
     logger_config.print_and_log_info(f"{len(enchainement_protocolaire_lines)} problèmes de enchainement numero protocolaire")
     decode_cck.save_cck_mpro_lines_in_excel(
         trace_lines=enchainement_protocolaire_lines, output_folder_path=OUTPUT_FOLDER_NAME, excel_output_file_name_without_extension="Problèmes enchainement numéro protocolaire"
@@ -30,6 +30,7 @@ with logger_config.application_logger("main_pai_analyzis"):
     decode_cck.plot_bar_graph_list_cck_mpro_lines_by_period(
         trace_lines=enchainement_protocolaire_lines, output_folder_path=OUTPUT_FOLDER_NAME, label="enchainement_protocolaire_lines", interval_minutes=2
     )
+
     pass
     plt.show()
 
