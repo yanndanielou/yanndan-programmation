2efc598df144dc64eddba602fe8c9eff6c023092
yanndu91 <yanndanielou@users.noreply.github.com>
2026-01-31 09:20:24 +0100

    Update main_pai_analyzis_TT.py

--- Python/STSLogAnalyzis/main_pai_analyzis_TT.py
+++ Python/STSLogAnalyzis/main_pai_analyzis_TT.py
@@ -23,7 +23,7 @@ with logger_config.application_logger("main_pai_analyzis_TT"):
         r"D:\temp\2027-01-27 avec hitachi\2027-01-27\PAI75\TT-026401 (TT)\Archives_maint"
     )
     for tt_maint_library in [tt_maint_p75_library, tt_maint_p81_library]:
-        tt_maint_library.dump_all_events_to_text_file(output_folder_path=OUTPUT_FOLDER_NAME, equipment_names_to_ignore=["81"])
+        tt_maint_library.dump_all_events_to_text_file(output_folder_path=OUTPUT_FOLDER_NAME)
         tt_maint_library.export_equipments_with_alarms_to_excel(output_folder_path=OUTPUT_FOLDER_NAME, equipment_names_to_ignore=["81"])
         tt_maint_library.plot_back_to_past_by_period(output_folder_path=OUTPUT_FOLDER_NAME, interval_minutes=60, do_show=False)
         tt_maint_library.plot_sahara_alarms_by_period(output_folder_path=OUTPUT_FOLDER_NAME, interval_minutes=60, do_show=False)
