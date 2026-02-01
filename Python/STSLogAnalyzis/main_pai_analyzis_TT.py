f4e4b24a49f492e54d3f72c363b3f56ffa4169e5
yanndu91 <yanndanielou@users.noreply.github.com>
2026-02-01 14:28:19 +0100

    Update main_pai_analyzis_TT.py

--- Python/STSLogAnalyzis/main_pai_analyzis_TT.py
+++ Python/STSLogAnalyzis/main_pai_analyzis_TT.py
@@ -18,12 +18,17 @@ OUTPUT_FOLDER_NAME = "output"
 with logger_config.application_logger("main_pai_analyzis_TT"):
     file_utils.create_folder_if_not_exist(OUTPUT_FOLDER_NAME)
 
+    tt_maint_p75_light_library = decode_pai_logs.TerminalTechniqueArchivesMaintLibrary("2027-01-27 P75 light_").load_folder(
+        r"D:\temp\2027-01-27 avec hitachi\2027-01-27\PAI75\TT-026401 (TT)\Archives_maint_light"
+    )
+
     tt_maint_p81_library = decode_pai_logs.TerminalTechniqueArchivesMaintLibrary("2027-01-27 P81 TT-026396").load_folder(r"D:\temp\2027-01-27 avec hitachi\2027-01-27\PAI81\TT-026396\Archives_maint")
     tt_maint_p75_library = decode_pai_logs.TerminalTechniqueArchivesMaintLibrary("2027-01-27 P75 TT-026396").load_folder(
         r"D:\temp\2027-01-27 avec hitachi\2027-01-27\PAI75\TT-026401 (TT)\Archives_maint"
     )
-    for tt_maint_library in [tt_maint_p81_library, tt_maint_p75_library]:
+    for tt_maint_library in [tt_maint_p75_light_library, tt_maint_p81_library, tt_maint_p75_library]:
         tt_maint_library.dump_all_events_to_text_file(output_folder_path=OUTPUT_FOLDER_NAME)
+        tt_maint_library.export_mesd_alarms_groups_to_excel(output_folder_path=OUTPUT_FOLDER_NAME)
         tt_maint_library.export_sahara_alarms_with_context_to_excel(output_folder_path=OUTPUT_FOLDER_NAME)
         tt_maint_library.export_equipments_with_alarms_to_excel(output_folder_path=OUTPUT_FOLDER_NAME, equipment_names_to_ignore=["81"])
         tt_maint_library.plot_back_to_past_by_period(output_folder_path=OUTPUT_FOLDER_NAME, interval_minutes=60, do_show=False)
