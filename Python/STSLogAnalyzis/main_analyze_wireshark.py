7c01ad80a24b45e3e6b22243ba0e065ee42fe746
yanndu91 <yanndanielou@users.noreply.github.com>
2026-02-02 12:08:07 +0100

    example pcap

--- Python/STSLogAnalyzis/main_analyze_wireshark.py
+++ Python/STSLogAnalyzis/main_analyze_wireshark.py
@@ -3,7 +3,7 @@ from logger import logger_config
 
 with logger_config.application_logger():
 
-    dissector = decode_wireshark.PcapDissector(pcap_file=r"Input_for_tests/pcap/SAAT_1_LINE.pcapng")
+    dissector = decode_wireshark.PcapDissector(pcap_file=r"Input_for_tests/pcap/SAAT_2_LINES.pcapng")
     dissector.load_capture()
     dissector.print_packets_with_pretty_print()
     dissector.fill_dictionnary_for_each_packet()
