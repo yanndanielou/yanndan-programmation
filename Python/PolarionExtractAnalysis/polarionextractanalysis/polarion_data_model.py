b38124fd1afb53e5e4c71dc54c1eb6e06cda6a28
yanndu91 <yanndanielou@users.noreply.github.com>
2026-02-10 20:48:15 +0100

    Update polarion_data_model.py

--- Python/PolarionExtractAnalysis/polarionextractanalysis/polarion_data_model.py
+++ Python/PolarionExtractAnalysis/polarionextractanalysis/polarion_data_model.py
@@ -105,7 +105,7 @@ class PolarionWorkItemLibrary:
         self.polarion_library = polarion_library
         self.all_work_items: List[PolarionWorkItem] = []
         self.all_not_parsed_because_errors_work_items_as_json: List[Dict] = []
-        self.all_work_items_by_type: Dict[PolarionAttributeType, List[PolarionWorkItem]] = []
+        self.all_work_items_by_type: Dict[PolarionAttributeType, List[PolarionWorkItem]] = {}
 
         with logger_config.stopwatch_with_label(f"Library {input_json_file_path} creation"):
 
