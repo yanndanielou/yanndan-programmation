090c1071ff613c6df9be34c762549468b1a42b32
yanndu91 <yanndanielou@users.noreply.github.com>
2026-02-10 21:04:05 +0100

    Update create_graphs.py

--- Python/PolarionExtractAnalysis/polarionextractanalysis/create_graphs.py
+++ Python/PolarionExtractAnalysis/polarionextractanalysis/create_graphs.py
@@ -44,16 +44,18 @@ def create_baregraph_work_item_number_by_status_by_type(output_directory_path: s
                 [
                     {
                         "Status": status.name,
-                        "Number of work items": len([work_item for work_item in work_items if work_item.attributes.status == status]),
+                        f"Number of work items {work_item_type}": len([work_item for work_item in work_items if work_item.attributes.status == status]),
                     }
                     for status in found_status
                 ]
             )
-            df.plot.bar(use_index=True, x="Status", y="Number of work items", rot=90, color="#9a95d1")
+            df.plot.bar(use_index=True, x="Status", y=f"Number of work items {work_item_type}", rot=90, color="#9a95d1")
             common_output_label = f"baregraph_work_item_{work_item_type.name}_per_status"
             matplotlib.pyplot.savefig(f"{output_directory_path}/{common_output_label}{file_name_utils.get_file_suffix_with_current_datetime()}.png")
 
             # Create HTML page with Plotly bar graph
-            fig = go.Figure(data=[go.Bar(x=df["Status"], y=df["Number of work items"], marker=dict(color="#9a95d1"), width=0.1)])
-            fig.update_layout(title="Work Items per User", xaxis_title="Status", yaxis_title="Number of work items", hovermode="x unified")
+            num_bars = len(df)
+            figure_width = max(800, num_bars * 100)
+            fig = go.Figure(data=[go.Bar(x=df["Status"], y=df[f"Number of work items {work_item_type}"], marker=dict(color="#9a95d1"), width=0.1)])
+            fig.update_layout(title=f"{work_item_type} per status", xaxis_title="Status", yaxis_title="Number of work items", hovermode="x unified", width=figure_width)
             fig.write_html(f"{output_directory_path}/{common_output_label}{file_name_utils.get_file_suffix_with_current_datetime()}.html")
