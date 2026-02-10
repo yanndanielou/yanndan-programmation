54349fff4668888807b177b6eee43c71c833e1f6
yanndu91 <yanndanielou@users.noreply.github.com>
2026-02-10 21:08:31 +0100

    Update main.py

--- Python/PolarionExtractAnalysis/main.py
+++ Python/PolarionExtractAnalysis/main.py
@@ -23,6 +23,7 @@ def main() -> None:
 
         create_graphs.create_baregraph_work_item_per_user(output_directory_path=OUTPUT_DIRECTORY_NAME, users_library=polarion_library.users_library)
         create_graphs.create_baregraph_work_item_number_by_status_by_type(output_directory_path=OUTPUT_DIRECTORY_NAME, polarion_library=polarion_library)
+        create_graphs.create_baregraph_work_item_number_cumulative_by_status(output_directory_path=OUTPUT_DIRECTORY_NAME, polarion_library=polarion_library)
 
         dump_to_excel.dump_companies_to_excel_file(users_library=polarion_library.users_library, output_directory_path=OUTPUT_DIRECTORY_NAME)
         dump_to_excel.dump_users_to_excel_file(users_library=polarion_library.users_library, output_directory_path=OUTPUT_DIRECTORY_NAME)
