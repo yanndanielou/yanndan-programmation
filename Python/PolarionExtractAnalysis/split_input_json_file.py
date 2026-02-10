c27cf1d6e4f393a47958db818792305796b0cbc6
yanndu91 <yanndanielou@users.noreply.github.com>
2026-02-10 17:39:36 +0100

    Update split_input_json_file.py

--- Python/PolarionExtractAnalysis/split_input_json_file.py
+++ Python/PolarionExtractAnalysis/split_input_json_file.py
@@ -1,8 +1,5 @@
-import json
-import os
 from common import json_utils
 from logger import logger_config
-
 
 # Exemple d'utilisation
 if __name__ == "__main__":
@@ -15,14 +12,14 @@ if __name__ == "__main__":
         json_utils.split_json_files_containing_list_to_files(
             input_file_path=r"C:\Users\fr232487\Downloads\Extraction_POLARION_User.json",
             output_directory=output_dir,
-            ouput_file_prefix="user_",
+            output_file_prefix="user_",
             attribute_to_use_in_file_name="id",
             pretty_print=True,
         )
         json_utils.split_json_files_containing_list_to_files(
             input_file_path=r"C:\Users\fr232487\Downloads\Extraction_POLARION_Full.json",
             output_directory=output_dir,
-            ouput_file_prefix="work_item_",
+            output_file_prefix="work_item_",
             attribute_to_use_in_file_name="id",
             pretty_print=True,
         )
