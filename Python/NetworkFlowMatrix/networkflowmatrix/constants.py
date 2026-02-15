455d6f90b41f7c264ed75cb1149a16de57209fb3
yanndu91 <yanndanielou@users.noreply.github.com>
2026-02-15 10:50:22 +0100

    handle ProdMigrationEssai

--- Python/NetworkFlowMatrix/networkflowmatrix/constants.py
+++ Python/NetworkFlowMatrix/networkflowmatrix/constants.py
@@ -13,6 +13,12 @@ class MatrixFlowModificationType(Enum):
     M = "MODIFIED"
 
 
+class ProdMigrationEssai(Enum):
+    P = "PRODUCTION"
+    M = "MIGRATION"
+    ET = "ESSAIS_TEMPORAIRE"
+
+
 ALL_USED_TRAINS_IDS = list(range(1, 131 + 1))
 TO_IGNORE_TRAINS_IDS = [511, 2047]
 
