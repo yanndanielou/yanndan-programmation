9f74c193a8d0acf89337e1e708fca03913d33ef2
yanndu91 <yanndanielou@users.noreply.github.com>
2026-03-20 07:52:16 +0100

    Update check_mesures_arssi_doors.py

--- Python/Sandbox/check_mesures_arssi_doors.py
+++ Python/Sandbox/check_mesures_arssi_doors.py
@@ -1,13 +1,48 @@
-from logger import logger_config
-import pandas
+from dataclasses import dataclass
 from typing import List
-from dataclasses import dataclass, field
+
+import pandas
+from logger import logger_config
 
 
 @dataclass
 class Mesure:
     arssi_id: str
     full_text: str
+
+    def __post_init__(self) -> None:
+        self.cleaned_text = self.full_text.replace("�uvre", "oeuvre").replace("�", "")
+
+
+def perform_check(parsed_mesures: List[Mesure], other_source_mesures: List[Mesure], parse_mesure_label: str, other_source_mesure_label: str) -> None:
+
+    for parsed_mesure in parsed_mesures:
+        found_other_source_mesures_by_arssi_id = [other_source_mesure for other_source_mesure in other_source_mesures if other_source_mesure.arssi_id == parsed_mesure.arssi_id]
+
+        if len(found_other_source_mesures_by_arssi_id) > 1:
+            logger_config.print_and_log_error(
+                f"{len(found_other_source_mesures_by_arssi_id)} {other_source_mesure_label} mesures found with id {parsed_mesure.arssi_id}: {found_other_source_mesures_by_arssi_id}"
+            )
+
+        assert len(found_other_source_mesures_by_arssi_id) < 2
+        found_other_source_mesure_by_arssi_id = found_other_source_mesures_by_arssi_id[0] if found_other_source_mesures_by_arssi_id else None
+
+        found_other_source_mesures_by_full_text = [other_source_mesure for other_source_mesure in other_source_mesures if other_source_mesure.cleaned_text == parsed_mesure.cleaned_text]
+        if len(found_other_source_mesures_by_full_text) > 1:
+            logger_config.print_and_log_error(
+                f"{len(found_other_source_mesures_by_full_text)} {other_source_mesure_label} mesures found with text {parsed_mesure.cleaned_text}: {found_other_source_mesures_by_full_text}"
+            )
+
+        if not found_other_source_mesures_by_full_text:
+            logger_config.print_and_log_error(
+                f"Could not find {parse_mesure_label} text \n{parsed_mesure.cleaned_text} in {other_source_mesure_label}.\nfound\n{found_other_source_mesure_by_arssi_id.cleaned_text if found_other_source_mesure_by_arssi_id else None}\nin {other_source_mesure_label} for {parsed_mesure.arssi_id}"
+            )
+
+        found_other_source_mesure_by_full_text = found_other_source_mesures_by_full_text[0] if found_other_source_mesures_by_full_text else None
+
+        logger_config.print_and_log_info(
+            f"Searching {parse_mesure_label} mesure {parsed_mesure}\nFound by ID {parsed_mesure.arssi_id}:{found_other_source_mesure_by_arssi_id is not None},\nFound by text {parsed_mesure.cleaned_text}:{found_other_source_mesure_by_full_text is not None}\n"
+        )
 
 
 with logger_config.application_logger():
@@ -23,51 +58,25 @@ with logger_config.application_logger():
         arssi_id = row["REQ Imported ID"]
         full_text_doors = row["Allocation Mesures Sous-système"]
         doors_mesures.append(Mesure(arssi_id=arssi_id, full_text=full_text_doors))
-    logger_config.print_and_log_info(f"{len(doors_mesures)} doors_mesures")
+    logger_config.print_and_log_info(f"{len(doors_mesures)} doors_mesures\n{'\n'.join(str(doors_mesures))}")
 
     for index, (_, row) in enumerate(arssi_v8_doors_data_frame.iterrows()):
         arssi_id = row["ID"]
         full_text = row["Description"]
         arssi_v8_mesures.append(Mesure(arssi_id=arssi_id, full_text=full_text))
 
-    logger_config.print_and_log_info(f"{len(arssi_v8_mesures)} arssi_v8_mesures")
+    logger_config.print_and_log_info(f"{len(arssi_v8_mesures)} arssi_v8_mesures\n{'\n'.join(str(arssi_v8_mesures))}")
 
-    for arssi_mesure in arssi_v8_mesures:
-        found_doors_mesures_by_arssi_id = [doors_mesure for doors_mesure in doors_mesures if doors_mesure.arssi_id == arssi_mesure.arssi_id]
-        assert len(found_doors_mesures_by_arssi_id) < 2
-        found_doors_mesure_by_arssi_id = found_doors_mesures_by_arssi_id[0] if found_doors_mesures_by_arssi_id else None
+    perform_check(
+        parsed_mesures=arssi_v8_mesures,
+        other_source_mesures=doors_mesures,
+        parse_mesure_label="arssi v8",
+        other_source_mesure_label="doors",
+    )
 
-        found_doors_mesures_by_full_text = [doors_mesure for doors_mesure in doors_mesures if doors_mesure.full_text == arssi_mesure.full_text]
-        if len(found_doors_mesures_by_full_text) > 1:
-            logger_config.print_and_log_error(f"{len(found_doors_mesures_by_full_text)} doors mesures found with text {arssi_mesure.full_text}: {found_doors_mesures_by_full_text}")
-
-        if not found_doors_mesures_by_full_text:
-            logger_config.print_and_log_error(
-                f"Could not find ARSSI text {arssi_mesure.full_text} in doors. found {found_doors_mesure_by_arssi_id.full_text if found_doors_mesure_by_arssi_id else None} in doors for {arssi_mesure.arssi_id}"
-            )
-
-        found_doors_mesure_by_full_text = found_doors_mesures_by_full_text[0] if found_doors_mesures_by_full_text else None
-
-        logger_config.print_and_log_info(
-            f"Searching arssi mesure {arssi_mesure}\nFound by ID {arssi_mesure.arssi_id}:{found_doors_mesure_by_arssi_id is not None},\nFound by text {arssi_mesure.full_text}:{found_doors_mesure_by_full_text is not None}\n"
-        )
-
-    for arssi_mesure in arssi_v8_mesures:
-        found_doors_mesures_by_arssi_id = [doors_mesure for doors_mesure in doors_mesures if doors_mesure.arssi_id == arssi_mesure.arssi_id]
-        assert len(found_doors_mesures_by_arssi_id) < 2
-        found_doors_mesure_by_arssi_id = found_doors_mesures_by_arssi_id[0] if found_doors_mesures_by_arssi_id else None
-
-        found_doors_mesures_by_full_text = [doors_mesure for doors_mesure in doors_mesures if doors_mesure.full_text == arssi_mesure.full_text]
-        if len(found_doors_mesures_by_full_text) > 1:
-            logger_config.print_and_log_error(f"{len(found_doors_mesures_by_full_text)} doors mesures found with text {arssi_mesure.full_text}: {found_doors_mesures_by_full_text}")
-
-        if not found_doors_mesures_by_full_text:
-            logger_config.print_and_log_error(
-                f"Could not find ARSSI text {arssi_mesure.full_text} in doors. found {found_doors_mesure_by_arssi_id.full_text if found_doors_mesure_by_arssi_id else None} in doors for {arssi_mesure.arssi_id}"
-            )
-
-        found_doors_mesure_by_full_text = found_doors_mesures_by_full_text[0] if found_doors_mesures_by_full_text else None
-
-        logger_config.print_and_log_info(
-            f"Searching arssi mesure {arssi_mesure}\nFound by ID {arssi_mesure.arssi_id}:{found_doors_mesure_by_arssi_id is not None},\nFound by text {arssi_mesure.full_text}:{found_doors_mesure_by_full_text is not None}\n"
-        )
+    perform_check(
+        parsed_mesures=doors_mesures,
+        other_source_mesures=arssi_v8_mesures,
+        parse_mesure_label="doors",
+        other_source_mesure_label="arssi v8",
+    )
