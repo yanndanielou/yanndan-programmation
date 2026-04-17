32c3b902c802c70c3c33e3a1a1732f9c3c7472ad
yanndu91 <yanndanielou@users.noreply.github.com>
2026-04-17 08:29:43 +0200

    Update json_encoders.py

--- Python/Common/src/common/json_encoders.py
+++ Python/Common/src/common/json_encoders.py
@@ -1,6 +1,6 @@
 # -*-coding:Utf-8 -*
 """json encoders"""
-
+import textwrap
 import json
 from json import JSONEncoder
 from typing import Any
@@ -34,3 +34,21 @@ class JsonEncodersUtils(metaclass=singleton.Singleton):
             json_file.write(result_json_dump)
 
             json_file.close()
+
+    @staticmethod
+    def serialize_list_objects_in_json_improved(list_objects: list[Any], json_file_full_path: str) -> None:
+
+        with logger_config.stopwatch_with_label(f"Serialize {len(list_objects)} in {json_file_full_path}"):
+            with open(json_file_full_path, "w", encoding="utf-8") as json_file:
+                json_file.write("[\n")
+                first_item = True
+
+                for item in list_objects:
+                    if not first_item:
+                        json_file.write(",\n")
+
+                    item_json = json.dumps(item, indent=4, cls=ListOfObjectsEncoder, ensure_ascii=False)
+                    json_file.write(textwrap.indent(item_json, "    "))
+                    first_item = False
+
+                json_file.write("\n]" if not first_item else "]")
