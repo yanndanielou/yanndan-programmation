45d068ce94bbffdbd430859b4c25318f554fb860
yanndu91 <yanndanielou@users.noreply.github.com>
2026-05-01 11:16:52 +0200

    Update main_archives_next.py

--- Python/STSLogAnalyzis/main_archives_next.py
+++ Python/STSLogAnalyzis/main_archives_next.py
@@ -46,8 +46,8 @@ def main() -> None:
 
         railway_line, archive_decoder = next_data.get_encoders()
 
-        for date_min, date_max, analyzis_label in [
-            ("2026-03-28T16:35:00.000", "2026-03-28T16:55:00.000", "CFX00921734"),
+        for date_min, date_max, analysis_label in [
+            ("2026-03-28T16:35:00.000", "2026-03-28T16:59:00.000", "CFX00921734"),
             ("2026-03-28T16:10:00.000", "2026-03-28T16:28:00.000", "CFX00921756"),
             ("2026-03-28T22:45:00.000", "2026-03-28T23:05:00.000", "CFX00921760"),
         ]:
@@ -66,7 +66,7 @@ def main() -> None:
                 .build()
             )
 
-            analyzis = archive_analyzis.ArchiveAnalyzis(railway_line=railway_line, archive_library=archive_library, label=analyzis_label)
+            analyzis = archive_analyzis.ArchiveAnalyzis(railway_line=railway_line, archive_library=archive_library, label=analysis_label)
             analyzis.create_reports_all_sqlarch_changes_since_previous(
                 output_directory_path=OUTPUT_DIRECTORY,
             )
