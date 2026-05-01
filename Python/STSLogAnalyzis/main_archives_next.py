432e01ba0c6dec2da2168bb54cd5e8017fa56208
yanndu91 <yanndanielou@users.noreply.github.com>
2026-05-01 11:04:18 +0200

    Update main_archives_next.py

--- Python/STSLogAnalyzis/main_archives_next.py
+++ Python/STSLogAnalyzis/main_archives_next.py
@@ -46,25 +46,30 @@ def main() -> None:
 
         railway_line, archive_decoder = next_data.get_encoders()
 
-        archive_library = (
-            next_data.get_classic_archive_library_base_builder(archive_decoder=archive_decoder).add_archive_files(
-                directory_path=r"C:\Users\fr232487\Downloads\Archives_site_202-03- 27 au 29", filename_pattern="NEXTFileArchiveServer_*.json"
-            )
-            # .add_archive_file(file_full_path=r"C:\Users\fr232487\Downloads\Archives_site_202-03- 27 au 29\CFX00921734_FU.json")
-            # .add_archive_file(file_full_path=r"C:\Users\fr232487\Downloads\Archives_site_202-03- 27 au 29\NEXTFileArchiveServer_238.json")
-            # add_sqlarch_archive_lines_filter(
-            #    decode_archive.DatesFilter.DateBetweenFilter(
-            #        date_min=parser.parse("2026-03-28T16:50:00.000"),
-            #        date_max=parser.parse("2026-03-28T16:56:00.000"),
-            #    )
-            # )
-            .build()
-        )
+        for date_min, date_max, analyzis_label in [
+            ("2026-03-28T16:35:00.000", "2026-03-28T16:55:00.000", "CFX00921734"),
+            ("2026-03-28T16:10:00.000", "2026-03-28T16:28:00.000", "CFX00921756"),
+            ("2026-03-28T22:45:00.000", "2026-03-28T23:05:00.000", "CFX00921760"),
+        ]:
 
-        analyzis = archive_analyzis.ArchiveAnalyzis(railway_line=railway_line, archive_library=archive_library, label="CFX")
-        analyzis.create_reports_all_sqlarch_changes_since_previous(
-            output_directory_path=OUTPUT_DIRECTORY,
-        )
+            archive_library = (
+                next_data.get_classic_archive_library_base_builder(archive_decoder=archive_decoder)
+                .add_archive_files(directory_path=r"C:\Users\fr232487\Downloads\Archives_site_202-03- 27 au 29", filename_pattern="NEXTFileArchiveServer_*.json")
+                # .add_archive_file(file_full_path=r"C:\Users\fr232487\Downloads\Archives_site_202-03- 27 au 29\CFX00921734_FU.json")
+                # .add_archive_file(file_full_path=r"C:\Users\fr232487\Downloads\Archives_site_202-03- 27 au 29\NEXTFileArchiveServer_238.json")
+                .add_sqlarch_archive_lines_filter(
+                    decode_archive.DatesFilter.DateBetweenFilter(
+                        date_min=parser.parse(date_min),
+                        date_max=parser.parse(date_max),
+                    )
+                )
+                .build()
+            )
+
+            analyzis = archive_analyzis.ArchiveAnalyzis(railway_line=railway_line, archive_library=archive_library, label=analyzis_label)
+            analyzis.create_reports_all_sqlarch_changes_since_previous(
+                output_directory_path=OUTPUT_DIRECTORY,
+            )
 
 
 if __name__ == "__main__":
