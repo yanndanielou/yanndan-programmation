a246fc15ee82f759fed1f3fefad7629eab6f1027
yanndu91 <yanndanielou@users.noreply.github.com>
2026-05-29 23:00:52 +0200

    Update test_decode_messages.py

--- Python/STSLogAnalyzis/test/test_decode_messages.py
+++ Python/STSLogAnalyzis/test/test_decode_messages.py
@@ -134,14 +134,14 @@ class TestDecodeCbtcMessageswithRecordWithDimension:
 
 class TestDecodeHlf:
     def test_0(self) -> None:
-        assert decode_message.HLFDecoder.decode_hlf_fields_to_datetime(time_field_value=0, time_offset_value=0, decade_field_value=0, day_on_decade_field_value=0) == datetime.datetime(
+        assert decode_message.HLFDecoderFromMessage.decode_hlf_fields_to_datetime(time_field_value=0, time_offset_value=0, decade_field_value=0, day_on_decade_field_value=0) == datetime.datetime(
             year=2000, month=1, day=1, hour=0, minute=0, second=0
         )
 
     @pytest.mark.parametrize("time_field_value, time_offset_value,decade_field_value, day_on_decade_field_value, expected_datetime", decode_hlf_test_data)
     def test_several_data(self, time_field_value: int, time_offset_value: int, decade_field_value: int, day_on_decade_field_value: int, expected_datetime: datetime.datetime) -> None:
 
-        decoded_hlf = decode_message.HLFDecoder.decode_hlf_fields_to_datetime(
+        decoded_hlf = decode_message.HLFDecoderFromMessage.decode_hlf_fields_to_datetime(
             time_field_value=time_field_value, time_offset_value=time_offset_value, decade_field_value=decade_field_value, day_on_decade_field_value=day_on_decade_field_value
         )
         assert decoded_hlf == expected_datetime, str(decoded_hlf)
