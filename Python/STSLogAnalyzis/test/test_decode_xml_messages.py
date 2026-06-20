837933c54a9d0983687a3515db49dd1074a025f8
yanndu91 <yanndanielou@users.noreply.github.com>
2026-06-20 02:02:14 +0200

    Update test_decode_xml_messages.py

--- Python/STSLogAnalyzis/test/test_decode_xml_messages.py
+++ Python/STSLogAnalyzis/test/test_decode_xml_messages.py
@@ -1,7 +1,3 @@
-from typing import List
-
-import pytest
-
 from stsloganalyzis.archive import decode_xml_message
 
 # fmt: off
@@ -23,3 +19,14 @@ def test_decode_message_101_rc_type_4()->None:
     assert decoded_message
     assert "RestrEnd1SegId"  in  decoded_message.all_fields_by_name
     assert "RestrEnd1SegId_0" not in  decoded_message.all_fields_by_name
+
+def test_decode_message_210()->None: 
+    hexa_content_as_str="00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 41 04 10 41 04 00 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 43 3C 00 01 19 40 29 38"
+    xml_message_decoder = decode_xml_message.XmlMessageDecoder(xml_directory_path=r"D:\NEXT\Data\Xml")
+    decoded_message = xml_message_decoder.decode_xml_fields_in_message_hexadecimal(message_number=210, hexadecimal_content=hexa_content_as_str) 
+    assert decoded_message.decoded_bytes_message.is_correctly_and_completely_decoded(), f"{decoded_message.decoded_bytes_message.number_of_bits_remaining_to_decode} remaing bits to decode"
+    assert decoded_message
+    assert "VBOccupancyTrain_0"  in  decoded_message.decoded_fields_flat_directory
+    assert "VBOccupancyTrain_1"  in  decoded_message.decoded_fields_flat_directory
+    assert "VBOccupancyTrain_2"  in  decoded_message.decoded_fields_flat_directory
+    assert "VBOccupancyTrain_3"  in  decoded_message.decoded_fields_flat_directory
