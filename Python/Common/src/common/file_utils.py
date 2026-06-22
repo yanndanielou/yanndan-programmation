33321924796311b08032e68aa19fc260487e2f88
yanndu91 <yanndanielou@users.noreply.github.com>
2026-06-22 05:54:34 +0200

    Update file_utils.py

--- Python/Common/src/common/file_utils.py
+++ Python/Common/src/common/file_utils.py
@@ -67,7 +67,7 @@ def create_folder_if_not_exist(directory_path: str) -> bool:
 
 def get_files_by_directory_and_file_name_mask(
     directory_path: str,
-    filename_pattern: str,
+    filename_pattern: str = "*",
     file_sort_order: FileSortOrder = FileSortOrder.NO_SORTING,
 ) -> List[str]:
     files_paths: List[str] = []
