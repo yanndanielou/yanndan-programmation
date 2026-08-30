0e6719e8b3ba163c87aede4124095019b2467913
yanndu91 <yanndanielou@users.noreply.github.com>
2026-08-30 08:59:52 +0200

    Store password encrypted

---
+++ Python/GenerateCFXHistory/connexion_param.py
@@ -0,0 +1,4 @@
+from common import basic_encryption
+
+champfx_login = basic_encryption.decode_basic_encryption_string("BE112&6Dgs34359")
+champfx_password = basic_encryption.decode_basic_encryption_string("BoupjofNplb2/")
