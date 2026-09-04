e0eee5c3a47662396c7557971a31521a1b26567a
yanndu91 <yanndanielou@users.noreply.github.com>
2026-09-04 21:15:14 +0200

    Add hlf decoding examples

--- Python/STSLogAnalyzis/main_decode_timestamps.py
+++ Python/STSLogAnalyzis/main_decode_timestamps.py
@@ -7,6 +7,7 @@ from dateutil import parser
 
 from stsloganalyzis.archive import archive_analyzis, decode_archive
 from stsloganalyzis.atc import atc_logs
+from stsloganalyzis.common import hlf
 
 OUTPUT_DIRECTORY = "output"
 
@@ -16,3 +17,5 @@ for c_heure in [
     419670,
 ]:
     print(str(c_heure) + " = " + str(atc_logs.pert_variable_to_timestamp(c_heure=c_heure, c_decalage=7200000, c_decenie=0, c_jour=0)))
+
+print(hlf.decode_hlf_to_datetime(time_field_value=586360, day_on_decade_field_value=2437, decade_field_value=2, time_offset_value=72000))
