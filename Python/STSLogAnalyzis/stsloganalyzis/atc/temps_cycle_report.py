c90eebe8c7a83beb6f9103ffec848e7694080a4a
yanndu91 <yanndanielou@users.noreply.github.com>
2026-09-05 07:19:45 +0200

    improve report

--- Python/STSLogAnalyzis/stsloganalyzis/atc/temps_cycle_report.py
+++ Python/STSLogAnalyzis/stsloganalyzis/atc/temps_cycle_report.py
@@ -1,11 +1,11 @@
-import pandas
+import statistics
 from collections import Counter, OrderedDict
 from dataclasses import dataclass
 from typing import cast
 
+import numpy
+import pandas
 from common import (
-    file_name_utils,
-    reports_utils,
     pandas_utils,
 )
 from logger import logger_config
@@ -24,17 +24,18 @@ class OneEquipmentReport:
     def __post_init__(self) -> None:
         super().__init__()
         logger_config.print_and_log_info(f"Create report for {self.atc_test_file.file_name} {self.variable.equipment.name} {self.variable.name}")
-        self.min_value = self.variable.min_numeric_values_by_number_occurrences
-        self.max_value = self.variable.max_numeric_values_by_number_occurrences
-        self.mean_value = round(self.variable.mean_numeric_values_by_number_occurrences, 2)
-        self.median_value = self.variable.median_numeric_values_by_number_occurrences
-        self.deciles = self.variable.deciles_numeric_values_by_number_occurrences
-        self.centiles = self.variable.centiles_numeric_values_by_number_occurrences
-        self.variance = self.variable.variance_numeric_values_by_number_occurrences
-        self.ecart_type = self.variable.ecart_type_numeric_values_by_number_occurrences
 
         self.all_relevant_values = [value for value in cast(list[int | float], self.variable.all_instant_states_best_values) if value > 50]
         self.number_relevant_values = len(self.all_relevant_values)
+
+        self.min_of_relevant_values = min(self.all_relevant_values)
+        self.max_value = self.variable.max_numeric_values_by_number_occurrences
+        self.mean_of_relevant_values = round(numpy.mean(self.all_relevant_values), 2)
+        self.median_of_relevant_values = numpy.median(self.all_relevant_values).item()
+        self.deciles_of_relevant_values = cast(list[float], numpy.percentile(self.all_relevant_values, numpy.arange(10, 100, 10)))
+        self.centiles_of_relevant_values = cast(list[float], numpy.percentile(self.all_relevant_values, numpy.arange(1, 100, 1)))
+        self.variance_of_relevant_values = statistics.pvariance(self.all_relevant_values)
+        self.ecart_type_of_relevant_values = statistics.pstdev(self.all_relevant_values)
 
         self.high_consumption_threshold = (
             # fmt: off
@@ -56,7 +57,7 @@ class OneEquipmentReport:
         # 1. Distribution (Médiane et Mode)
         relevant_values_sorted_by_value = sorted(self.all_relevant_values)
         n = len(relevant_values_sorted_by_value)
-        self.mediane = relevant_values_sorted_by_value[n // 2] if n % 2 != 0 else (relevant_values_sorted_by_value[n // 2 - 1] + relevant_values_sorted_by_value[n // 2]) / 2
+        self.mediane_relevant_values = relevant_values_sorted_by_value[n // 2] if n % 2 != 0 else (relevant_values_sorted_by_value[n // 2 - 1] + relevant_values_sorted_by_value[n // 2]) / 2
         self.mode = Counter(self.all_relevant_values).most_common(1)[0][0]
 
         # 3. Identification des anomalies et segments de surconsommation
@@ -89,10 +90,6 @@ class OneEquipmentReport:
         conso_anomalies_very_high = sum(x for x in self.all_relevant_values if x > self.very_high_consumption_threshold)
         self.ratio_energie_pics_very_high = (conso_anomalies_very_high / conso_totale * 100) if conso_totale > 0 else 0
 
-        # 4. Saturation au plafond (ex: >= 98%)
-        points_saturation = sum(1 for x in self.all_relevant_values if x >= 98)
-        self.taux_saturation = (points_saturation / len(self.all_relevant_values)) * 100
-
         # 5. Temps de recouvrement (Cool-down) après anomalie
         en_crise = False
         temps_recouvrement = []
@@ -114,7 +111,7 @@ class OneEquipmentReport:
 
         self.nb_anomalies_high = len(self.anomalies_high)
         self.taux_anomalie_high = (self.nb_anomalies_high / len(self.all_relevant_values)) * 100
-        self.taux_anomalie_high = (len(self.anomalies_very_high) / len(self.all_relevant_values)) * 100
+        self.taux_anomalie_very_high = (len(self.anomalies_very_high) / len(self.all_relevant_values)) * 100
 
 
 def build_temps_cycle_report_from_atc_log(atc_test_results: list[atc_logs.ATCTestResult]) -> None:
@@ -148,40 +145,40 @@ def build_temps_cycle_report_from_atc_log(atc_test_results: list[atc_logs.ATCTes
                 equipment_report_dict = OrderedDict(
                     {
                         "File name": equipment_report.atc_test_file.file_name,
-                        "File full path": equipment_report.atc_test_file.file_full_path,
                         "label": equipment_report.atc_test_file.atc_test_result.label,
                         "environment": equipment_report.atc_test_file.atc_test_result.environment_name,
                         "variable": equipment_report.variable.name,
                         "equipment": equipment_report.variable.equipment.name,
                         "equipment type": equipment_report.variable.equipment.equipment_type.name,
                         "redundancy status": equipment_report.atc_test_file.atc_test_result.get_equipment_redundancy_by_name(equipment_report.variable.equipment.name).name,
-                        "min_value": equipment_report.min_value,
+                        "min_of_relevant_values": equipment_report.min_of_relevant_values,
                         "max_value": equipment_report.max_value,
-                        "mean_value": equipment_report.mean_value,
-                        "median_value": equipment_report.median_value,
-                        "mediane": equipment_report.mediane,
-                        "variance": equipment_report.variance,
-                        "ecart_type": equipment_report.ecart_type,
+                        "mean_of_relevant_values": equipment_report.mean_of_relevant_values,
+                        "median_of_relevant_values": equipment_report.median_of_relevant_values,
+                        "mediane_relevant_values": equipment_report.mediane_relevant_values,
+                        "variance_of_relevant_values": equipment_report.variance_of_relevant_values,
+                        "ecart_type_of_relevant_values": equipment_report.ecart_type_of_relevant_values,
                         "Number relevant values": len(equipment_report.all_relevant_values),
                         "Number not relevant (filtered) values": len(equipment_report.variable.instant_states_chronologically_sorted) - len(equipment_report.all_relevant_values),
                         "duree_max_consecutive above high": equipment_report.duree_max_above_high_consecutive,
                         "nombre anomalies high": len(equipment_report.anomalies_high),
+                        "taux_anomalie high (%)": equipment_report.taux_anomalie_high,
                         "nombre anomalies very high": len(equipment_report.anomalies_very_high),
-                        "taux_anomalie (%)": equipment_report.taux_anomalie_high,
+                        "taux_anomalie very high (%)": equipment_report.taux_anomalie_very_high,
                         "mode": equipment_report.mode,
-                        "recouvrement_moyen": equipment_report.recouvrement_moyen_high,
-                        "taux_saturation (%)": equipment_report.taux_saturation,
+                        "recouvrement_moyen high": equipment_report.recouvrement_moyen_high,
                         "ratio_energie_pics high (%)": equipment_report.ratio_energie_pics_high,
                         "ratio_energie_pics very high (%)": equipment_report.ratio_energie_pics_very_high,
                         "max_hausse_brutale": equipment_report.max_hausse_brutale,
                         "high_consumption_threshold": equipment_report.high_consumption_threshold,
                         "very_high_consumption_threshold": equipment_report.very_high_consumption_threshold,
+                        "File full path": equipment_report.atc_test_file.file_full_path,
                     }
                 )
                 rows_as_list_dict.append(equipment_report_dict)
-                for decile_index, decile_value in enumerate(equipment_report.deciles):
+                for decile_index, decile_value in enumerate(equipment_report.deciles_of_relevant_values):
                     equipment_report_dict[f"Decile_{decile_index+1}"] = decile_value
-                for centile_index, centile_value in enumerate(equipment_report.centiles):
+                for centile_index, centile_value in enumerate(equipment_report.centiles_of_relevant_values):
                     if centile_index > 90:
                         equipment_report_dict[f"Centile_{centile_index+1}"] = centile_value
 
