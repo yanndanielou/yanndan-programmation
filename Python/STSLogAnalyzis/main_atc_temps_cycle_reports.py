48e9bf1c63d30d7a9dad7a7d04cc1467fa092847
yanndu91 <yanndanielou@users.noreply.github.com>
2026-09-07 08:24:45 +0200

    Update main_atc_temps_cycle_reports.py

--- Python/STSLogAnalyzis/main_atc_temps_cycle_reports.py
+++ Python/STSLogAnalyzis/main_atc_temps_cycle_reports.py
@@ -25,15 +25,11 @@ def main() -> None:
             [
                 (
                     "PLT_SYST_1 RL3A02 Conf30",
-                    r"D:\temp\mesures_temps_cycle_usine\PLT_SYST_1_RL3a.0.2_Conf_30____",
+                    r"D:\temp\mesures_temps_cycle_usine\PLT_SYST_1_RL3a.0.2_Conf_30",
                 ),
                 (
                     "PLT_SYST_2 RL3A02 Conf30",
-                    r"D:\temp\mesures_temps_cycle_usine\PLT_SYST_2_RL3a.0.2_Conf_30___",
-                ),
-                (
-                    "4.1_LR_TrDyn_KVB",
-                    r"D:\temp\mesures_temps_cycle_usine\PLT_SYST_2_RL3a.0.2_Conf_30\4.1_LR_TrDyn_KVB-CMC_V2EO_PLDE2480__CMC_US_AC_V2EO_PLDE2480__CMC_US_A",
+                    r"D:\temp\mesures_temps_cycle_usine\PLT_SYST_2_RL3a.0.2_Conf_30",
                 ),
             ],
         )
