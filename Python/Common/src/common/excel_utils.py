1157d44e5d2b9a6499744bcb476e7bdfc55ca5ac
yanndu91 <yanndanielou@users.noreply.github.com>
2026-09-15 15:20:56 +0200

    Update excel_utils.py

--- Python/Common/src/common/excel_utils.py
+++ Python/Common/src/common/excel_utils.py
@@ -5,7 +5,7 @@ import time
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from enum import Enum, auto
-from typing import List, cast
+from typing import cast
 
 import openpyxl
 import xlwings
@@ -100,8 +100,8 @@ def convert_xlsx_file_to_xls_with_openpyxl(xlsx_file_full_path: str) -> str:
     return xls_output_file_path
 
 
+@logger_config.stopwatch_decorator(inform_beginning=True, monitor_ram_usage=True)
 def close_all_xlwings() -> None:
-    logger_config.print_and_log_info("close_all_xlwings")
     app = xlwings.apps.add()
     wb = xlwings.Book()
     wb.close()
@@ -134,7 +134,7 @@ class XlWingsOpenWorkbookOperation:
 
     @staticmethod
     def open_workbook(input_excel_file_path: str, excel_visibility: bool) -> xlwings.Book:
-        with logger_config.stopwatch_with_label(label=f"Open {input_excel_file_path}", inform_beginning=True):
+        with logger_config.stopwatch_with_label(label=f"Open {input_excel_file_path}", inform_beginning=True, monitor_ram_usage=True):
             xlwings.App(visible=excel_visibility)
             workbook_dml = xlwings.Book(input_excel_file_path)
 
@@ -206,13 +206,13 @@ class XlWingsSaveAndCloseWorkbookOperation(XlWingOperationBase):
 
 @dataclass
 class XlWingsRemoveTabsOperation(XlWingOperationBase):
-    sheets_to_keep_names: List[str]
+    sheets_to_keep_names: list[str]
 
     def do(self, workbook_dml: xlwings.Book) -> None:
         XlWingsRemoveTabsOperation.remove_tabs_with_xlwings(workbook_dml, self.sheets_to_keep_names)
 
     @staticmethod
-    def remove_tabs_with_xlwings(workbook_dml: xlwings.Book, sheets_to_keep_names: List[str]) -> None:
+    def remove_tabs_with_xlwings(workbook_dml: xlwings.Book, sheets_to_keep_names: list[str]) -> None:
         with logger_config.stopwatch_with_label(f"remove_tabs_with_xlwings , sheets_to_keep_names:{sheets_to_keep_names}", inform_beginning=True):
 
             sheets_names = workbook_dml.sheet_names
@@ -287,7 +287,6 @@ class XlWingsReplaceFormulasWithCurrentValueOperation(XlWingOperationBase):
 
             # Parcourir toutes les feuilles
             for sheet in workbook_dml.sheets:
-                sheet = cast(xlwings.Sheet, sheet)
                 number_of_cells_to_parse = len(sheet.used_range)
                 with logger_config.stopwatch_with_label(label=f"Handle sheet:{sheet.name} with {number_of_cells_to_parse} cells to parse", inform_beginning=True):
                     # Parcourir toutes les cellules dans la zone utilisée de la feuille
@@ -310,13 +309,13 @@ class XlWingsReplaceFormulasWithCurrentValueOperation(XlWingOperationBase):
 
 @dataclass
 class XlWingsRemoveRangesOperation(XlWingOperationBase):
-    ranges_to_remove: List[str] = field(default_factory=list)
+    ranges_to_remove: list[str] = field(default_factory=list)
 
     def do(self, workbook_dml: xlwings.Book) -> None:
         XlWingsRemoveRangesOperation.remove_ranges_with_xlwings(workbook_dml, self.ranges_to_remove)
 
     @staticmethod
-    def remove_ranges_with_xlwings(workbook_dml: xlwings.Book, ranges_to_remove: List[str]) -> None:
+    def remove_ranges_with_xlwings(workbook_dml: xlwings.Book, ranges_to_remove: list[str]) -> None:
         with logger_config.stopwatch_with_label(f"remove_ranges_with_xlwings input_excel_file_path:{workbook_dml.name}, ranges_to_remove:{ranges_to_remove}", inform_beginning=True):
 
             for range_to_remove in ranges_to_remove:
@@ -335,7 +334,7 @@ class ColumnRemovalOperationType(Enum):
 class RemoveColumnsInstructions:
     input_excel_file_path: str
     sheet_name: str
-    columns_to_remove_names: List[str]
+    columns_to_remove_names: list[str]
     removal_operation_type: ColumnRemovalOperationType
     file_to_create_path: str
     assert_if_column_is_missing: bool
@@ -345,7 +344,7 @@ class RemoveColumnsInstructions:
 class XlWingsRemoveColumnsOperation(XlWingOperationBase):
 
     sheet_name: str
-    columns_to_remove_names: List[str]
+    columns_to_remove_names: list[str]
     assert_if_column_is_missing: bool
     removal_operation_type: ColumnRemovalOperationType = ColumnRemovalOperationType.COLUMN_ONE_BY_ONE_USING_LETTER
 
@@ -362,7 +361,7 @@ class XlWingsRemoveColumnsOperation(XlWingOperationBase):
     def remove_columns_with_xlwings(
         workbook_dml: xlwings.Book,
         sheet_name: str,
-        columns_to_remove_names: List[str],
+        columns_to_remove_names: list[str],
         assert_if_column_is_missing: bool,
         removal_operation_type: ColumnRemovalOperationType = ColumnRemovalOperationType.COLUMN_ONE_BY_ONE_USING_LETTER,
     ) -> None:
@@ -377,13 +376,13 @@ class XlWingsRemoveColumnsOperation(XlWingOperationBase):
             sht: xlwings.Sheet = workbook_dml.sheets(sheet_name)
 
             # Obtenir toutes les valeurs de la première ligne
-            at_beginning_headers: List[str] = sht.range("A1").expand("right").value
+            at_beginning_headers: list[str] = sht.range("A1").expand("right").value
             logger_config.print_and_log_info(f"{inspect.stack(0)[0].function}: At beginning, {len(at_beginning_headers)} headers: {at_beginning_headers[:5]}...")
             logger_config.print_and_log_info(f"{inspect.stack(0)[0].function}: At beginning, {len(at_beginning_headers)} headers: {at_beginning_headers}", do_not_print=True)
 
             if removal_operation_type == ColumnRemovalOperationType.ALL_COLUMNS_AT_ONCE:
 
-                all_col_initial_index_to_remove: List[int] = []
+                all_col_initial_index_to_remove: list[int] = []
                 for colum_it, column_name_to_remove in enumerate(columns_to_remove_names):
                     col_index_starting_0 = at_beginning_headers.index(column_name_to_remove)
                     all_col_initial_index_to_remove.append(col_index_starting_0)
@@ -407,7 +406,7 @@ class XlWingsRemoveColumnsOperation(XlWingOperationBase):
             else:
 
                 # Obtenir toutes les valeurs de la première ligne
-                headers_found_in_excel: List[str] = sht.range("A1").expand("right").value
+                headers_found_in_excel: list[str] = sht.range("A1").expand("right").value
                 logger_config.print_and_log_info(f"{len(headers_found_in_excel)} headers:{headers_found_in_excel[:5]}...")
                 logger_config.print_and_log_info(f"{len(headers_found_in_excel)} headers:{headers_found_in_excel}", do_not_print=True)
 
@@ -435,6 +434,7 @@ class XlWingsRemoveColumnsOperation(XlWingOperationBase):
                         with logger_config.stopwatch_with_label(
                             label=f"Removing {colum_it+1}/{number_of_columns_to_remove}th column {column_name_to_remove} with index(starting0):{col_index_starting_0}, letter:{col_letter}.  {round((colum_it+1)/number_of_columns_to_remove*100,2)}%",
                             inform_beginning=False,
+                            monitor_ram_usage=True,
                         ):
                             if removal_operation_type == ColumnRemovalOperationType.COLUMN_ONE_BY_ONE_USING_LETTER:
                                 col_range = f"{col_letter}:{col_letter}"
@@ -450,7 +450,7 @@ class XlWingsRemoveColumnsOperation(XlWingOperationBase):
                     else:
                         logger_config.print_and_log_error(f"{inspect.stack(0)[0].function}: Column {column_name_to_remove} not found in excel among {headers_found_in_excel}")
 
-            at_end_headers: List[str] = sht.range("A1").expand("right").value
+            at_end_headers: list[str] = sht.range("A1").expand("right").value
             logger_config.print_and_log_info(f"{inspect.stack(0)[0].function}: At the end, {len(at_end_headers)} headers: {at_end_headers}")
 
 
@@ -460,7 +460,7 @@ class XlWingsOperationsBatch:
     excel_visibility: bool
     input_excel_file_path: str
     file_to_create_path: str
-    operations: List[XlWingOperationBase] = field(default_factory=list)
+    operations: list[XlWingOperationBase] = field(default_factory=list)
 
     def hide_excel(self) -> "XlWingsOperationsBatch":
         self.excel_visibility = False
@@ -487,7 +487,7 @@ def save_and_close_workbook(workbook_dml: xlwings.Book | openpyxl.Workbook, file
     XlWingsSaveAndCloseWorkbookOperation(file_path).do(workbook_dml)
 
 
-def remove_tabs_with_openpyxl(input_excel_file_path: str, file_to_create_path: str, sheets_to_keep_names: List[str]) -> str:
+def remove_tabs_with_openpyxl(input_excel_file_path: str, file_to_create_path: str, sheets_to_keep_names: list[str]) -> str:
     with file_utils.temporary_copy_of_file(input_excel_file_path) as temp_file_full_path:
 
         with logger_config.stopwatch_with_label(
@@ -528,7 +528,7 @@ def remove_tabs_with_openpyxl(input_excel_file_path: str, file_to_create_path: s
             return file_to_create_path
 
 
-def remove_tabs_with_xlwings(input_excel_file_path: str, file_to_create_path: str, sheets_to_keep_names: List[str], excel_visibility: bool = False) -> str:
+def remove_tabs_with_xlwings(input_excel_file_path: str, file_to_create_path: str, sheets_to_keep_names: list[str], excel_visibility: bool = False) -> str:
     with file_utils.temporary_copy_of_file(input_excel_file_path) as temp_file_full_path:
         with logger_config.stopwatch_with_label(
             f"remove_tabs_with_xlwings input_excel_file_path:{input_excel_file_path}, file_to_create_path:{file_to_create_path}, sheets_to_keep_names:{sheets_to_keep_names}", inform_beginning=True
@@ -575,7 +575,7 @@ def replace_formulas_with_values_with_xlwings(input_excel_file_path: str, file_t
             return file_to_create_path
 
 
-def remove_ranges_with_xlwings(input_excel_file_path: str, file_to_create_path: str, ranges_to_remove: List[str], excel_visibility: bool = False) -> str:
+def remove_ranges_with_xlwings(input_excel_file_path: str, file_to_create_path: str, ranges_to_remove: list[str], excel_visibility: bool = False) -> str:
     with file_utils.temporary_copy_of_file(input_excel_file_path) as temp_file_full_path:
 
         with logger_config.stopwatch_with_label(
@@ -737,6 +737,7 @@ def copy_and_paste_excel_content_with_format_with_openpyxl(input_excel_file_path
             print(f"Contenu copié avec succès dans : {output_excel_file_path}")
 
 
+@logger_config.stopwatch_decorator(inform_beginning=True, monitor_ram_usage=True)
 def copy_and_paste_excel_content_with_format_with_win32(
     input_excel_file_path: str, sheet_name: str, output_excel_file_path: str, replace_formulas_by_value: bool, excel_visibility: bool = False
 ) -> str:
