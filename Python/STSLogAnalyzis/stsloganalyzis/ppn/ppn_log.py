b02424f263407bbc3fec4c1fd6a88af68beaf9c9
yanndu91 <yanndanielou@users.noreply.github.com>
2026-09-20 20:03:56 +0200

    Update ppn_log.py

--- Python/STSLogAnalyzis/stsloganalyzis/ppn/ppn_log.py
+++ Python/STSLogAnalyzis/stsloganalyzis/ppn/ppn_log.py
@@ -126,4 +126,4 @@ class ProfibusLogLine:
         if self.mode == SendingMode.SDA:
             self.unisig_messages = decode_unisig.SdaUnisigMessage.from_sda_hexa_bytes_str(self.bytes_hexa, self.upper_layer_decoding_library)
         else:
-            self.unisig_messages = decode_unisig.SdnUnisigMessage.from_sda_hexa_bytes_str(self.bytes_hexa, self.upper_layer_decoding_library)
+            self.unisig_messages = decode_unisig.SdnUnisigMessage.decode_sdn_bytes_hexa(self.bytes_hexa, self.upper_layer_decoding_library)
