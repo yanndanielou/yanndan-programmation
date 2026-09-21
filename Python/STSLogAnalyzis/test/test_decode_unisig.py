acbe287ea8f27fa77b490eb90a3fc69fa5d82317
yanndu91 <yanndanielou@users.noreply.github.com>
2026-09-21 08:23:37 +0200

    Update test_decode_unisig.py

--- Python/STSLogAnalyzis/test/test_decode_unisig.py
+++ Python/STSLogAnalyzis/test/test_decode_unisig.py
@@ -7,6 +7,22 @@ from stsloganalyzis.unisig import decode_unisig, upper_layer_libraries
 from common import bytes_messages
 
 
+class TestUpperLayerLibrary:
+
+    def tests_upper_layer_library_packet_field_containing_fields(self) -> None:
+        upper_layer_decoding_library = upper_layer_libraries.UpperLayerDecodingLibrary.from_next_json_file_full_path(r"test\resources\ppn\upper_layer_library_packet_field_containing_fields.json")
+
+        data_to_encode = bytes_messages.convert_hex_string_to_hex_bytes("7Bh 89h 34h 53h 01h 02h 03h 04h 05h 06h 07h 08h 09h 10h")
+        telegram = decode_unisig.UpperLayerTelegram(
+            safety_level=decode_unisig.SafetyLevel.SL4,
+            telegram_name="",
+            byte_message_decoded=bytes_messages.DecodedBytesMessage.from_hex_string("34h 53h 01h 02h 03h 04h 05h 06h 07h 08h 09h 10h"),
+            lowest_order_byte_sequence_number=12,
+            command_type=decode_unisig.SdaUnisigMessage.CommandTypeSubset57.SL4_TELEGRAM_FOR_UPPER_LAYER,
+            upper_layer_decoding_library=upper_layer_decoding_library,
+        )
+
+
 class TestDecodeUnisigCrc:
 
     class TestSrcSl4:
