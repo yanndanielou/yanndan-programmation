7eb9ead243251ee915a6a8d7c46ef869d92b0678
yanndu91 <yanndanielou@users.noreply.github.com>
2026-09-22 15:37:19 +0200

    Update main_decode_ppn.py

--- Python/STSLogAnalyzis/main_decode_ppn.py
+++ Python/STSLogAnalyzis/main_decode_ppn.py
@@ -20,21 +20,20 @@ def main() -> None:
         interesting_stm_ids = [179, 184, 175, 176]
         interesting_stm_messages = library.get_upper_layer_stms_by_stm_ids(interesting_stm_ids)
 
-        json_encoders.JsonEncodersUtils.serialize_list_objects_in_json(interesting_stm_messages, f"STM {' '.join(str(interesting_stm_id) for interesting_stm_id in interesting_stm_ids)}")
+        # json_encoders.JsonEncodersUtils.serialize_list_objects_in_json(interesting_stm_messages, f"STM {' '.join(str(interesting_stm_id) for interesting_stm_id in interesting_stm_ids)}")
         logger_config.print_and_log_info(f"interesting_stm_messages: {len(interesting_stm_messages)}")
 
-        def build_row(interesting_stm_message):
-            row = OrderedDict(
-                {
-                    "timestamp": interesting_stm_message.upper_layer_telegram.timestamp,
-                    "nid_stm": interesting_stm_message.nid_stm,
-                }
-            )
-            row.update({field_name: field_value for field_name, field_value in interesting_stm_message})
-            return row
-
         reports_utils.save_rows_to_output_files(
-            rows_as_list_dict=[build_row(interesting_stm_message) for interesting_stm_message in interesting_stm_messages],
+            rows_as_list_dict=[
+                OrderedDict(
+                    {
+                        "timestamp": interesting_stm_message.upper_layer_telegram.timestamp,
+                        "nid_stm": interesting_stm_message.nid_stm,
+                        **{field_name: field_value for field_name, field_value in interesting_stm_message.fields_names_and_values.items()},
+                    }
+                )
+                for interesting_stm_message in interesting_stm_messages
+            ],
             file_base_name=f"interesting_stm_messages: {len(interesting_stm_messages)}",
         )
         pass
